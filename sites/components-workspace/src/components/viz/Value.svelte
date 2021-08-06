fa7c51aa93b2b5704b9aaf19071ea49bc76d9602
Sean Hughes <sean@evidence.dev>
2021-08-06 13:32:02 -0400

    add format override to value component

--- sites/components-workspace/src/components/viz/Value.svelte
+++ sites/components-workspace/src/components/viz/Value.svelte
@@ -2,14 +2,18 @@
     import getColumnSummary from "../modules/getColumnSummary.js";
     import formatValue from "../modules/formatValue.js";
     import getParsedDate from "../modules/getParsedDate.js";
-    let fmt = null
-    let units = null
     
+    // Passing in value from dataset:
     export let data = null 
     export let row = 0    
     export let column = null
 
+    // Passing in single value directly:
     export let value = null
+
+    // Overrides for format and units:
+    export let fmt = null
+    let units = null
 
     if(data) {
         if(!column){
@@ -26,10 +30,20 @@
         }
         value = data[row][column]
         columnSummary = columnSummary.filter(d => d.id === column);
-        fmt = columnSummary[0].format;
-        units = columnSummary[0].units;
+
+        if(fmt == null){
+            fmt = columnSummary[0].format;
+        }
+
+        // Units (k, M, B) - not used in <Value> yet:
+        // units = columnSummary[0].units;
+    } else {
+        value = isNaN(value) ? value : Number.parseFloat(value);
+        if(fmt == null){
+            fmt = (typeof value === "number" ? "num" : "str");
+        }
     }
     
 </script>
 
-{formatValue(value, fmt, units)} 
+{formatValue(value, fmt)} 
