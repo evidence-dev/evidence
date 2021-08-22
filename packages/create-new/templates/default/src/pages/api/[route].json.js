5712346f1ab299fd7ddc17dfdd26d55ed0be6283
Adam <mcrascal@users.noreply.github.com>
2021-08-22 15:20:29 -0400

    build template in create new

--- packages/create-new/templates/default/src/pages/api/[route].json.js
+++ packages/create-new/templates/default/src/pages/api/[route].json.js
@@ -1,12 +1,12 @@
-import runQueries from '@evidence-dev/db-orchestrator'
+import runQueries from '@evidence-dev/db-orchestrator';
 import { dev } from '$app/env';
 
-export async function get({params}) {
-  const { route } = params;
-  const data = await runQueries(route, dev)
-  return {
-      body: {
-        data
-      }
-  };
+export async function get({ params }) {
+	const { route } = params;
+	const data = await runQueries(route, dev);
+	return {
+		body: {
+			data
+		}
+	};
 }
