2e87c81f9aa893157a895dfba844e87f82ad2eaf
Sean Hughes <sean@evidence.dev>
2021-09-09 12:01:50 -0400

    add basic error formatting to value component

--- sites/components-workspace/src/components/viz/Value.svelte
+++ sites/components-workspace/src/components/viz/Value.svelte
@@ -15,6 +15,8 @@
     export let fmt = null
     let units = null
 
+    let error;
+    try {
     if(data) {
         if(!column){
             column = Object.keys(data[row])[0]
@@ -43,7 +45,47 @@
             fmt = (typeof value === "number" ? "num" : "str");
         }
     }
+} catch(e) {
+    error = e.message;
+}
     
 </script>
 
+{#if !error}
 {formatValue(value, fmt)} 
+{:else}
+    <div class="error">
+        error
+        <span class="error-msg">{error}</span>
+    </div>
+{/if}
+
+<style>
+     .error {
+        display: inline;
+        position: relative;
+        color: red;
+        cursor: help;
+    }
+
+    .error .error-msg {
+        visibility: hidden;
+        position: absolute;
+        top: -5px;
+        left: 105%;
+        white-space: nowrap;
+        padding-left: 4px;
+        padding-right: 4px;        
+        color: white;
+        font-family: sans-serif;
+        font-size: 0.8em;
+        background-color: black;
+        border-radius: 6px;
+        z-index: 1;
+    }
+
+    .error:hover .error-msg {
+        visibility: visible;
+    }
+
+</style>
