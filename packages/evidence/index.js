acf7f158d905a6d32cf157ab5296f7914c3ca4b5
Sean Hughes <sean@evidence.dev>
2021-11-29 10:18:46 -0500

    fix echarts-stat dependency

--- packages/evidence/index.js
+++ packages/evidence/index.js
@@ -15,10 +15,11 @@ export const config = {
 		},
 		vite: {
 			optimizeDeps: {
+				include: ['echarts-stat'],
 				exclude: ['@evidence-dev/components']
 			},
 			ssr: {
-				external: ['@evidence-dev/db-orchestrator', 'echarts-stat']
+				external: ['@evidence-dev/db-orchestrator']
 			},
 			plugins: [
 				FullReload.default(['./.evidence/build/queries/**'], {delay: 150}),
--- packages/preprocess/index.cjs
+++ packages/preprocess/index.cjs
@@ -48,12 +48,14 @@ const createDefaultProps = function(filename){
         import Bar from '@evidence-dev/components/viz/Bar.svelte';
         import Bubble from '@evidence-dev/components/viz/Bubble.svelte';
         import Scatter from '@evidence-dev/components/viz/Scatter.svelte';
+        import Hist from '@evidence-dev/components/viz/Hist.svelte';
         import AreaChart from '@evidence-dev/components/viz/AreaChart.svelte';
         import BarChart from '@evidence-dev/components/viz/BarChart.svelte';
         import BubbleChart from '@evidence-dev/components/viz/BubbleChart.svelte';
         import DataTable from '@evidence-dev/components/viz/DataTable.svelte';
         import LineChart from '@evidence-dev/components/viz/LineChart.svelte';
         import ScatterPlot from '@evidence-dev/components/viz/ScatterPlot.svelte';
+        import Histogram from '@evidence-dev/components/viz/Histogram.svelte';
         import ECharts from '@evidence-dev/components/viz/ECharts.svelte';
         `
     if(hasQueries(filename)){
@@ -68,12 +70,14 @@ const createDefaultProps = function(filename){
             import Bar from '@evidence-dev/components/viz/Bar.svelte';
             import Bubble from '@evidence-dev/components/viz/Bubble.svelte';
             import Scatter from '@evidence-dev/components/viz/Scatter.svelte';
+            import Hist from '@evidence-dev/components/viz/Hist.svelte';
             import AreaChart from '@evidence-dev/components/viz/AreaChart.svelte';
             import BarChart from '@evidence-dev/components/viz/BarChart.svelte';
             import BubbleChart from '@evidence-dev/components/viz/BubbleChart.svelte';
             import DataTable from '@evidence-dev/components/viz/DataTable.svelte';
             import LineChart from '@evidence-dev/components/viz/LineChart.svelte';
             import ScatterPlot from '@evidence-dev/components/viz/ScatterPlot.svelte';
+            import Histogram from '@evidence-dev/components/viz/Histogram.svelte';
             import ECharts from '@evidence-dev/components/viz/ECharts.svelte';
             `
     }
