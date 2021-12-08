4fe684a23bda65890a015866dbaa9afdd55040e8
Sean Hughes <sean@evidence.dev>
2021-12-08 15:30:17 -0500

    change extension to use webpack bundling

--- packages/evidence-vscode/src/extension.ts
+++ packages/evidence-vscode/src/extension.ts
@@ -8,18 +8,8 @@ export function activate(context: vscode.ExtensionContext) {
 	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "evidence-vscode" is now active!');
+	console.log('Evidence VSCode is now active');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('evidence-vscode.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Welcome to evidence-vscode!');
-	});
-
-	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
