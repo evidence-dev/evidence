b65569fc4ddce42a2d1d8ced45c964c520a68171
Sean Hughes <sean@evidence.dev>
2021-12-13 16:19:10 -0500

    clean up activation events

--- packages/evidence-vscode/out/extension.js
+++ packages/evidence-vscode/out/extension.js
@@ -9,16 +9,10 @@ const vscode = require("vscode");
 function activate(context) {
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "evidence-vscode" is now active!');
+    console.log('Evidence VS Code is now active');
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('evidence-vscode.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Welcome to evidence-vscode!');
-    });
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
