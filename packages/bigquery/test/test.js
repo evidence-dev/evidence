ec2ea912a4cbab5924eb8dc137e59678341ded78
Sean Hughes <sean@evidence.dev>
2022-03-03 18:57:12 -0500

    condense tests into one per db

--- packages/bigquery/test/test.js
+++ packages/bigquery/test/test.js
@@ -7,25 +7,8 @@ let results;
 test('query runs', async () => {
     try{
         results = await runQuery("select 100 as num")
-    } catch(e) {
-        throw Error(e)
-    }
-})
-
-test('query result is an array of objects', async () => {
-    try{
-        results = await runQuery("select 100 as num")
         assert.instance(results, Array)
         assert.type(results[0], "object")
-    } catch(e) {
-        throw Error(e)
-    }
-})
-
-
-test('query result matches expected value', async () => {
-    try{
-        results = await runQuery("select 100 as num")
         assert.equal(results[0].num, 100)
     } catch(e) {
         throw Error(e)
