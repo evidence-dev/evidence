03382df4985f90c482a66714da4b696451ed3cf0
Sean Hughes <sean@evidence.dev>
2022-04-01 17:02:59 -0400

    change settings icon to svelte icon import

--- sites/example-project/src/components/ui/Sidebar.svelte
+++ sites/example-project/src/components/ui/Sidebar.svelte
@@ -37,6 +37,7 @@
     import { page } from '$app/stores';
 	import { dev } from '$app/env';
 	import CollapsibleSection from '$lib/ui/CollapsibleSection.svelte'
+	import IoMdSettings from 'svelte-icons/io/IoMdSettings.svelte'
 
 	export const load = async() => {
 		const menu = await Promise.all(menu)
@@ -98,13 +99,10 @@
         </nav>
         {#if dev}
         <div class="nav-footer">
-
 			<a href='/settings' class="settings-link" class:selected="{$page.path === '/settings'}">
-				<div class="settings-icon" href='/settings'>				
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="22" height="22">
-						<path d="M413.967 276.8c1.06-6.235 1.06-13.518 1.06-20.8s-1.06-13.518-1.06-20.8l44.667-34.318c4.26-3.118 5.319-8.317 2.13-13.518L418.215 115.6c-2.129-4.164-8.507-6.235-12.767-4.164l-53.186 20.801c-10.638-8.318-23.394-15.601-36.16-20.801l-7.448-55.117c-1.06-4.154-5.319-8.318-10.638-8.318h-85.098c-5.318 0-9.577 4.164-10.637 8.318l-8.508 55.117c-12.767 5.2-24.464 12.482-36.171 20.801l-53.186-20.801c-5.319-2.071-10.638 0-12.767 4.164L49.1 187.365c-2.119 4.153-1.061 10.399 2.129 13.518L96.97 235.2c0 7.282-1.06 13.518-1.06 20.8s1.06 13.518 1.06 20.8l-44.668 34.318c-4.26 3.118-5.318 8.317-2.13 13.518L92.721 396.4c2.13 4.164 8.508 6.235 12.767 4.164l53.187-20.801c10.637 8.318 23.394 15.601 36.16 20.801l8.508 55.117c1.069 5.2 5.318 8.318 10.637 8.318h85.098c5.319 0 9.578-4.164 10.638-8.318l8.518-55.117c12.757-5.2 24.464-12.482 36.16-20.801l53.187 20.801c5.318 2.071 10.637 0 12.767-4.164l42.549-71.765c2.129-4.153 1.06-10.399-2.13-13.518l-46.8-34.317zm-158.499 52c-41.489 0-74.46-32.235-74.46-72.8s32.971-72.8 74.46-72.8 74.461 32.235 74.461 72.8-32.972 72.8-74.461 72.8z"></path>
-					</svg>
-				</div>
+				<span class="settings-icon">
+					<IoMdSettings/>
+				</span>
 				<a class="settings-label" href='/settings'>				
 					Settings
 				</a>
@@ -223,7 +221,7 @@ div.nav-header a {
 }
 
 .settings-link:hover .settings-icon {
-	fill: var(--grey-700);
+	color: var(--grey-700);
 }
 
 .settings-link:hover a {
@@ -235,14 +233,16 @@ div.nav-header a {
 }
 
 .settings-link.selected .settings-icon {
-	fill: var(--blue-600);
+	color: var(--blue-600);
 }
 
 .settings-icon {
 	/* padding: 0.2rem 1rem 1.2rem 0.5rem; */
 	/* padding: 0.05rem 10rem 1.2rem 0.1rem; */
 	padding: 0.082rem 0rem 0rem 0.5rem;
-	fill: var(--grey-500);
+	color: var(--grey-500);
+	height: 22px;
+	width: 22px;
 }
 
 
