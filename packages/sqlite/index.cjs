93e5e073cb42141ba4570a900f70fda388873ce8
Sean Hughes <sean@evidence.dev>
2022-04-07 11:05:49 -0400

    add error handling to sqlite connector

--- packages/sqlite/index.cjs
+++ packages/sqlite/index.cjs
@@ -2,14 +2,23 @@ const sqlite3 = require('sqlite3');
 const { open } = require('sqlite');
 
 const runQuery = async (queryString, database) => {
-    const filename = database ? database.filename : process.env["SQLITE_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
-    const db = await open({
-        filename: filename,
-        driver: sqlite3.Database,
-        mode: sqlite3.OPEN_READONLY,
-    })
 
-    return await db.all(queryString)
+    try {
+        const filename = database ? database.filename : process.env["SQLITE_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
+        const db = await open({
+            filename: filename,
+            driver: sqlite3.Database,
+            mode: sqlite3.OPEN_READONLY,
+        })
+        const result = await db.all(queryString);
+        return result;
+    } catch(err) {
+        if (err.message) {
+            throw err.message
+        } else {
+            throw err
+        }
+    }
 }
 
 module.exports = runQuery
