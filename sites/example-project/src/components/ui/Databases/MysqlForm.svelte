1b064519afcb6f85c3582f00a3dc533637a0cf7d
Sean Hughes <sean@evidence.dev>
2022-04-11 12:28:57 -0400

    get mysql optional field in line with postgres format

--- sites/example-project/src/components/ui/Databases/MysqlForm.svelte
+++ sites/example-project/src/components/ui/Databases/MysqlForm.svelte
@@ -17,7 +17,15 @@
         {id: "database", label: "Database", placeholder: "my-database-name", type: "text", value: credentials.database ?? ""},
         {id: "user", label: "User", type: "text", placeholder: "username", value: credentials.user ?? ""},
         {id: "password", label: "Password", type: "password", placeholder: "password", value: credentials.password ?? ""},
-        {id: "socketPath", label: "Socket Path (optional)", type: "text", value: credentials.socketPath ?? ""}
+        {
+            id: "socketPath", 
+            label: "Socket Path", 
+            type: "text", 
+            additionalInstructions:
+				'This is an optional field, when using XYZ this is commonly required.',
+            optional: true,
+            value: credentials.socketPath ?? ""
+        }
     ]
     import GenericForm from './GenericForm.svelte'
 
