b1bd12fc74549a11e106fdbd7829ebab24205d42
Adam <mcrascal@users.noreply.github.com>
2022-04-13 17:20:06 -0400

    enter pre-release mode and add patch change

--- sites/example-project/scripts/clean-package.js
+++ sites/example-project/scripts/clean-package.js
@@ -1,6 +1,6 @@
 // Runs after we package the components. Removes the publish config from the created package.json. 
 // Leaving it in there trips up changesets (https://github.com/changesets/changesets/issues/773) 
-// // 
+// // //
 
 import fs from 'fs-extra'
 
