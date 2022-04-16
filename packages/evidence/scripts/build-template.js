5b447cf0a6c028d06a9b889934a9140c6c50453d
Adam <mcrascal@users.noreply.github.com>
2022-04-15 21:53:42 -0400

    in-line sk config in template

--- packages/evidence/scripts/build-template.js
+++ packages/evidence/scripts/build-template.js
@@ -23,8 +23,37 @@ templatePaths.forEach(p => {
 // Create a clean SK config (workspace's is modified)
 fs.outputFileSync('./template/svelte.config.js', 
     `
-import {config} from '@evidence-dev/evidence'
-export default config
+    import evidencePreprocess from '@evidence-dev/preprocess'
+    import adapter from '@sveltejs/adapter-static';
+    import FullReload from 'vite-plugin-full-reload'
+    
+    /** @type {import('@sveltejs/kit').Config} */
+    
+    const config = {
+        extensions: ['.svelte', ".md"],
+        preprocess: evidencePreprocess(),
+        kit: {
+            adapter: adapter(),
+            files: {
+                routes: 'src/pages',
+                lib: 'src/components'
+            },
+            vite: {
+                optimizeDeps: {
+                    include: ['echarts-stat'],
+                    exclude: ['@evidence-dev/components']
+                },
+                ssr: {
+                    external: ['@evidence-dev/db-orchestrator']
+                },
+                plugins: [
+                    FullReload.default(['./.evidence-queries/extracted/**'], {delay: 150}),
+                ]
+            }
+        }
+    };
+    
+    export default config    
     `
 )
 
--- sites/example-project/svelte.config.js
+++ sites/example-project/svelte.config.js
@@ -1,20 +1,31 @@
-import {config} from '@evidence-dev/evidence'
-import adapter from '@sveltejs/adapter-static';
 import evidencePreprocess from '@evidence-dev/preprocess'
+import adapter from '@sveltejs/adapter-static';
+import FullReload from 'vite-plugin-full-reload'
 
 /** @type {import('@sveltejs/kit').Config} */
-let sconfig = config;
-let newKit = {
+
+const config = {
+	extensions: ['.svelte', ".md"],
+	preprocess: evidencePreprocess(true), // Modify preprocess to allow for loading of $lib instead of package version of components library
+	kit: {
 		adapter: adapter(),
-		package: {
-			dir: '../../packages/components',
-			emitTypes: true
+		files: {
+			routes: 'src/pages',
+			lib: 'src/components'
+		},
+		vite: {
+			optimizeDeps: {
+				include: ['echarts-stat'],
+				exclude: ['@evidence-dev/components']
+			},
+			ssr: {
+				external: ['@evidence-dev/db-orchestrator']
+			},
+			plugins: [
+				FullReload.default(['./.evidence-queries/extracted/**'], {delay: 150}),
+			]
 		}
+	}
 };
 
-sconfig.kit = {...sconfig.kit, ...newKit}
-
-// Modify preprocess to allow for loading of $lib instead of package version of components library
-sconfig.preprocess = evidencePreprocess(true)
-
-export default sconfig;
+export default config;
