c0049521c5e719edf63a23ecb413db35568bb39d
Sean Hughes <sean@evidence.dev>
2022-04-20 15:26:38 -0400

    change location of sqlite db path

--- packages/sqlite/index.cjs
+++ packages/sqlite/index.cjs
@@ -4,10 +4,10 @@ const path = require('path')
 
 const runQuery = async (queryString, database) => {
     const filename = database ? database.filename : process.env["SQLITE_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
-
+    const filepath = "../../" + filename
     try {
         const db = await open({
-            filename: filename,
+            filename: filepath,
             driver: sqlite3.Database,
             mode: sqlite3.OPEN_READONLY,
         })
@@ -16,7 +16,7 @@ const runQuery = async (queryString, database) => {
     } catch(err) {
         if (err.message) {
             if(err.errno === 14){
-                throw "Unable to open " + filename + " in " + path.resolve()
+                throw "Unable to open " + filename + " in " + path.resolve("../../")
             } else {
                 throw err.message
             }
