353aed8692dfa2fcb1021aa9204e1e7da888afe9
Adam <mcrascal@users.noreply.github.com>
2022-05-06 13:04:20 -0400

    other deployment panel

--- sites/example-project/src/components/ui/Deployment/OtherDeploy.svelte
+++ sites/example-project/src/components/ui/Deployment/OtherDeploy.svelte
@@ -1,54 +1,81 @@
 <script>
+    import EnvironmentVarListing from "./EnvironmentVarListing.svelte";
+    import VariableCopy from "./VariableCopy.svelte";
     export let settings
-    let targetEnvVars = []
-
-    if(settings.credentials) {
-        for(const key in settings.credentials) {
-            let envVar = {
-                name: settings.database.toUpperCase() + '_' + key.toUpperCase(),
-                value: settings.credentials[key]
-            }
-            targetEnvVars.push(envVar)
-        }
-    }
-
 </script>
 
-<p>
-    Your Evidence project will run in any environment that runs node.
-</p>
+<h1>Deploying your Project</h1>
 
-<h2>
-    Build command 
-</h2>
-<p>
-    <code>npm run build</code>
-</p>
+<ol>
+    <li>Running <code>npm run build</code> will build a static site in the <code>/build</code> directory which you can host in a variety of environments.</li>
+    <li>You can share your database credentials with your production environment using the environment variables below</li>
+</ol>
 
-<h2>Output directory</h2>
-<p>
-    <code>/build</code>
-</p>
+<div class="separator">Building Your Project</div>
 
-<h2>Environment variables</h2>
-<p>The following environment variables are required for your database connection: </p>
-
-{#each targetEnvVars as envVar }
-<div class=environment-variable>
-    <span class=var-name>{envVar.name}: </span> <span class=var-value>{envVar.value}</span>
+<div class='setting-row'>
+    <span class='setting'>Build command</span>
+    <div class='setting-value'><VariableCopy text={'npm run build'}/></div>
 </div>
-{/each}
+
+<div class='setting-row'>
+    <span class='setting'>Publish directory</span>  
+    <div class='setting-value'><VariableCopy text={'build/'}/> </div>
+    
+</div>
+
+<div class="separator">Environment Variables</div>
+<p>The following environment variables must be present in your deployment environment to enable your database connection</p>
+
+{#if !settings.credentials}
+
+<p>Your project does not have a database connection.</p>
+
+{:else}
+
+<EnvironmentVarListing {settings}/>
+
+{/if}
 
 <style>
-    div.environment-variable{
-        font-family: var(--ui-font-family);
-        color: var(--grey-999);
-        font-size: 16px;
-        margin-top: 1.25em;
-        display:flex;
-        flex-direction: row;
-        align-items: center;
+    span.setting {
+        font-size: 0.85em;
+        color: var(--grey-800);
+        text-transform: uppercase;
+        letter-spacing: 0.05em;
     }
+
+    div.setting-row {
+        margin-top: 1.25em;
+    }
+
+    div.setting-row:first-of-type {
+        margin-top: 0em;
+    }
+
+    div.setting-value {
+        margin-top: 0.25em;
+        width: 45%;
+    }
+
+    .separator {
+        display: flex;
+        align-items: center;
+        text-align: center;
+        margin-block-start: 2.5em;
+        color: var(--grey-700);
+        font-weight:bold;
+    }
+
+    .separator::after {
+        content: '';
+        flex: 1;
+        border-bottom: 1px solid var(--grey-200);
+    }
+
+    .separator:not(:empty)::after {
+        margin-left: 1.5em;
+        margin-top: 0.1em;
+    }
+
 </style>
-
-
