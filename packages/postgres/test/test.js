9538456fd6c91e40a0a6b1e98a3acd8a465e64a1
Udesh Senaratne <usenaratne@gmail.com>
2022-06-05 21:16:41 -0400

    Added type tests for postgres

--- packages/postgres/test/test.js
+++ packages/postgres/test/test.js
@@ -10,11 +10,19 @@ test('query runs', async () => {
         assert.instance(results.rows, Array);
         assert.instance(results.columnTypes, Array);
         assert.type(results.rows[0], "object");
-        assert.equal(results.rows[0].num, 100);
+        assert.equal(results.rows[0].number_col, 100);
 
         let actualColumnTypes = results.columnTypes.map(columnType => columnType.evidenceType);
+        let actualColumnNames = results.columnTypes.map(columnType => columnType.name);
+        let actualTypePrecisions = results.columnTypes.map(columnType => columnType.typeFidelity);
+
         let expectedColumnTypes = ['number', 'date', 'date', 'string', 'boolean'];
+        let expectedColumnNames = ['number_col', 'date_col', 'timestamp_col', 'string_col', 'bool_col'];
+        let expectedTypePrecision = Array(5).fill('precise');
+
         assert.equal(true, (expectedColumnTypes.length === actualColumnTypes.length && expectedColumnTypes.every((value, index) => value === actualColumnTypes[index])));
+        assert.equal(true, (expectedColumnNames.length === actualColumnNames.length && expectedColumnNames.every((value, index) => value === actualColumnNames[index])));
+        assert.equal(true, (expectedTypePrecision.length === actualTypePrecisions.length && expectedTypePrecision.every((value, index) => value === actualTypePrecisions[index])));
     } catch(e) {
         throw Error(e)
     }
