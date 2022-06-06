bd9dae5c42fa55b37196582f9687e5b25212ef06
Udesh Senaratne <usenaratne@gmail.com>
2022-06-05 21:22:57 -0400

    Added BigQuery Tests for column types

--- packages/bigquery/test/test.js
+++ packages/bigquery/test/test.js
@@ -6,11 +6,23 @@ let results;
 
 test('query runs', async () => {
     try{
-        results = await runQuery("select 100 as num");
+        results = await runQuery("select 100 as number_col, current_date  as date_col, current_timestamp as timestamp_col, 'Evidence' as string_col, true as bool_col");
         assert.instance(results.rows, Array);
         assert.instance(results.columnTypes, Array);
-        assert.type(results.rows[0], "object")
-        assert.equal(results.rows[0].num, 100)
+        assert.type(results.rows[0], "object");
+        assert.equal(results.rows[0].number_col, 100);
+
+        let actualColumnTypes = results.columnTypes.map(columnType => columnType.evidenceType);
+        let actualColumnNames = results.columnTypes.map(columnType => columnType.name);
+        let actualTypePrecisions = results.columnTypes.map(columnType => columnType.typeFidelity);
+
+        let expectedColumnTypes = ['number', 'date', 'date', 'string', 'boolean'];
+        let expectedColumnNames = ['number_col', 'date_col', 'timestamp_col', 'string_col', 'bool_col'];
+        let expectedTypePrecision = Array(5).fill('precise');
+
+        assert.equal(true, (expectedColumnTypes.length === actualColumnTypes.length && expectedColumnTypes.every((value, index) => value === actualColumnTypes[index])));
+        assert.equal(true, (expectedColumnNames.length === actualColumnNames.length && expectedColumnNames.every((value, index) => value === actualColumnNames[index])));
+        assert.equal(true, (expectedTypePrecision.length === actualTypePrecisions.length && expectedTypePrecision.every((value, index) => value === actualTypePrecisions[index])));
     } catch(e) {
         throw Error(e)
     }
