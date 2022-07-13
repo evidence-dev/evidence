ac95c0cfd7ba4751d52712b3879cf4298c7c4b24
Udesh Senaratne <udesh@evidence.dev>
2022-07-12 22:26:46 -0400

    Added more tests

---
+++ sites/example-project/src/components/tests/columnUnitSummary.test.js
@@ -0,0 +1,45 @@
+import { getColumnUnitSummary } from "../modules/getColumnExtents";
+
+test("getColumnUnitSummary returns undefined for none numeric types", () => {
+  const data = [
+    { column1: "foo", column2: 3 },
+    { column1: "bar", column2: "value" },
+    { column1: "far", column2: 4 },
+  ];
+  const summary = getColumnUnitSummary(data, "column2");
+  expect(summary).toBeUndefined();
+});
+
+test("getColumnUnitSummary returns correct values with typical example", () => {
+  const data = [
+    { columnOne: "three", columnTwo: 3 },
+    { columnOne: "threpointone", columnTwo: 3.1 },
+    { columnOne: "pi", columnTwo: 3.14159 },
+    { columnOne: "one", columnTwo: 1 },
+    { columnOne: "five", columnTwo: 5 },
+  ];
+  const summary = getColumnUnitSummary(data, "columnTwo");
+  expect(summary).toStrictEqual({
+    min: 1,
+    max: 5,
+    median: 3.1,
+    maxDecimals: 5,
+  });
+});
+
+test("getColumnUnitSummary returns correct values when there are undefined and null elements", () => {
+  const data = [
+    { column1: "foo", column2: 3 },
+    { column1: "bar", column2: null },
+    { column1: "dar", column2: undefined },
+    { column1: "blah", column2: 1 },
+    { column1: "blah", column2: 5 },
+  ];
+  const summary = getColumnUnitSummary(data, "column2");
+  expect(summary).toStrictEqual({
+    min: 1,
+    max: 5,
+    median: 3,
+    maxDecimals: 0,
+  });
+});
