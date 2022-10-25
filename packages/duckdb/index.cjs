2cc93786112ab7f68fc3b1791c526905ed2103b8
Archie Wood <58074498+archiewood@users.noreply.github.com>
2022-10-24 20:50:22 -0400

    unpack error messages

--- packages/duckdb/index.cjs
+++ packages/duckdb/index.cjs
@@ -5,9 +5,18 @@ const { Database } = require('duckdb-async');
 const runQuery = async (queryString, database) => {
     const filename = database ? database.filename : process.env["DUCKDB_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
     const filepath = filename !== ":memory:" ? "../../" + filename : filename
-    const db = await Database.create(filepath);
-    const rows = await db.all(queryString);
-    return processQueryResults(rows);
+    try {
+        const db = await Database.create(filepath);
+        const rows = await db.all(queryString);
+        return processQueryResults(rows);
+    } catch(err) {
+        if (err.message) {
+                throw err.message
+            }
+        else {
+            throw err
+        }
+    }
 }
 
 module.exports = runQuery
