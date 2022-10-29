9b712d0ef4da66beaa85da370eb1abc743ac4532
Josh Wills <josh@weavegrid.com>
2022-10-28 21:08:54 -0700

    Add in READONLY mode logic

--- packages/duckdb/index.cjs
+++ packages/duckdb/index.cjs
@@ -1,12 +1,12 @@
 const path = require('path')
 const { processQueryResults } = require('@evidence-dev/db-commons')
-const { Database } = require('duckdb-async');
+const { Database, OPEN_READONLY } = require('duckdb-async');
 
 const runQuery = async (queryString, database) => {
     const filename = database ? database.filename : process.env["DUCKDB_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
     const filepath = filename !== ":memory:" ? "../../" + filename : filename
     try {
-        const db = await Database.create(filepath);
+        const db = await Database.create(filepath, OPEN_READONLY);
         const rows = await db.all(queryString);
         return processQueryResults(rows);
     } catch(err) {
