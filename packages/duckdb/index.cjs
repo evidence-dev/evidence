5ee2d61364946a29ec39742fa70d3e19b42c73f1
Josh Wills <josh@weavegrid.com>
2022-10-30 14:06:50 -0700

    This will probably make the tests pass

--- packages/duckdb/index.cjs
+++ packages/duckdb/index.cjs
@@ -1,12 +1,14 @@
 const path = require('path')
 const { processQueryResults } = require('@evidence-dev/db-commons')
-const { Database, OPEN_READONLY } = require('duckdb-async');
+const { Database, OPEN_READONLY, OPEN_READWRITE } = require('duckdb-async');
 
 const runQuery = async (queryString, database) => {
-    const filename = database ? database.filename : process.env["DUCKDB_FILENAME"] || process.env["filename"] || process.env["FILENAME"]
-    const filepath = filename !== ":memory:" ? "../../" + filename : filename
+    const filename = database ? database.filename : process.env["DUCKDB_FILENAME"] || process.env["filename"] || process.env["FILENAME"];
+    const filepath = filename !== ":memory:" ? "../../" + filename : filename;
+    const mode = filename !== ":memory:" ? OPEN_READONLY : OPEN_READWRITE;
+
     try {
-        const db = await Database.create(filepath, OPEN_READONLY);
+        const db = await Database.create(filepath, mode);
         const rows = await db.all(queryString);
         return processQueryResults(rows);
     } catch(err) {
