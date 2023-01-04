b59eded0ab17d0b0e6ee47b17c96a027ac5d19ad
Sean Hughes <sean@evidence.dev>
2023-01-04 11:07:54 -0500

    fix search icon color

--- sites/example-project/src/components/viz/SearchBar.svelte
+++ sites/example-project/src/components/viz/SearchBar.svelte
@@ -1,5 +1,5 @@
 <script>
-    // import MdSearch from 'svelte-icons/md/MdSearch.svelte'
+    import MdSearch from 'svelte-icons/md/MdSearch.svelte'
 
     export let placeholder = "Search";
     export let value;
@@ -9,7 +9,7 @@
 <div class=search-container>
 <input class=search-bar type=text placeholder={placeholder} bind:value={value} on:keyup={() => searchFunction(value)}/>
 <div class=search-icon>
-    <svg color="var(--grey-400)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="s-zV66bAK704iV"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path></svg>
+    <MdSearch/>
 </div>
 </div>
 
@@ -36,6 +36,7 @@
         top: 50%;
         transform: translateY(-50%);
         -ms-transform: translateY(-50%);
+        color: var(--grey-400);
     }
 
     .search-bar {
@@ -52,10 +53,6 @@
         font-size: 9pt;
 
         width: calc(100% - 30px);
-    }
-
-    svg {
-        display: block;
     }
 
     *:focus {
