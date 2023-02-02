356fbade8318c58831686a4595c7fb27d656ceed
Nidhi Kala <nidhikala95@gmail.com>
2023-02-02 00:23:16 -0500

    ErrorChart styled with tailwind CSS

--- sites/example-project/src/components/viz/ErrorChart.svelte
+++ sites/example-project/src/components/viz/ErrorChart.svelte
@@ -3,62 +3,13 @@
     export let chartType;
 </script>
 
-<div width=100% class="error">
-    <div class="wrapper">
-        <div class="chart-type">{chartType}</div>
+<div width=100% class="error grid [grid-template-rows:auto] grid-cols-[100%] justify-center bg-red-50 text-grey-700 font-ui font-normal rounded border border-red-100 min-h-[150px] py-5 px-[30px] my-5 print:break-inside-avoid">
+    <div class="m-auto w-full">
+        <div class="font-bold text-center text-[1.2rem]">{chartType}</div>
         {#if chartType.includes("Value")}
-        <div class="value-message">{error}</div>
+        <div class="value-message text-center [word-wrap:break-word] text-[0.8rem] w-[7.8em]">{error}</div>
         {:else}
-        <div class="message">{error}</div>
+        <div class="text-center [word-wrap:break-word] text-[0.8rem]">{error}</div>
         {/if}
     </div>
 </div>
-
-<style>
-    .error {
-        background-color: var(--red-50);
-        font-family: var(--ui-font-family);
-        color: var(--grey-700);
-        font-weight: normal;
-        border-radius: 4px;
-        border: 1px solid var(--red-100);
-        min-height: 150px;
-        padding: 20px 30px 20px 30px;
-        margin-top: 20px;
-        margin-bottom: 20px;
-        display: grid;
-        grid-template-rows: auto;
-        grid-template-columns: 100%;
-        justify-content: center;
-    }
-
-    .wrapper {
-        margin: auto;
-        width: 100%;
-    }
-
-    .chart-type {
-        font-weight: bold;
-        text-align: center;
-        font-size: 1.2rem;
-    }
-
-    .message {
-        text-align: center;
-        word-wrap: break-word;
-        font-size: 0.8rem;
-    }
-
-    .value-message {
-        text-align: center;
-        word-wrap: break-word;
-        font-size: 0.8rem;
-        width: 7.8em;
-    }
-
-    @media print {
-    .error {
-        break-inside: avoid;
-    }
-  }
-</style>
