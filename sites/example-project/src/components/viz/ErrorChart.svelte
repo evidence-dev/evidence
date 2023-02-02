a7126fecd4ee844139e226de2e7442385baa05c5
Nidhi Kala <nidhikala95@gmail.com>
2023-02-02 00:23:16 -0500

    ErrorChart styled with tailwind CSS

--- sites/example-project/src/components/viz/ErrorChart.svelte
+++ sites/example-project/src/components/viz/ErrorChart.svelte
@@ -2,68 +2,25 @@
     import { dev } from '$app/environment';
     export let error;
     export let chartType;
+
+    const MissingCredentialsError = "SQL Error: Missing database credentials"
 </script>
 
-
-<div width=100% class="error">
-    <div class="wrapper">
-        <div class="chart-type">{chartType}</div>
-        <div class={chartType.includes("Value") ? "value-message" : "message"}>
-            {#if dev && error === "SQL Error: Missing database credentials"}
-              {error}.
-              <a class=credentials-link href='/settings'> Add credentials &rarr;</a>
-            {:else}
-              {error}
-            {/if}
-        </div>
+<div width=100% class="error grid [grid-template-rows:auto] grid-cols-[100%] justify-center bg-red-50 text-grey-700 font-ui font-normal rounded border border-red-100 min-h-[150px] py-5 px-[30px] my-5 print:break-inside-avoid">
+    <div class="m-auto w-full">
+        <div class="font-bold text-center text-[1.2rem]">{chartType}</div>
+        <div class="message" class:value-message={chartType.includes("Value")}>{error}</div>
+        {#if dev && error === MissingCredentialsError}
+            <a class=credentials-link href='/settings'> Add credentials &rarr;</a>
+        {/if}
     </div>
 </div>
 
-<style>
-    .error {
-        background-color: var(--red-50);
-        font-family: var(--ui-font-family);
-        color: var(--grey-700);
-        font-weight: normal;
-        border-radius: 4px;
-        border: 1px solid var(--red-100);
-        min-height: 150px;
-        padding: 20px 30px 20px 30px;
-        margin-top: 20px;
-        margin-bottom: 20px;
-        display: grid;
-        grid-template-rows: auto;
-        grid-template-columns: 100%;
-        justify-content: center;
+<style lang="postcss">
+    div.message {
+        @apply text-center [word-wrap:break-work] text-[0.8rem]
+        &.value-message {
+            @apply w-[7.8em];
+        }
     }
-
-    .wrapper {
-        margin: auto;
-        width: 100%;
-    }
-
-    .chart-type {
-        font-weight: bold;
-        text-align: center;
-        font-size: 1.2rem;
-    }
-
-    .message {
-        text-align: center;
-        word-wrap: break-word;
-        font-size: 0.8rem;
-    }
-
-    .value-message {
-        text-align: center;
-        word-wrap: break-word;
-        font-size: 0.8rem;
-        width: 7.8em;
-    }
-
-    @media print {
-    .error {
-        break-inside: avoid;
-    }
-  }
-</style>
+    </style>
