f9d15bc51872716121bdb1523d0a20cb6fa5df80
Nidhi Kala <nidhikala95@gmail.com>
2023-02-02 19:10:37 -0500

    using tailwind font sizing, styled BigValue using tailwind

--- sites/example-project/src/components/viz/BigValue.svelte
+++ sites/example-project/src/components/viz/BigValue.svelte
@@ -85,7 +85,7 @@
 
 
 <div
-    class=container 
+    class="inline-block font-ui py-[.75em] pr-[.75em] pl-0 mr-[0.75em] [margin-block-end:1em] items-center select-none align-top"
     style={`
         min-width: ${minWidth};
         max-width: ${maxWidth};
@@ -94,12 +94,12 @@
     {#if error}
     <ErrorChart chartType="Big Value" error={error.message}/>
     {:else}
-    <p class=title>{title}</p> 
-    <div class=value> 
+    <p class="text-sm font-medium text-grey-700 [text-shadow:1px_solid_white] m-0">{title}</p> 
+    <div class="relative"> 
         <Value {data} column={value}/> 
         {#if sparkline}
             {#if isLinkedChartReady()}
-                <div class='sparkline'>
+                <div class='inline-block'>
                     <svelte:component 
                     this={LinkedChart}
                     data={sparklineData}
@@ -120,66 +120,21 @@
         {/if}
     </div> 
     {#if comparison}
-        <p class=comparison style={`color:${comparisonColor}`}> 
+        <p class="m-0 text-xs font-medium font-ui [font-feature-settings:normal]" style={`color:${comparisonColor}`}> 
             {@html positive ? "&#9650;" : "&#9660;"} 
             <Value {data} column={comparison}/>
-            <span class="comparison-type">{comparisonTitle}</span>
+            <span class="text-grey-700 font-normal">{comparisonTitle}</span>
         </p> 
     {/if}
     {/if}
 </div>  
 
 <style>
+    /* 
+        TODO: Identify if this can be moved to app.css, or scoped to this component.
+        Leaky global styles are problematic
+     */
     :global(.sparkline svg) {
         height: 16px;
     }
-
-    div.sparkline {
-        display: inline-block;
-    }
-
-    div.value {
-        position:relative;
-    }
-
-    div.container {
-        display: inline-block;
-        font-family: var(--ui-font-family);
-        padding: .75em .75em .75em 0; 
-        margin-right: 0.75em;
-        margin-block-end: 1.0em;
-        align-items: center;
-        user-select: none;
-        -webkit-user-select:none ;
-        vertical-align:top;
-    }
-    p {
-        margin: 0;
-    }
-
-    .title {
-        font-size: 0.8em;
-        font-weight: 500;
-        color: var(--grey-700);
-        text-shadow: 1px solid white;
-    }
-
-    .value {
-        font-size: 1.2em;
-        font-weight: 500;
-        color: var(--grey-700);
-    }
-
-    .comparison {
-        font-size: .65em;
-        font-weight: 500;
-        font-family: var(--ui-compact-font-family);
-        font-feature-settings: normal;
-    }
-
-    .comparison-type {
-        color: var(--grey-700);
-        font-weight: normal;
-    }
-
-</style>
+</style> 
--- sites/example-project/src/components/viz/ErrorChart.svelte
+++ sites/example-project/src/components/viz/ErrorChart.svelte
@@ -6,10 +6,10 @@
     const MissingCredentialsError = "SQL Error: Missing database credentials"
 </script>
 
-<div class="grid [grid-template-rows:auto] grid-cols-[100%] justify-center bg-red-50 text-grey-700 font-ui font-normal rounded border border-red-100 min-h-[150px] box-content py-5 px-[30px] my-5 print:break-inside-avoid">
+<div width=100% class="grid [grid-template-rows:auto] box-content grid-cols-[100%] justify-center bg-red-50 text-grey-700 font-ui font-normal rounded border border-red-100 min-h-[150px] py-5 px-[30px] my-5 print:break-inside-avoid">
     <div class="m-auto w-full">
-        <div class="font-bold text-center text-[1.2rem]">{chartType}</div>
-        <div class="text-center [word-wrap:break-work] text-[0.8rem]" class:w-[7.8em]={chartType.includes("Value")}>{error}</div>
+        <div class="font-bold text-center text-lg">{chartType}</div>
+        <div class="text-center [word-wrap:break-work] text-xs" class:w-[7.8em]={chartType.includes("Value")}>{error}</div>
         {#if dev && error === MissingCredentialsError}
             <a class=credentials-link href='/settings'> Add credentials &rarr;</a>
         {/if}
