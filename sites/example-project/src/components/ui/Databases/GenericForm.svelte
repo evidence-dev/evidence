d6e1d3ea2999175206105336620ac42ec0e0e13a
null <wasimthoufiq21@gmail.com>
2023-02-27 18:20:01 +0530

    Extract information to a specific component and add transition

--- sites/example-project/src/components/ui/Databases/GenericForm.svelte
+++ sites/example-project/src/components/ui/Databases/GenericForm.svelte
@@ -1,5 +1,5 @@
 <script>
-    import IoIosHelpCircleOutline from 'svelte-icons/io/IoIosHelpCircleOutline.svelte'
+    import InfoIcon from './InfoIcon.svelte';
 
     export let opts
     export let credentials
@@ -43,10 +43,7 @@
         {opt.label}
 
         {#if opt.additionalInstructions}
-        <span class="additional-info-icon">
-                <IoIosHelpCircleOutline/>
-                <span class=info-msg>{opt.additionalInstructions}</span>
-        </span>
+        <InfoIcon>{opt.additionalInstructions}</InfoIcon>
         {/if}
     </label>
 
@@ -86,10 +83,7 @@
         {opt.label}
 
         {#if opt.additionalInstructions}
-        <span class="additional-info-icon">
-                <IoIosHelpCircleOutline/>
-                <span class=info-msg>{opt.additionalInstructions}</span>
-        </span>
+        <InfoIcon>{opt.additionalInstructions}</InfoIcon>
         {/if}
     </label>
 
@@ -118,18 +112,6 @@
 {/each}
 
 <style>
-
-    span.additional-info-icon {
-        width: 18px;
-        color:var(--grey-600);
-        display:inline-block;
-        vertical-align: middle;
-        line-height: 1em;
-        cursor: help;
-        position:relative;
-        text-transform: none;
-    }
-
     div.input-item{
         font-family: var(--ui-font-family);
         color: var(--grey-999);
@@ -168,32 +150,6 @@
         font-weight: normal;
         font-size: 14px;
         color: var(--grey-800)
-    }
-
-    .additional-info-icon .info-msg {
-        visibility: hidden;
-        display: none;
-        position: absolute;
-        top: -5px;
-        left: 105%;
-        padding-left: 5px;
-        padding-right: 5px;     
-        padding-top: 2px;
-        padding-bottom: 1px;   
-        color: white;
-        font-family: sans-serif;
-        font-size: 0.8em;
-        background-color: var(--grey-900);
-        opacity: 0.85;
-        border-radius: 6px;
-        z-index: 1;
-        max-width: 200px;
-        min-width: 150px;
-    }
-
-    .additional-info-icon:hover .info-msg {
-        visibility: visible;
-        display: inline;
     }
 
     .separator {
