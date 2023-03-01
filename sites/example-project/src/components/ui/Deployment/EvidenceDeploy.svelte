1b28cbde537c501751fad869c383e97ec14c7421
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-03-01 16:02:22 -0500

    add evidence deploy instructions

---
+++ sites/example-project/src/components/ui/Deployment/EvidenceDeploy.svelte
@@ -0,0 +1,39 @@
+<h2>Deploying to Evidence Cloud</h2>
+<p>Evidence's Cloud hosting service allows you to: 
+    <ul>
+        <li>Host your project at <code>{'<'}organisation{'>'}.evidence.app</code></li>
+        <li>Authenticate users</li>
+        <li>Schedule updates to your data</li>
+        <li>Re-build when you push changes to your project</li>
+    </ul>
+<p>Evidence Cloud is in private beta. To join the waitlist, sign up below.</p>
+<div class="new-format-buttons">
+    <button type="button" on:click={() => window.open('https://du3tapwtcbi.typeform.com/to/kwp7ZD3q?utm_source=product&utm_campaign=deploy_panel')}>Join Waitlist</button>
+</div>
+
+
+<style>
+
+  button {
+    margin-right: 0.25em;
+    margin-left: 0.25em;
+    font-style: normal;
+    text-decoration: none;
+    font-size: 14px;
+    cursor: pointer;
+    background-color: var(--blue-600);
+    color:white;
+    font-weight: bold;
+    border-radius: 4px;
+    border: 1px solid var(--blue-700);
+    padding:0.4em 1.10em;
+    transition-property: background, color;
+    transition-duration: 350ms;
+  }
+
+  div.new-format-buttons {
+    display: flex;
+    justify-content: flex-start;
+    padding-top: 0.5em;
+  }
+</style>
