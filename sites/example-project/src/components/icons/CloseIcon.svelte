e6c94b12c739c64c0d9aad85b0d0853ae3e89bae
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-03-14 19:27:25 -0400

    add icons

---
+++ sites/example-project/src/components/icons/CloseIcon.svelte
@@ -0,0 +1,31 @@
+<script>
+  export let color="--grey-900"
+  export let height=24
+  export let width=24
+</script>
+
+<span class=icon-container>
+  <span class=icon style="color: var({color}); height: {height}px; width:{width}px">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><title>Close</title><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M368 368L144 144M368 144L144 368"/></svg>
+          </svg>          
+    </span>
+</span>
+
+<style>
+    .icon {
+      align-items: center;
+      vertical-align: middle;
+    }
+
+    .icon-container {
+      display: inline-flex;
+      align-items: center;
+      vertical-align: middle;
+    }
+
+    svg {
+    margin: auto;
+    display: block;
+    }
+</style>
---
+++ sites/example-project/src/components/icons/KebabIcon.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let color="--grey-900"
+  export let height=24
+  export let width=24
+</script>
+
+<span class=icon-container>
+  <span class=icon style="color: var({color}); height: {height}px; width:{width}px">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <g transform="rotate(90 12 12)">
+            <path fill="currentColor" d="M20 14a2 2 0 1 1-.001-3.999A2 2 0 0 1 20 14ZM6 12a2 2 0 1 1-3.999.001A2 2 0 0 1 6 12Zm8 0a2 2 0 1 1-3.999.001A2 2 0 0 1 14 12Z"/>
+          </g>
+        </svg>
+  </span>
+</span>
+
+<style>
+    .icon {
+      width: 24px;
+      height: 24px;
+      align-items: center;
+    }
+
+    .icon-container {
+      display: inline-flex;
+      align-items: center;
+      vertical-align: middle;
+    }
+
+    svg {
+    margin: auto;
+    display: block;
+    }
+</style>
