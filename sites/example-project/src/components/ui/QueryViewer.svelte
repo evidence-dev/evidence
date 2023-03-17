f7a08956f22d2187b9faacde74f59de5d7770286
Charles <charles@evidence.dev>
2023-03-17 15:40:01 -0400

    LinkedChart Fix + Fix dependencies (#698)
    * Move to svelte:component instead of direct reference, validate that the library is not undefined before using it
    * Add missing dependencies to packages, update lock, add comment on the BigValue/LinkedChart solution
    * Fix vul. in the project
    * Fix the vertical scroll-bar flash
    * Fix the error flash on LinkedChart on import

--- sites/example-project/src/components/ui/QueryViewer.svelte
+++ sites/example-project/src/components/ui/QueryViewer.svelte
@@ -64,7 +64,7 @@
   
  </script>
 
- <div in:blur|local> 
+ <div in:blur|local class="over-container"> 
  {#if $showQueries}
     <!-- Title -->
     <div class="container" transition:slide|local>
@@ -119,17 +119,24 @@
       --scrollbar-minlength: 1.5rem; /* Minimum length of scrollbar thumb (width of horizontal, height of vertical) */
     }
 
+    .over-container {
+      overflow-y: hidden;
+      overflow-x: scroll;
+    }
+
     .code-container {
         background-color: var(--grey-100);
         border-left: 1px solid var(--grey-200);
         border-right: 1px solid var(--grey-200);
-        overflow: auto;
+        overflow-x: auto;
+        overflow-y: hidden;
         padding-top: 0;
         padding-right: 12px;
         padding-bottom: 6px;
         padding-left: 15px;
         scrollbar-width: thin; 
         scrollbar-color: var(--scrollbar-color) var(--scrollbar-track-color);
+        
     }
     .code-container::-webkit-scrollbar {
       height: var(--scrollbar-size);
@@ -163,6 +170,7 @@
         border-right: 1px solid var(--grey-200);
         border-bottom: 1px solid var(--grey-200);
         overflow-x: auto;
+        overflow-y: hidden;
         scrollbar-width: thin; 
         scrollbar-color: var(--scrollbar-color) var(--scrollbar-track-color);
     }
@@ -190,6 +198,7 @@
       min-width: var(--scrollbar-minlength);
       border: 3px solid transparent;
     }
+
     
     .closed {
         border-bottom-left-radius: 6px;
