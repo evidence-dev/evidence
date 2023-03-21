8edee14d2a90b796c1b12fb1141a8608fbf667cd
Brian D <contact@noimbrian.com>
2023-03-21 10:52:05 -0500

    chore: sans-serif font, solid 1px border

--- sites/example-project/src/components/ui/Alert.svelte
+++ sites/example-project/src/components/ui/Alert.svelte
@@ -13,18 +13,18 @@
 
 <style lang="postcss">
   .alert {
-    @apply p-4 mb-4 text-sm rounded text-gray-800 bg-gray-50 font-medium;
+    @apply p-4 mb-4 text-sm rounded text-gray-800 bg-gray-50 font-medium font-sans border;
     &.info {
-      @apply text-blue-800 bg-blue-50;
+      @apply text-blue-800 bg-blue-50 border-blue-800;
     }
     &.danger {
-      @apply text-red-800 bg-red-50;
+      @apply text-red-800 bg-red-50 border-red-800;
     }
     &.success {
-      @apply text-green-800 bg-green-50;
+      @apply text-green-800 bg-green-50 border-green-800;
     }
     &.warning {
-      @apply text-yellow-800 bg-yellow-50;
+      @apply text-yellow-800 bg-yellow-50 border-yellow-800;
     }
   }
 </style>
