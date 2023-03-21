d66fcce1667bce5119aa8a4666b8442df21eddc3
Brian D <contact@noimbrian.com>
2023-03-21 11:05:18 -0500

    chore: un-nest styles

--- sites/example-project/src/components/ui/Alert.svelte
+++ sites/example-project/src/components/ui/Alert.svelte
@@ -19,18 +19,18 @@
 
 <style lang="postcss">
   .alert {
-    &.info {
-      @apply text-blue-800 bg-blue-50 border-blue-800;
-    }
-    &.danger {
-      @apply text-red-800 bg-red-50 border-red-800;
-    }
-    &.success {
-      @apply text-green-800 bg-green-50 border-green-800;
-    }
-    &.warning {
-      @apply text-yellow-800 bg-yellow-50 border-yellow-800;
-    }
     @apply p-4 mb-4 text-sm rounded text-gray-800 bg-gray-50 font-medium font-sans border flex items-center gap-2;
+  }
+  .alert.info {
+    @apply text-blue-800 bg-blue-50 border-blue-800;
+  }
+  .alert.danger {
+    @apply text-red-800 bg-red-50 border-red-800;
+  }
+  .alert.success {
+    @apply text-green-800 bg-green-50 border-green-800;
+  }
+  .alert.warning {
+    @apply text-yellow-800 bg-yellow-50 border-yellow-800;
   }
 </style>
