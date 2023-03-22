7e3a73334ddcd916f24da0f541422040793e9b3f
Brian D <contact@noimbrian.com>
2023-03-22 13:25:06 -0500

    feat: handle more cases of frontmatter

--- packages/preprocess/src/process-frontmatter/process-frontmatter.cjs
+++ packages/preprocess/src/process-frontmatter/process-frontmatter.cjs
@@ -11,12 +11,23 @@ module.exports = () => {
     let handleOgContent
     return {
         markup: async ({content, filename}) => {
-            if (typeof filename !== "undefined" && filename.endsWith("+page.md")) {
-                if (!handleOgContent) handleOgContent = await readFile(`${__dirname}/handle-og.svelte`)
-                return {
-                    code: content + handleOgContent
-                }
+            if (typeof filename === "undefined") return
+            if (!filename.endsWith("+page.md")) return
+            if (!handleOgContent) handleOgContent = await readFile(`${__dirname}/handle-og.svelte`)
+            return {
+                code: content + handleOgContent
             }
         },
+        script: ({content, filename, attributes}) => {
+            if (typeof filename === "undefined") return
+            if (attributes.context !== "module") return
+            if (!filename.endsWith("+page.md")) return
+
+            if (!content.includes("export const metadata =")) {
+                // There is no frontmatter, and we want to make sure that it as at least defined.
+                // Technically this won't _break_ things, just spam the logs with a vite warning.
+                return {code: content + ";const metadata = undefined;"}
+            }
+        }
     }
 }
