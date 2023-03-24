c9e8ad0d1750a3830c52628a68d997380efc7e14
Brian D <contact@noimbrian.com>
2023-03-24 15:09:32 -0500

    chore: remove console.time, add changeset

--- sites/example-project/src/pages/+layout.server.js
+++ sites/example-project/src/pages/+layout.server.js
@@ -4,18 +4,13 @@ export const prerender = true;
 export const trailingSlash = 'always';
 
 export async function load({fetch, route}) {
-    console.info("route", route.id)
     if(route.id && route.id !== "/settings"){
         const routeHash = md5(route.id)
         // ensure that queries have been extracted before initiating the load process 
-        console.time("fetch-status")
         let statusEndpoint = `/api/status${route.id}`.replace(/\/$/, "")
         await fetch(statusEndpoint);
-        console.timeEnd("fetch-status")
-        console.time("fetch-data")
         const res = await fetch(`/api/${routeHash}.json`);
         const {data} = await res.json();
-        console.timeEnd("fetch-data")
     
         const customFormattingSettingsRes = await GET();
         const { customFormattingSettings } = await customFormattingSettingsRes.json();
