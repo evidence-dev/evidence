7ec5a64fd09a64d53a5798772d304b4240087e75
Brian D <contact@noimbrian.com>
2023-04-10 14:24:33 -0500

    feat: accept metadata in query extraction

--- packages/preprocess/src/extract-queries/extract-queries.cjs
+++ packages/preprocess/src/extract-queries/extract-queries.cjs
@@ -2,15 +2,12 @@ const unified = require("unified");
 const remarkParse = require("remark-parse");
 const visit = require("unist-util-visit");
 const fs = require("fs");
-const getPrismLangs = require('../utils/get-prism-langs.cjs')
-const {
-  parseFrontmatter,
-} = require("../frontmatter/parse-frontmatter.cjs");
-const chalk = require('chalk')
+const getPrismLangs = require("../utils/get-prism-langs.cjs");
+const { parseFrontmatter } = require("../frontmatter/parse-frontmatter.cjs");
+const chalk = require("chalk");
 /** @typedef {{id: string, compiledQueryString: string, inputQueryString: string, compiled: boolean, inline: boolean}} Query */
 
-
-const warnedSources = {}
+const warnedSources = {};
 
 // Unified parser step to ignore indented code blocks.
 // Adapted from the mdsvex source, here: https://github.com/pngwn/MDsveX/blob/master/packages/mdsvex/src/parsers/index.ts
@@ -34,71 +31,85 @@ const extractExternalQueries = (content, filename) => {
     console.warn(
       `Malformed frontmatter found in ${filename}. Unable to extract external queries.`
     );
-    return []
+    return [];
   }
 
   /**
-   * 
-   * @param {string} source 
+   *
+   * @param {string} source
    * @returns {boolean}
    */
   const validateSource = (source) => {
     if (!source.endsWith(".sql")) {
       if (!warnedSources[source]) {
-        warnedSources[source] = true
-        console.warn(chalk.bold.red(`! ${source}`) + chalk.gray(" does not appear to be a .sql file, and will not be loaded"))
+        warnedSources[source] = true;
+        console.warn(
+          chalk.bold.red(`! ${source}`) +
+            chalk.gray(
+              " does not appear to be a .sql file, and will not be loaded"
+            )
+        );
       }
-      return false
+      return false;
     }
 
-    return true
-  }
+    return true;
+  };
 
   /**
    * @type Query[]
    */
-  return frontmatter.sources.map((source) => {
-    if (typeof source === "string") {
-      if (!validateSource(source)) return false
-      const id = source.split(".sql")[0].replace("/", "_").replace("\\", "_");
-      try {
-        const content = fs.readFileSync(`./sources/${source}`).toString().trim();
-        return {
-          id: id.toLowerCase(),
-          compiledQueryString: content,
-          inputQueryString: content,
-          compiled: false,
-          inline: false,
-        };  
-      } catch {
-        console.warn(`Failed to load sql file ${source}`)
-      }
-    } else if (typeof source === "object") {
-      const usedKey = Object.keys(source)[0]
+  return frontmatter.sources
+    .map((source) => {
+      if (typeof source === "string") {
+        if (!validateSource(source)) return false;
+        const id = source.split(".sql")[0].replace("/", "_").replace("\\", "_");
+        try {
+          const content = fs
+            .readFileSync(`./sources/${source}`)
+            .toString()
+            .trim();
+          return {
+            id: id.toLowerCase(),
+            compiledQueryString: content,
+            inputQueryString: content,
+            compiled: false,
+            inline: false,
+          };
+        } catch {
+          console.warn(`Failed to load sql file ${source}`);
+        }
+      } else if (typeof source === "object") {
+        const usedKey = Object.keys(source)[0];
 
-      const value = source[usedKey];
-      // Note; this is to be obseleted, as the import syntax evolves, but for now only one key should be used.
-      if (Object.keys(source).length > 1) {
-        console.warn(`Source object has more than one key, this may lead to unintended behavior. Only ${usedKey}: ${value} will be imported.`)
-      }
+        const value = source[usedKey];
+        // Note; this is to be obseleted, as the import syntax evolves, but for now only one key should be used.
+        if (Object.keys(source).length > 1) {
+          console.warn(
+            `Source object has more than one key, this may lead to unintended behavior. Only ${usedKey}: ${value} will be imported.`
+          );
+        }
 
-      if (!validateSource(value)) return false
+        if (!validateSource(value)) return false;
 
-      try {
-        const content = fs.readFileSync(`./sources/${value}`).toString().trim()
-        return {
-          id: usedKey.toLowerCase(),
-          compiledQueryString: content,
-          inputQueryString: content,
-          compiled: false,
-          inline: false,
-        };  
-      } catch {
-        console.warn(`Failed to load sql file ${value}`)
+        try {
+          const content = fs
+            .readFileSync(`./sources/${value}`)
+            .toString()
+            .trim();
+          return {
+            id: usedKey.toLowerCase(),
+            compiledQueryString: content,
+            inputQueryString: content,
+            compiled: false,
+            inline: false,
+          };
+        } catch {
+          console.warn(`Failed to load sql file ${value}`);
+        }
       }
-        
-    }
-  }).filter(Boolean) // filter out queries that returned false;
+    })
+    .filter(Boolean); // filter out queries that returned false;
 };
 
 /**
@@ -108,11 +119,14 @@ const extractExternalQueries = (content, filename) => {
 const extractInlineQueries = (content) => {
   let queries = [];
   let tree = unified().use(remarkParse).use(ignoreIndentedCode).parse(content);
-  const prismLangs = getPrismLangs()
+  const prismLangs = getPrismLangs();
 
   visit(tree, "code", function (node) {
     let id = node.lang ?? "untitled";
-    if (!prismLangs.has(id.toLowerCase()) && id.toLowerCase() !== 'plaintext') {
+    if (id.toLowerCase() === "sql" && node.meta) {
+      id = node.meta;
+    }
+    if (!prismLangs.has(id.toLowerCase()) && id.toLowerCase() !== "plaintext") {
       // Prevent prism code blocks from being interpreted as queries
       let compiledQueryString = node.value.trim(); // refs get compiled and sent to db orchestrator
       let inputQueryString = compiledQueryString; // original, as written
@@ -122,7 +136,7 @@ const extractInlineQueries = (content) => {
         compiledQueryString,
         inputQueryString,
         compiled,
-        inline: true
+        inline: true,
       });
     }
   });
@@ -142,8 +156,8 @@ const extractQueries = (content) => {
 };
 
 /**
- * 
- * @param {string} content File Content 
+ *
+ * @param {string} content File Content
  * @returns {string[]}
  */
 const getQueryIds = (content) => {
