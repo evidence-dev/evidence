9b00dade97371308276d0b69d17c7cf36fcf6808
Brian D <contact@noimbrian.com>
2023-04-11 23:12:34 -0500

    fix: nullish coalesce

--- packages/preprocess/src/utils/highlighter.cjs
+++ packages/preprocess/src/utils/highlighter.cjs
@@ -1,13 +1,13 @@
 const getPrismLangs = require('./get-prism-langs.cjs')
 
-function highlighter(code, lang = "") {
+function highlighter(code, lang) {
     code = code.replace(/'/g, "&apos;");
     code = code.replace(/"/g, "&quot;");
 
     // Replace curly braces or Svelte will try to evaluate as a JS expression
     code = code.replace(/{/g, "&lbrace;").replace(/}/g,"&rbrace;");
     // Ensure that "real" code blocks are rendered not run as queries
-    if (getPrismLangs().has(lang.toLowerCase())) {
+    if (getPrismLangs().has(lang?.toLowerCase() ?? "")) {
         return `<CodeBlock source="${code}" copyToClipboard=true></CodeBlock>`;
     }
     return `
