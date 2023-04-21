475fa6793ed00f9f5854e7b66630e591fa2b2489
Winterhart <charles@evidence.dev>
2023-04-21 15:01:06 -0400

    100% height/width

--- sites/example-project/src/components/ui/Deployment/CopyIcon.svelte
+++ sites/example-project/src/components/ui/Deployment/CopyIcon.svelte
@@ -12,6 +12,8 @@
 	xmlns="http://www.w3.org/2000/svg"
 	viewBox="0 0 32 32"
 	fill="currentColor"
+	width="100%"
+	height="100%"
 	preserveAspectRatio="xMidYMid meet"
 	{...attributes}
 	{...$$restProps}
