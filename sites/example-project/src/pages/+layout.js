1f3e3217f91f4072b6aeed9b3c124c1c8ac3d5e5
csjh <61282104+csjh@users.noreply.github.com>
2023-05-29 19:05:41 -0400

    slightly less hacky version

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,19 +1,10 @@
-import { browser } from '$app/environment';
-
-async function getDataOnServer(route) {
-	return (await import('./api/[route].json/+server.js')).GET({ params: { route } });
-}
-
 export const load = async ({ fetch, route, data: { customFormattingSettings, routeHash } }) => {
 	if (route.id && route.id !== '/settings') {
-        if (!browser) await fetch(`/api/${routeHash}.json`);
+		const res = await fetch(`/api/${routeHash}.json`);
+        // sveltekit inlines json but not arraybuffers, do not change
+		const buffer = await res.arrayBuffer();
+        const { data } = JSON.parse(new TextDecoder().decode(buffer));
 
-		// not using the load fetch so the data is not inlined in the html
-		const res = await (browser
-			? window.fetch(`/api/${routeHash}.json`)
-			: getDataOnServer(routeHash));
-
-		const { data } = await res.json();
 		return {
 			data,
 			customFormattingSettings
