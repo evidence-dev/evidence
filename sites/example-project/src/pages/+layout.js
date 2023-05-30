c0750cbaccec500d32c5a5e54ad348113d82bb7b
csjh <61282104+csjh@users.noreply.github.com>
2023-05-30 10:10:37 -0400

    fixed load function not working on /settings

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,5 +1,6 @@
-export const load = async ({ fetch, route, data: { customFormattingSettings, routeHash } }) => {
+export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
+        const { customFormattingSettings, routeHash } = parentData;
 		const res = await fetch(`/api/${routeHash}.json`);
         // has to be cloned to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
 		const { data } = await res.clone().json();
