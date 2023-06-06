d7c8a81622fccb9e82481549e6e93aaf02b96d9e
csjh <61282104+csjh@users.noreply.github.com>
2023-06-06 17:49:31 -0400

    formatting

--- packages/core-components/src/lib/unsorted/ui/Databases/BigqueryForm.svelte
+++ packages/core-components/src/lib/unsorted/ui/Databases/BigqueryForm.svelte
@@ -96,9 +96,9 @@
 	<!-- gcloud -->
 	<div class="mt-5">
 		<p>
-			If you have the <a target="_blank" href="https://cloud.google.com/sdk/gcloud">gcloud CLI</a> installed, you can
-			log in to BigQuery using the following command. Evidence will use the credentials stored by the
-			gcloud CLI to connect to BigQuery.
+			If you have the <a target="_blank" href="https://cloud.google.com/sdk/gcloud">gcloud CLI</a> installed,
+			you can log in to BigQuery using the following command. Evidence will use the credentials stored
+			by the gcloud CLI to connect to BigQuery.
 		</p>
 		<pre><code class="block p-1">gcloud auth application-default login</code></pre>
 	</div>
