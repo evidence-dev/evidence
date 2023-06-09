44c0c4ca043c9cf0fbe2ba83a2c621f77c557684
csjh <61282104+csjh@users.noreply.github.com>
2023-06-09 15:34:29 -0400

    fix bigquery default connection & other places (#938)
    fix bigquery default connection & other places

--- packages/core-components/src/lib/unsorted/ui/Deployment/EnvironmentVarListing.svelte
+++ packages/core-components/src/lib/unsorted/ui/Deployment/EnvironmentVarListing.svelte
@@ -17,11 +17,58 @@
 		];
 		credentials = settings.credentials;
 		if (settings.database == 'bigquery') {
-			credentials = {
-				project_id: settings.credentials.project_id,
-				client_email: settings.credentials.client_email,
-				private_key: settings.credentials.private_key
-			};
+			if (credentials.authenticator === 'service-account') {
+				credentials = {
+					project_id: settings.credentials.project_id,
+					client_email: settings.credentials.client_email,
+					private_key: settings.credentials.private_key
+				};
+			} else if (credentials.authenticator === 'oauth') {
+				credentials = {
+					project_id: settings.credentials.project_id,
+					token: settings.credentials.token
+				};
+			} else if (credentials.authenticator === 'gcloud-cli') {
+				credentials = {
+					project_id: settings.credentials.project_id
+				};
+			}
+		}
+		if (settings.database == 'snowflake') {
+			if (credentials.authenticator === 'externalbrowser') {
+				credentials = {
+					account: settings.credentials.account,
+					username: settings.credentials.username,
+					warehouse: settings.credentials.warehouse,
+					database: settings.credentials.database
+				};
+			} else if (credentials.authenticator === 'okta') {
+				credentials = {
+					okta_url: settings.credentials.okta_url,
+					account: settings.credentials.account,
+					username: settings.credentials.username,
+					password: settings.credentials.password,
+					warehouse: settings.credentials.warehouse,
+					database: settings.credentials.database
+				};
+			} else if (credentials.authenticator === 'snowflake_jwt') {
+				credentials = {
+					account: settings.credentials.account,
+					username: settings.credentials.username,
+					private_key: settings.credentials.private_key,
+					passphrase: settings.credentials.passphrase,
+					warehouse: settings.credentials.warehouse,
+					database: settings.credentials.database
+				};
+			} else {
+				credentials = {
+					account: settings.credentials.account,
+					username: settings.credentials.username,
+					password: settings.credentials.password,
+					warehouse: settings.credentials.warehouse,
+					database: settings.credentials.database
+				};
+			}
 		}
 		for (const key in credentials) {
 			if (key != 'gitignoreSqlite') {
