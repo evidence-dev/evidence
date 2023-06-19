963d060ccc3a8c73eb2c9bfc065d0c0be4123019
csjh <61282104+csjh@users.noreply.github.com>
2023-06-19 16:31:00 -0400

    removed redundant tailwind config lines that broke HMR (#961)
    removed redundant tailwind config lines that broke HMR

--- sites/example-project/tailwind.config.cjs
+++ sites/example-project/tailwind.config.cjs
@@ -6,8 +6,6 @@ module.exports = {
 		relative: true,
 		files: [
 			'./src/**/*.{html,js,svelte,ts,md}', // This is used for everything in base evidence template
-			'../../pages/**/*.{html,js,svelte,ts,md}', // This is used in end user projects to let them access tailwind classes
-			'../../src/**/*.{html,js,svelte,ts,md}', // This is used in end user projects to let them access tailwind classes
 			'./node_modules/@evidence-dev/core-components/dist/**/*.{html,js,svelte,ts,md}',
 			'../../node_modules/@evidence-dev/core-components/dist/**/*.{html,js,svelte,ts,md}'
 		]
