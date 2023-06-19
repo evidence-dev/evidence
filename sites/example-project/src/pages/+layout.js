a334307e43e0b1660fea1350052a488dfc8e7ca7
csjh <61282104+csjh@users.noreply.github.com>
2023-06-19 10:17:57 -0400

    formatting

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,8 +1,8 @@
 export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
-        const { customFormattingSettings, routeHash } = parentData;
+		const { customFormattingSettings, routeHash } = parentData;
 		const res = await fetch(`/api/${routeHash}.json`);
-        // has to be cloned to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
+		// has to be cloned to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
 		const { data } = await res.clone().json();
 
 		return {
