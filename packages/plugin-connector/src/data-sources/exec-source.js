ce0457588cd175347c764f774157bf1712d297f5
Brian D <contact@noimbrian.com>
2023-06-27 10:37:00 -0500

    feat: support dynamic output directory for parquet files

--- packages/plugin-connector/src/data-sources/exec-source.js
+++ packages/plugin-connector/src/data-sources/exec-source.js
@@ -1,12 +1,14 @@
 import { buildParquetFromResultSet } from '@evidence-dev/universal-sql';
 import fs from 'fs/promises';
+import path from "path";
 /**
  *
  * @param {DatasourceSpec} source
  * @param {PluginDatabases} supportedDbs
+ * @param {string} outDir
  * @returns {Promise<void>}
  */
-export const execSource = async (source, supportedDbs) => {
+export const execSource = async (source, supportedDbs, outDir) => {
 	if (!(source.type in supportedDbs)) {
 		// TODO: Make this error message better
 		throw new Error(`Unsupported database type: ${source.type}`);
@@ -27,6 +29,10 @@ export const execSource = async (source, supportedDbs) => {
 		const { result } = query;
 		if (!result) continue;
 		const parquetBuffer = await buildParquetFromResultSet(result.columnTypes, result.rows);
-		await fs.writeFile(query.filepath.split('.')[0] + '.parquet', parquetBuffer);
+		const fileparts = query.filepath.split('/')
+		const outputFilename = fileparts.pop()?.split(".")[0]  + '.parquet'
+		const outputSubdir = fileparts.join("/").split("sources").slice(1).join("/")
+		await fs.mkdir(path.join(outDir, outputSubdir), { recursive: true })
+		await fs.writeFile(path.join(outDir, outputSubdir, outputFilename), parquetBuffer);
 	}
 };
