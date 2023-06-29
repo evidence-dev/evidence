1a29dc91969aed0b949f7efaaf74860b42ac5b69
csjh <61282104+csjh@users.noreply.github.com>
2023-06-29 16:44:55 -0400

    fix up getColumnSummary and getColumnExtents

--- packages/component-utilities/src/getColumnSummary.js
+++ packages/component-utilities/src/getColumnSummary.js
@@ -1,25 +1,46 @@
 import getColumnEvidenceType from './getColumnEvidenceType.js';
-import { getColumnExtentsLegacy, getColumnUnitSummary } from './getColumnExtents.js';
-import { lookupColumnFormat } from './formatting';
+import { getColumnUnitSummary } from './getColumnExtents.js';
+import { lookupColumnFormat } from './formatting.js';
 import formatTitle from './formatTitle.js';
 
+/**
+ * @typedef {Object} ColumnSummary
+ * @property {string} title
+ * @property {string} type
+ * @property {Object} evidenceColumnType
+ * @property {ReturnType<typeof lookupColumnFormat>} format
+ * @property {ReturnType<typeof getColumnUnitSummary>} columnUnitSummary
+ */
+
+/**
+ * @function
+ * @template T
+ * @param {Record<string, unknown>[]} data 
+ * @param {T} returnType 
+ * @returns {T extends 'object' ? Record<string, ColumnSummary> : (ColumnSummary & { id: string })[]}
+ */
 export default function getColumnSummary(data, returnType = 'object') {
+    /** @type {Record<string, ColumnSummary>} */
 	const columnSummary = {};
 
 	for (const colName of Object.keys(data[0])) {
 		const evidenceColumnType = getColumnEvidenceType(data, colName);
 		const type = evidenceColumnType.evidenceType;
-		const columnUnitSummary = getColumnUnitSummary(data, colName);
+		const columnUnitSummary =
+			evidenceColumnType.evidenceType === 'number'
+				? getColumnUnitSummary(data, colName)
+				: {
+						maxDecimals: 0,
+						unitType: evidenceColumnType.evidenceType
+				  };
 		const format = lookupColumnFormat(colName, evidenceColumnType, columnUnitSummary);
-		const extentsLegacy = getColumnExtentsLegacy(data, colName);
 
 		columnSummary[colName] = {
 			title: formatTitle(colName, format),
 			type,
 			evidenceColumnType,
 			format,
-			columnUnitSummary,
-			extentsLegacy
+			columnUnitSummary
 		};
 	}
 
