0ec435870ce69ddf6729171a48d3d8984bd6b765
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-08-02 09:54:01 -0400

    format

--- packages/core-components/src/lib/unsorted/ui/Details.svelte
+++ packages/core-components/src/lib/unsorted/ui/Details.svelte
@@ -5,49 +5,39 @@
 <script>
 	export let title = 'Details';
 	export let open = false;
-    import { slide } from 'svelte/transition';
+	import { slide } from 'svelte/transition';
 </script>
 
 <div class="mb-4 mt-3 ml-1">
+	<button class="italic text-base text-grey-800 cursor-pointer" on:click={() => (open = !open)}>
+		<span class={open ? 'marker rotate-marker' : 'marker'} />
+		{title}
+	</button>
 
-<button 
-    class="italic text-base text-grey-800 cursor-pointer" 
-    on:click={() => (open = !open)}>
-    <span 
-        class={open ? "marker rotate-marker" : "marker"} />
-        {title}
-</button>
-
-{#if open}
-    <div 
-        class="ml-3 pt-3 mb-6 text-base"
-        transition:slide
-    >
-        <slot />
-    </div>
-{/if}
-
+	{#if open}
+		<div class="ml-3 pt-3 mb-6 text-base" transition:slide>
+			<slot />
+		</div>
+	{/if}
 </div>
 
 <style>
-    .marker {
-        
-        border-left: 5px solid transparent;
-        border-right: 5px solid transparent;
-        border-top: 9px solid var(--grey-800);
-        margin-right: 8px;
-        transform: rotate(-90deg);
-        transition: transform 0.2s ease;
-    }
+	.marker {
+		border-left: 5px solid transparent;
+		border-right: 5px solid transparent;
+		border-top: 9px solid var(--grey-800);
+		margin-right: 8px;
+		transform: rotate(-90deg);
+		transition: transform 0.2s ease;
+	}
 
-    .rotate-marker {
-        transform: rotate(0deg);
-    }
+	.rotate-marker {
+		transform: rotate(0deg);
+	}
 
-    button {
-        display: flex;
-        align-items: center;
-        cursor: pointer;
-        
-    }
+	button {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+	}
 </style>
