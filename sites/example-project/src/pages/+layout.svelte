27f989b1962e2114490e995734c6829ec3d35b9e
Adam <mcrascal@users.noreply.github.com>
2023-08-06 13:02:25 -0400

    ensure chrome text select on article

--- sites/example-project/src/pages/+layout.svelte
+++ sites/example-project/src/pages/+layout.svelte
@@ -99,7 +99,7 @@
 					</div>
 					<span class="px-1 text-xs text-gray-500">Updated 7 hours ago</span>
 				</div> -->
-				<article class="select-auto">
+				<article class="select-text">
 					<slot />
 				</article>
 			{:else}
