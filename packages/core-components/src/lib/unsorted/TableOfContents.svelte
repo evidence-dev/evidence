737ea427f78dfca03cc147abf13d0119a5bb37e3
Adam <mcrascal@users.noreply.github.com>
2023-08-16 18:41:29 -0400

    nudge up toc padding

--- packages/core-components/src/lib/unsorted/TableOfContents.svelte
+++ packages/core-components/src/lib/unsorted/TableOfContents.svelte
@@ -5,7 +5,7 @@
 
 <aside class="hidden lg:block w-48">
 	{#if !$navigating}
-		<div class="fixed w-48 top-20 bottom-20 px-3 overflow-auto pretty-scrollbar">
+		<div class="fixed w-48 top-20 bottom-20 pl-4 pr-3 overflow-auto pretty-scrollbar">
 			<ContentsList />
 		</div>
 	{/if}
