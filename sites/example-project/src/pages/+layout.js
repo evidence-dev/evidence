14d138feca07f29c8aa39e9058be58acb990adab
Brian D <contact@noimbrian.com>
2023-08-17 12:34:16 -0500

    chore: throw error when sources have not been built

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -21,6 +21,10 @@ const initDb = async () => {
 		if (res.ok) ({ renderedFiles } = await res.json());
 	}
 
+	if (!renderedFiles) {
+		throw new Error("Unable to load source manifest. Do you need to run build:sources?")
+	}
+
 	await initDB();
 	await setParquetURLs(renderedFiles);
 	await updateSearchPath(Object.keys(renderedFiles));
