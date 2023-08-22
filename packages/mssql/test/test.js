83af02bd145afcf70824fef7e1f2866ee643fe4c
csjh <61282104+csjh@users.noreply.github.com>
2023-08-22 15:51:37 -0400

    add mssql tests

--- packages/mssql/test/test.js
+++ packages/mssql/test/test.js
@@ -1,34 +1,113 @@
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 import runQuery from '../index.cjs';
-import { TypeFidelity } from '@evidence-dev/db-commons';
 import 'dotenv/config';
 
 test('query runs', async () => {
-	if (process.env.MYSQL_DATABASE) {
+	if (process.env.MSSQL_DATABASE) {
 		try {
-			const results = await runQuery(
-				"select 100 as number_col, GETDATE() as date_col, current_timestamp as timestamp_col, 'Evidence' as string_col, CAST(0 AS BIT) as bool_col"
-			);
+			const results = await runQuery(`
+			SELECT
+				CAST(1 AS bigint) as bigint_col,
+				CAST(1 AS numeric) as numeric_col,
+				CAST(1 AS bit) as bit_col,
+				CAST(1 AS smallint) as smallint_col,
+				CAST(1 AS decimal) as decimal_col,
+				CAST(1 AS smallmoney) as smallmoney_col,
+				CAST(1 AS int) as int_col,
+				CAST(1 AS tinyint) as tinyint_col,
+				CAST(1 AS money) as money_col,
+				CAST(1.5 AS float) AS float_col,
+				CAST(1.5 AS real) AS real_col,
+				CAST('1909-01-01' AS date) AS date_col,
+				CAST('1909-01-01' AS datetimeoffset) AS datetimeoffset_col,
+				CAST('1909-01-01' AS datetime2) AS datetime2_col,
+				CAST('1909-01-01' AS smalldatetime) AS smalldatetime_col,
+				CAST('1909-01-01' AS datetime) AS datetime_col,
+				CAST('10:00:00' AS time) as time_col,
+				CAST('a' AS char) as char_col,
+				CAST('ahhhh' AS varchar) as varchar_col,
+				CAST('ahhh' AS text) AS text_col,
+				CAST('h' AS nchar) AS nchar_col,
+				CAST('hhhaaa' AS nvarchar) AS nvarchar_col,
+				CAST('haaa' AS ntext) AS ntext_col,
+				CAST('1010101' AS binary) as binary_col,
+				CAST('1010101' AS varbinary) as varbinary_col,
+				CAST('1010101' AS image) as image_col,
+				CAST('6F9619FF-8B86-D011-B42D-00C04FC964FF' as uniqueidentifier) as uniqueidentifier_col,
+				CAST(1.5 AS sql_variant) as sql_variant_col,
+				CAST('<html></html>' AS xml) as xml_col
+			`);
+
 			assert.instance(results.rows, Array);
 			assert.instance(results.columnTypes, Array);
 			assert.type(results.rows[0], 'object');
-			assert.equal(results.rows[0].number_col, 100);
-			assert.equal(results.rows[0].bool_col, false);
 
 			const actualColumnTypes = results.columnTypes.map((columnType) => columnType.evidenceType);
 			const actualColumnNames = results.columnTypes.map((columnType) => columnType.name);
-			const actualTypePrecisions = results.columnTypes.map((columnType) => columnType.typeFidelity);
 
-			const expectedColumnTypes = ['number', 'date', 'date', 'string', 'boolean'];
-			const expectedColumnNames = [
-				'number_col',
-				'date_col',
-				'timestamp_col',
-				'string_col',
-				'bool_col'
+			const expectedColumnTypes = [
+				'number',
+				'number',
+				'boolean',
+				'number',
+				'number',
+				'number',
+				'number',
+				'number',
+				'number',
+				'number',
+				'number',
+				'date',
+				'date',
+				'date',
+				'date',
+				'date',
+				'date',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string',
+				'string'
 			];
-			const expectedTypePrecision = Array(5).fill(TypeFidelity.PRECISE);
+			const expectedColumnNames = [
+				'bigint_col',
+				'numeric_col',
+				'bit_col',
+				'smallint_col',
+				'decimal_col',
+				'smallmoney_col',
+				'int_col',
+				'tinyint_col',
+				'money_col',
+				'float_col',
+				'real_col',
+				'date_col',
+				'datetimeoffset_col',
+				'datetime2_col',
+				'smalldatetime_col',
+				'datetime_col',
+				'time_col',
+				'char_col',
+				'varchar_col',
+				'text_col',
+				'nchar_col',
+				'nvarchar_col',
+				'ntext_col',
+				'binary_col',
+				'varbinary_col',
+				'image_col',
+				'uniqueidentifier_col',
+				'sql_variant_col',
+				'xml_col'
+			];
 
 			assert.equal(
 				true,
@@ -40,10 +119,14 @@ test('query runs', async () => {
 				expectedColumnNames.length === actualColumnNames.length &&
 					expectedColumnNames.every((value, index) => value === actualColumnNames[index])
 			);
+
+			const rows = Object.values(results.rows[0]);
 			assert.equal(
 				true,
-				expectedTypePrecision.length === actualTypePrecisions.length &&
-					expectedTypePrecision.every((value, index) => value === actualTypePrecisions[index])
+				expectedColumnTypes.length === actualColumnTypes.length &&
+					expectedColumnTypes.every((value, index) =>
+						value === 'date' ? rows[index] instanceof Date : typeof rows[index] === value
+					)
 			);
 		} catch (e) {
 			throw Error(e);
