a9112a053f03fc7329e1e41d4c2777d2c9800779
csjh <61282104+csjh@users.noreply.github.com>
2023-08-28 13:30:06 -0400

    Update packages/core-components/src/lib/atoms/InvisibleLinks.svelte
    add explanation for `building` condition

--- packages/core-components/src/lib/atoms/InvisibleLinks.svelte
+++ packages/core-components/src/lib/atoms/InvisibleLinks.svelte
@@ -7,6 +7,7 @@
 	export let link;
 </script>
 
+<!-- we only care about links being crawled during build time -->
 {#if building}
 	{#each Array.from(new Set(data.map((row) => row[link]))) as href}
 		<a {href} class="hidden" aria-hidden="true">{''}</a>
