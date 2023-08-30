f2c957decb3057985efc6360ed28e973a2b2eaf4
Brian D <contact@noimbrian.com>
2023-08-30 12:22:29 -0500

    feat: improve error pages

---
+++ sites/example-project/src/pages/break/+page.svelte
@@ -0,0 +1,3 @@
+<script>
+	throw new Error("I should be able to access this message", { cause: new Error("I should see the cause of the first level!", {cause: new Error("I should see the cause of the second level")}) } )
+</script>
