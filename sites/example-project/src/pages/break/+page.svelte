767355874986cc69e8960d0d5ecd5dd572790a3a
Brian D <contact@noimbrian.com>
2023-08-30 16:58:17 -0500

    chore: remove break page

--- sites/example-project/src/pages/break/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	throw new Error('I should be able to access this message', {
-		cause: new Error('I should see the cause of the first level!', {
-			cause: new Error('I should see the cause of the second level')
-		})
-	});
-</script>
