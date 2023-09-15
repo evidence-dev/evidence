a47ce9f4111ecfb460b3c491f8b1d2402a3ec6ef
Brian D <contact@noimbrian.com>
2023-09-15 11:44:46 -0500

    chore: use evidence column types

--- packages/core-components/src/lib/unsorted/viz/BarChart.svelte
+++ packages/core-components/src/lib/unsorted/viz/BarChart.svelte
@@ -31,7 +31,9 @@
 	export let yMin = undefined;
 	export let yMax = undefined;
 	export let swapXY = false;
-	export let showAllXAxisLabels = xType === 'category' || typeof data?.[0]?.[x] === "string";
+
+	export let showAllXAxisLabels = xType === 'category' || data?.[0]?._evidenceColumnTypes[x]?.evidenceType === "string";
+
 	$: {
 		if (swapXY === 'true' || swapXY === true) {
 			swapXY = true;
