439dfb9e7e89870f74e16eb007ca083344040817
Brian D <contact@noimbrian.com>
2023-09-25 16:22:24 -0500

    chore: disable label trunc on swapped charts

--- packages/component-utilities/src/echarts.js
+++ packages/component-utilities/src/echarts.js
@@ -465,17 +465,25 @@ export default (node, option) => {
 			const modConst = 4 / 5;
 			const clientWidth = node?.clientWidth ?? 0;
 
-			/** @type {import("echarts").EChartsOption} */
-			const newOption = {
-				xAxis: {
-					axisLabel: {
-						interval: 0,
-						overflow: 'truncate',
-						width: (clientWidth * modConst) / distinctXValues.size
+
+			// We disable this behavior because it doesn't make sense on horizontal bar charts
+			// Category labels will grow to be visible
+			// Value labels are interpolatable anyway
+			if (!options.swapXY) {
+				/** @type {import("echarts").EChartsOption} */
+				const newOption =  {
+					xAxis: {
+						axisLabel: {
+							interval: 0,
+							overflow: 'truncate',
+							width: (clientWidth * modConst) / distinctXValues.size
+						}
 					}
-				}
-			};
-			chart.setOption(newOption);
+				};
+				chart.setOption(newOption);
+			}
+
+
 		}
 	};
 
