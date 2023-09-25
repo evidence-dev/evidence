1eaff8f4ed54d1ddb2f3597cb211abbc30f8a79c
Adam <mcrascal@users.noreply.github.com>
2023-09-25 18:21:16 -0400

    prevent linebreaks in breadcrumbs

--- packages/core-components/src/lib/unsorted/ui/BreadCrumbs.svelte
+++ packages/core-components/src/lib/unsorted/ui/BreadCrumbs.svelte
@@ -55,8 +55,8 @@
 	$: crumbs = buildCrumbs(pathArray);
 </script>
 
-<div class="flex items-start mt-8 sm:mt-12">
-	<div class="inline-flex items-center text-sm capitalize gap-1 text-gray-500 mb-2 sm:mb-4">
+<div class="flex items-start mt-8 sm:mt-12 whitespace-nowrap overflow-auto">
+	<div class="inline-flex items-center text-sm capitalize gap-1 text-gray-500 mb-2 sm:mb-4 ">
 		{#each crumbs as crumb, i}
 			{#if i > 0}
 				<Icon src={ChevronRight} size="12px" theme="solid" />
