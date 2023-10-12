235959b5ad6e3585e19a2e0ad996928fcec64c08
csjh <61282104+csjh@users.noreply.github.com>
2023-10-12 18:50:57 -0400

    add link to evidence environment variables

--- packages/core-components/src/lib/unsorted/ui/QueryViewer.svelte
+++ packages/core-components/src/lib/unsorted/ui/QueryViewer.svelte
@@ -108,6 +108,10 @@
 					{error.message}
 					{#if dev && error.message === 'Missing database connection.'}
 						<a class="credentials-link" href="/settings"> Add credentials &rarr;</a>
+					{:else if !dev && error.message === 'Missing database connection; set the EVIDENCE_DATABASE environment variable.'}
+						<a class="credentials-link" href="https://docs.evidence.dev/cli/#environment-variables"
+							>View environment variables &rarr;</a
+						>
 					{/if}
 				{:else if nRecords > 0}
 					<ChevronToggle toggled={$showResults} color="#3488e9" />
