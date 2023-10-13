17e2b444a1872e21ac01e8b976edabdbbffcccc0
csjh <61282104+csjh@users.noreply.github.com>
2023-10-13 16:24:39 -0400

    use old unnamed query behavior

--- packages/preprocess/src/utils/highlighter.cjs
+++ packages/preprocess/src/utils/highlighter.cjs
@@ -13,11 +13,8 @@ function highlighter(code, lang, meta) {
 
 	// Replace curly braces or Svelte will try to evaluate as a JS expression
 	code = code.replace(/{/g, '&lbrace;').replace(/}/g, '&rbrace;');
-	if (
-		lang &&
-		((lang.toLowerCase() === 'sql' && meta) || !getPrismLangs().has(lang.toLowerCase() ?? ''))
-	) {
-		const queryId = lang.toLowerCase() === 'sql' && meta ? meta : lang;
+	if ((lang?.toLowerCase() === 'sql' && meta) || !getPrismLangs().has(lang?.toLowerCase() ?? '')) {
+		const queryId = lang?.toLowerCase() === 'sql' && meta ? meta : lang;
 		return `
         {#if data.${queryId} }
             <QueryViewer pageQueries = {data.evidencemeta.queries} queryID = "${
@@ -26,13 +23,6 @@ function highlighter(code, lang, meta) {
         {/if}
         `;
 	}
-
-	if (lang == null) {
-		console.error(
-			`Missing language for code block. If it was intended to be a query, please add a name. If it was intended to be a code snippet, please add a language.\nFound code:\n${code}\n`
-		);
-	}
-
 	// Ensure that "real" code blocks are rendered not run as queries
 	return `<CodeBlock source="${code}" copyToClipboard=true></CodeBlock>`;
 }
