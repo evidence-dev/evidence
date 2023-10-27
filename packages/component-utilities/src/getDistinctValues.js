f2c9328990a09c83b3aa412337b90ef4411e006d
csjh <61282104+csjh@users.noreply.github.com>
2023-10-27 12:47:24 -0400

    clean up getDistinctValues

--- packages/component-utilities/src/getDistinctValues.js
+++ packages/component-utilities/src/getDistinctValues.js
@@ -5,9 +5,5 @@
  * @returns {import("./types.js").EvidenceTypeUnion[]}
  */
 export default function getDistinctValues(data, column) {
-	const distinctValueSet = new Set();
-	data.forEach((d) => {
-		distinctValueSet.add(d[column]);
-	});
-	return Array.from(distinctValueSet);
+	return Array.from(new Set(data.map((d) => d[column])));
 }
