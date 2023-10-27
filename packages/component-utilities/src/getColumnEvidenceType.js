e98ad96325ae687300dd0d98e92a6ceecc0c58a1
csjh <61282104+csjh@users.noreply.github.com>
2023-10-27 15:17:58 -0400

    trim fat from getColumnEvidenceType

--- packages/component-utilities/src/getColumnEvidenceType.js
+++ packages/component-utilities/src/getColumnEvidenceType.js
@@ -1,26 +1,19 @@
-import inferColumnTypes from './inferColumnTypes';
+import inferColumnTypes from './inferColumnTypes.js';
 
+/**
+ *
+ * @param {import("./types.js").QueryStoreValue} data
+ * @param {keyof data[number]} [column]
+ * @returns {import('./types.js').EvidenceTypeDescriptor | undefined}
+ */
 export default function getColumnEvidenceType(data, column) {
-	let item;
-	if (data) {
-		if (data._evidenceColumnTypes) {
-			let columnTypes = data._evidenceColumnTypes;
-			return columnTypes.find((item) => item.name?.toLowerCase() === column?.toLowerCase());
-		}
-		if (Array.isArray(data) && data.length > 0) {
-			item = data[0];
-		} else {
-			item = data;
-		}
-		if (item && item['_evidenceColumnTypes']) {
-			let columnTypes = item['_evidenceColumnTypes'];
-			return columnTypes.find((item) => item.name?.toLowerCase() === column?.toLowerCase());
-		} else {
-			// infer types as a fall-back (when someone is passing arbitrary data objects)
-			let columnTypes = inferColumnTypes(data);
-			return columnTypes.find((item) => item.name?.toLowerCase() === column?.toLowerCase());
-		}
+	if (!data || !column) return undefined;
+
+	if (data._evidenceColumnTypes) {
+		const columnTypes = data._evidenceColumnTypes;
+		return columnTypes.find((item) => item.name?.toLowerCase() === column.toLowerCase());
 	}
 
-	return null;
+	const columnTypes = inferColumnTypes(data);
+	return columnTypes?.find((item) => item.name?.toLowerCase() === column.toLowerCase());
 }
