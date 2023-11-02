a2cc3ca48b2ff543567e4bddf4ec9a079055f65e
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-11-02 18:05:04 -0400

    only do this in dev

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,7 +1,19 @@
+import { dev } from '$app/environment';
 export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
 		const { customFormattingSettings, routeHash } = parentData;
-		const { data } = await fetch(`/api/${routeHash}.json`).then((res) => res.json());
+
+		let data, json;
+		const res = await fetch(`/api/${routeHash}.json`);
+		
+		// don't clone the response if we're in dev mode as it causes multiple queries to fire
+		// but when building we do to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
+		if (dev) {
+			json = await res.json();
+		} else {
+			json = await res.clone().json();
+		}
+		data = json.data;
 
 		return {
 			data,
