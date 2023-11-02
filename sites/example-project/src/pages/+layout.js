58fd53045625cc09a9c7af990414229f2c2fda2b
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-11-02 16:23:21 -0400

    format

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,7 +1,7 @@
 export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
 		const { customFormattingSettings, routeHash } = parentData;
-		const { data } = await fetch(`/api/${routeHash}.json`).then(res=> res.json());
+		const { data } = await fetch(`/api/${routeHash}.json`).then((res) => res.json());
 
 		return {
 			data,
