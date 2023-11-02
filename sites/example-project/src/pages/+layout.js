291590f05ea6874df37ce879d3a1e308e5d8a682
csjh <61282104+csjh@users.noreply.github.com>
2023-11-02 18:26:04 -0400

    add detailed explanation of the conditional clone

--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -3,17 +3,21 @@ export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
 		const { customFormattingSettings, routeHash } = parentData;
 
-		let data, json;
 		const res = await fetch(`/api/${routeHash}.json`);
 
-		// don't clone the response if we're in dev mode as it causes multiple queries to fire
-		// but when building we do to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
-		if (dev) {
-			json = await res.json();
-		} else {
-			json = await res.clone().json();
-		}
-		data = json.data;
+		/*
+			Explanation of the below:
+			The SvelteKit `fetch` uses a proxy[0] to prevent a request happening on
+			both server and client execution of load functions
+			- This is good during dev, as it prevents the multiple queries from firing
+			- But bad during build, as it inlines the response into the SSR'd page,
+			  which blows up build folder/html size
+			So, we conditionally clone the response to bypass the proxy during build,
+			but not during dev
+
+			[0] - https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
+		*/
+		const { data } = dev? await res.json() : await res.clone().json();
 
 		return {
 			data,
