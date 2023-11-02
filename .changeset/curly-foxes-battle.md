4c54ae21836e0176f14668dc2667f35b8b4e3c9a
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-11-02 15:53:52 -0400

    don't clone the results

---
+++ .changeset/curly-foxes-battle.md
@@ -0,0 +1,5 @@
+---
+'@evidence-dev/components': patch
+---
+
+prevent multiple hits to the data API in the terminal
--- sites/example-project/src/pages/+layout.js
+++ sites/example-project/src/pages/+layout.js
@@ -1,9 +1,7 @@
 export const load = async ({ fetch, route, data: parentData }) => {
 	if (route.id && route.id !== '/settings') {
 		const { customFormattingSettings, routeHash } = parentData;
-		const res = await fetch(`/api/${routeHash}.json`);
-		// has to be cloned to bypass the proxy https://github.com/sveltejs/kit/blob/master/packages/kit/src/runtime/server/page/load_data.js#L297
-		const { data } = await res.clone().json();
+		const { data } = await fetch(`/api/${routeHash}.json`).then(res=> res.json());
 
 		return {
 			data,
