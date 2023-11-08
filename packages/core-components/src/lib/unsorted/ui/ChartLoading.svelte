8bc5eac10179d1c5eabf016ea517a5e8e552e38f
Sean Hughes <sean@evidence.dev>
2023-11-08 11:22:08 -0500

    adjust margins

--- packages/core-components/src/lib/unsorted/ui/ChartLoading.svelte
+++ packages/core-components/src/lib/unsorted/ui/ChartLoading.svelte
@@ -8,5 +8,5 @@
 
 <div role="status" class="animate-pulse" in:fade|local>
 	<span class="sr-only">Loading...</span>
-	<div class="h-48 bg-gray-200 rounded-md dark:bg-gray-400 max-w-[100%] mb-5" />
+	<div class="h-48 bg-gray-200 rounded-md dark:bg-gray-400 max-w-[100%] mt-5 mb-12" />
 </div>
