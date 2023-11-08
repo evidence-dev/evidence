44dd2831f63c41c1e77aeddc10462e658498cbf5
Sean Hughes <sean@evidence.dev>
2023-11-08 11:38:02 -0500

    format

--- packages/core-components/src/lib/unsorted/ui/ChartLoading.svelte
+++ packages/core-components/src/lib/unsorted/ui/ChartLoading.svelte
@@ -10,5 +10,8 @@
 
 <div role="status" class="animate-pulse" in:fade|local>
 	<span class="sr-only">Loading...</span>
-	<div class="bg-gray-200 rounded-md dark:bg-gray-400 max-w-[100%] mt-5 mb-7" style="height:{height}"/>
+	<div
+		class="bg-gray-200 rounded-md dark:bg-gray-400 max-w-[100%] mt-5 mb-7"
+		style="height:{height}"
+	/>
 </div>
