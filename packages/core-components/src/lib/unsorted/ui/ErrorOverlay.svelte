6ce1a7409a1f9bf0723c9fab4a1346ba46b6c002
Adam <mcrascal@users.noreply.github.com>
2023-11-29 21:49:42 -0500

    add links

--- packages/core-components/src/lib/unsorted/ui/ErrorOverlay.svelte
+++ packages/core-components/src/lib/unsorted/ui/ErrorOverlay.svelte
@@ -59,12 +59,28 @@
 	>
 		<div
 			transition:fly|local={{ y: 100, duration: 300 }}
-			class="min-w-full h-screen bg-gradient-to-b from-black/95 to-gray-900/90 border-t-red-600 rounded-lg border-t-8 border-red-600 shadow-xl p-8"
+			class="relative min-w-full h-screen bg-gradient-to-b from-black/95 to-gray-900/90 border-t-red-600 rounded-lg border-t-8 border-red-600 shadow-xl p-8"
 		>
 			<h1 class="text-4xl font-bold tracking-wide text-gray-200 border-b pb-4 border-gray-800">
 				Error
 			</h1>
 			<p class="text-xl text-gray-200 mt-6 leading-relaxed select-text">{message}</p>
+			<div class="absolute bottom-0 flex items-end gap-4 text-lg mb-6">
+				<a
+					href="https://docs.evidence.dev"
+					target="”_blank”"
+					class="text-gray-200 hover:text-white transition-colors duration-200">Docs</a
+				>
+				<a
+					href="https://evidencedev.slack.com/join/shared_invite/zt-uda6wp6a-hP6Qyz0LUOddwpXW5qG03Q#/shared-invite/email"
+					target="”_blank”"
+					class="text-gray-200 hover:text-white transition-colors duration-200 ">Slack</a
+				>
+				<a
+					href="mailto:support@evidence.dev"
+					class="text-gray-200 hover:text-white transition-colors duration-200 ">Email</a
+				>
+			</div>
 		</div>
 	</div>
 {/if}
