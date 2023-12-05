fe466b1318269da50c571ae5db9c091bb11811dd
Brian D <contact@noimbrian.com>
2023-12-04 22:40:24 -0600

    feat: localstorage backed store

--- packages/component-utilities/src/stores.js
+++ packages/component-utilities/src/stores.js
@@ -2,11 +2,6 @@ import { dev } from '$app/environment';
 import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-// Persist ShowQueries user choice
-export const showQueries = writable(
-	dev && browser && localStorage.getItem('showQueries') != 'false'
-);
-showQueries.subscribe((value) => browser && localStorage.setItem('showQueries', value));
 export const pageHasQueries = writable(true);
 export const routeHash = writable('');
 
@@ -28,3 +23,55 @@ function createToastsObject() {
 /** @typedef {{ id: unknown; status?: ToastStatus; title: string; message: string; }} Toast */
 /** @type {import('svelte/store').Readable<Toast[]> & { add: (toast: Toast, timeout: number) => void }} */
 export const toasts = createToastsObject();
+
+/**
+ * @template T
+ * @param {import('svelte/store').Readable<T>} store
+ * @returns {T}
+ */
+const getStoreVal = (store) => {
+	let v;
+	store.subscribe((x) => (v = x))();
+	return v;
+};
+
+/**
+ * Implementation of a writable store that also saves it's values to localStorage
+ * @template T
+ * @param {string} key localStorage key
+ * @param {T} init
+ * @returns {Writable<T>}
+ */
+export const localStorageStore = (key, init) => {
+	const store = writable(browser ? JSON.parse(localStorage.getItem(key)) ?? init : init);
+	const { subscribe, set } = store;
+
+	/** @type {(v: T) => void} */
+	const flush = (v) => {
+		if (browser) {
+			if (typeof v === 'undefined' || v === null) {
+				localStorage.removeItem(key);
+			} else {
+				localStorage.setItem(key, JSON.stringify(v));
+			}
+		}
+	};
+
+	flush(getStoreVal(store));
+
+	/** @type {Writable<T>} */
+	return {
+		subscribe,
+		set: (v) => {
+			set(v);
+			flush(v);
+		},
+		update: (cb) => {
+			const updatedStore = cb(getStoreVal(store));
+			set(updatedStore);
+		}
+	};
+};
+
+// Persist ShowQueries user choice
+export const showQueries = localStorageStore('showQueries', dev);
