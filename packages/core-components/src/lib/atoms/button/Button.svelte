b4de6d5515ec2e1e3e86cf9100ae3e4199e5217f
Brian D <contact@noimbrian.com>
2023-12-05 22:51:01 -0600

    chore: hint and button styles

--- packages/core-components/src/lib/atoms/button/Button.svelte
+++ packages/core-components/src/lib/atoms/button/Button.svelte
@@ -1,47 +1,23 @@
 <script context="module">
 	export const evidenceInclude = true;
 
-	/** @typedef {"sm" | "base" | "lg"} ButtonSize */
+	/** @typedef {"sm" | "md" | "base" | "lg"} ButtonSize */
 	/** @typedef {"info" | "success" | "warn" | "error"} ButtonVariant */
 	/** @typedef {"left" | "right"} ButtonIconPosition */
 	/** @typedef {boolean} ButtonOutline */
 
-	/**
-	 * @type {Record<ButtonOutline, Record<ButtonVariant, string>>}
-	 * Lookup for the available styles for a button
-	 * We have to do it this way so tailwind can statically analyze the file
-	 */
-	const colors = {
-		// Outlined
-		/** @type {Record<ButtonVariant, string>}*/
-		[true]: {
-			info: 'border border-blue-700 text-blue-700 hover:bg-blue-100 hover:text-blue-800 hover:border-blue-800 active:text-blue-900 active:border-blue-900',
-			warn: 'border border-yellow-700 text-yellow-700 hover:bg-yellow-100 hover:text-yellow-800 hover:border-yellow-800 active:text-yellow-900 active:border-yellow-900',
-			error:
-				'border border-red-700 text-red-700 hover:bg-red-100 hover:text-red-800 hover:border-red-800 active:text-red-900 active:border-red-900',
-			success:
-				'border border-green-700 text-green-700 hover:bg-green-100 hover:text-green-800 hover:border-green-800 active:text-green-900 active:border-green-900'
-		},
-		// Not Outlined
-		/** @type {Record<ButtonVariant, string>}*/
-		[false]: {
-			info: 'bg-blue-600 text-white hover:bg-blue-700 active:hover:bg-blue-800',
-			warn: 'bg-yellow-600 text-white hover:bg-yellow-700 active:hover:bg-yellow-800',
-			error: 'bg-red-600 text-white hover:bg-red-700 active:hover:bg-red-800',
-			success: 'bg-green-600 text-white hover:bg-green-700 active:hover:bg-green-800'
-		}
-	};
-
 	/** @type {Record<ButtonSize, string>}*/
 	const sizes = {
 		base: 'px-2 py-1 mx-1 gap-2',
+		md: 'px-2 py-1 mx-1 gap-2 text-xs',
 		sm: 'px-1 py-0.5 mx-0.5 gap-1 text-xs',
 		lg: 'px-4 py-2 mx-2 gap-4'
 	};
 
 	/** @type {Record<ButtonSize, string>}*/
-	const iconSize = {
+	const iconSizes = {
 		base: 'w-4',
+		md: 'w-4',
 		sm: 'w-3',
 		lg: 'w-5'
 	};
@@ -58,26 +34,98 @@
 	/** @type {ButtonSize} */
 	export let size = 'base';
 
+	/** @type {ButtonSize} */
+	export let iconSize = size;
+
 	/** @type {ButtonVariant} */
 	export let variant = 'info';
 
 	/** @type {boolean} */
 	export let outline = false;
+
+	/** @type {boolean} */
+	export let disabled = false;
+
+	/** @type {string | undefined} */
+	export let formaction = undefined;
+
+	/** @type {HTMLButtonAttributes["type"]} */
+	let _type = 'button';
+	export { _type as type };
+
+	$: if (formaction) _type = 'submit';
 </script>
 
 <button
-	type="button"
+	type={_type}
+	{disabled}
+	{formaction}
 	on:click|stopPropagation
-	class="flex items-center {colors[outline][variant]} {sizes[size]} transition-colors rounded"
+	class:outlined={outline}
+	class="flex items-center transition-colors rounded variant-{variant} {sizes[size]}"
 >
 	{#if iconPosition === 'left' && icon}
-		<Icon src={icon} class={iconSize[size]} />
+		<Icon src={icon} class={iconSizes[iconSize]} />
 	{/if}
 	<slot />
 	{#if iconPosition === 'right' && icon}
-		<Icon src={icon} class={iconSize[size]} />
+		<Icon src={icon} class={iconSizes[iconSize]} />
 	{/if}
 </button>
 
 <style lang="postcss">
+	/* 
+		Colors are handled here because of the amount of permutations between outlined, variant, and disabled states
+
+		There is probably room to clean this up; but right now it works
+	*/
+	button {
+		&.variant-info {
+			--100: theme(colors.blue.100);
+			--400: theme(colors.blue.400);
+			--500: theme(colors.blue.500);
+			--600: theme(colors.blue.600);
+			--700: theme(colors.blue.700);
+			--800: theme(colors.blue.800);
+			--900: theme(colors.blue.900);
+		}
+		&.variant-warn {
+			--100: theme(colors.yellow.100);
+			--400: theme(colors.yellow.400);
+			--500: theme(colors.yellow.500);
+			--600: theme(colors.yellow.600);
+			--700: theme(colors.yellow.700);
+			--800: theme(colors.yellow.800);
+			--900: theme(colors.yellow.900);
+		}
+		&.variant-success {
+			--100: theme(colors.green.100);
+			--400: theme(colors.green.400);
+			--500: theme(colors.green.500);
+			--600: theme(colors.green.600);
+			--700: theme(colors.green.700);
+			--800: theme(colors.green.800);
+			--900: theme(colors.green.900);
+		}
+		&.variant-error {
+			--100: theme(colors.red.100);
+			--400: theme(colors.red.400);
+			--500: theme(colors.red.500);
+			--600: theme(colors.red.600);
+			--700: theme(colors.red.700);
+			--800: theme(colors.red.800);
+			--900: theme(colors.red.900);
+		}
+
+		@apply disabled:cursor-default enabled:cursor-pointer;
+
+		&.outlined {
+			@apply border border-[var(--700)] text-[var(--700)] enabled:hover:bg-[var(--100)] enabled:hover:text-[var(--800)] enabled:hover:border-[var(--800)] enabled:active:text-[var(--900)] enabled:active:border-[var(--900)]
+					disabled:border-[var(--400)] disabled:text-[var(--400)];
+		}
+
+		&:not(.outlined) {
+			@apply bg-[var(--600)] text-white enabled:hover:bg-[var(--700)] enabled:active:bg-[var(--800)] border border-transparent disabled:bg-[var(--400)];
+		}
+	}
 </style>
