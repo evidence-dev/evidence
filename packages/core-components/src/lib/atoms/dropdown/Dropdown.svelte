4542d9789edf73432de3edcb0b4f12d3f0be9b52
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-12-11 19:04:03 -0500

    dropdown styling

--- packages/core-components/src/lib/atoms/dropdown/Dropdown.svelte
+++ packages/core-components/src/lib/atoms/dropdown/Dropdown.svelte
@@ -36,6 +36,7 @@
 		`Dropdown-${name}`
 	));
 </script>
+<div class="mt-2 mb-4 mx-1">
 
 {#if title}
 	<span class="text-sm text-gray-500 block">{title}</span>
@@ -60,6 +61,8 @@
 	<select
 		disabled={hasQuery && !$query.loaded}
 		on:change={(e) => ($inputs[name] = e.currentTarget.value)}
+		class="border border-gray-300 bg-white rounded-lg p-1 mt-2 px-2 pr-5 flex flex-row items-center max-w-fit bg-transparent cursor-pointer bg-right bg-no-repeat"
+		style="background-image: url('data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' class=\'icon icon-tabler icon-tabler-chevron-down\' width=\'18\' height=\'18\' viewBox=\'0 0 24 24\' stroke-width=\'2\' stroke=\'currentColor\' fill=\'none\' stroke-linecap=\'round\' stroke-linejoin=\'round\'%3E%3Cpath stroke=\'none\' d=\'M0 0h24v24H0z\' fill=\'none\'/%3E%3Cpath d=\'M6 9l6 6l6 -6\' /%3E%3C/svg%3E');"
 	>
 		<slot />
 
@@ -70,3 +73,5 @@
 		{/if}
 	</select>
 {/if}
+
+</div>
