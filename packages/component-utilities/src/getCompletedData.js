bb850b4b07df23f2bf75c4304ae890178293853d
Brian D <contact@noimbrian.com>
2023-12-21 10:20:35 -0600

    chore: progress

--- packages/component-utilities/src/getCompletedData.js
+++ packages/component-utilities/src/getCompletedData.js
@@ -1,11 +1,11 @@
-import { tidy, complete, mutate } from '@tidyjs/tidy';
+import { tidy, complete } from '@tidyjs/tidy';
 import getDistinctValues from './getDistinctValues';
 import { findInterval, vectorSeq } from './helpers/getCompletedData.helpers.js';
 
 /**
  * This function fills missing data points in the given data array for a specific series.
  *
- * @param {Record<string, unknown>[]} data - The data as an array of objects.
+ * @param {Record<string, unknown>[]} _data - The data as an array of objects.
  * @param {string} x - The property used as x-axis.
  * @param {string} y - The property used as y-axis.
  * @param {string} series - The specific series in the data to be filled.
@@ -13,11 +13,15 @@ import { findInterval, vectorSeq } from './helpers/getCompletedData.helpers.js';
  * @param {boolean} [fillX=false] - A flag indicating whether the x-axis values should be filled (based on the found interval distance).
  * @return {Record<string, unknown>[]} An array containing the filled data objects.
  */
-export default function getCompletedData(data, x, y, series, nullsZero = false, fillX = false) {
+export default function getCompletedData(_data, x, y, series, nullsZero = false, fillX = false) {
+	const data = _data.map((d) => Object.assign({}, d));
 	const groups = Array.from(data).reduce((a, v) => {
+		if (v[x] instanceof Date) {
+			v[x] = v[x].toLocaleString();
+		}
 		if (series) {
-			if (!a[v[series]]) a[v[series]] = [];
-			a[v[series]].push(v);
+			if (!a[v[series] ?? 'null']) a[v[series] ?? 'null'] = [];
+			a[v[series] ?? 'null'].push(v);
 		} else {
 			if (!a.default) a.default = [];
 			a.default.push(v);
@@ -28,8 +32,6 @@ export default function getCompletedData(data, x, y, series, nullsZero = false, 
 	// Ensures that all permutations of this map exist in the output
 	// e.g. can include series and x values to ensure that all series have all x values
 	const expandKeys = {};
-
-	const xIsDate = data[0]?.[x] instanceof Date;
 
 	/** @type {Array<number | string>} */
 	let xDistinct;
@@ -46,6 +48,7 @@ export default function getCompletedData(data, x, y, series, nullsZero = false, 
 				data.map((d) => ({ [x]: d[x].getTime() })),
 				x
 			);
+
 			// We don't fillX here because date numbers are very large, so a small interval would create a _massive_ array
 			break;
 		case 'number':
@@ -84,14 +87,6 @@ export default function getCompletedData(data, x, y, series, nullsZero = false, 
 			tidyFuncs.push(complete([x], nullySpec));
 		} else {
 			tidyFuncs.push(complete(expandKeys, nullySpec));
-		}
-		if (xIsDate) {
-			// Ensure that x is actually a date
-			tidyFuncs.push(
-				mutate({
-					[x]: (val) => new Date(val[x])
-				})
-			);
 		}
 
 		output.push(tidy(value, ...tidyFuncs));
