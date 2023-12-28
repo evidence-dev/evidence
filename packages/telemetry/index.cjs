f7d72ac29a0db16fea2969543b8ead31b7212d9b
Udesh Senaratne <udesh@evidence.dev>
2023-12-28 15:12:46 -0500

    Brought back the profile file so that example project doesn't create new profiles

--- packages/telemetry/index.cjs
+++ packages/telemetry/index.cjs
@@ -1,6 +1,6 @@
 const secure = require('@lukeed/uuid/secure');
 const md5 = require('blueimp-md5');
-const { readJSONSync, writeJSONSync, pathExistsSync, moveSync } = require('fs-extra');
+const { readJSONSync, writeJSONSync, pathExistsSync, copySync } = require('fs-extra');
 const wK = 'ydlp5unBbi75doGz89jC3P1Llb4QjYkM';
 const Analytics = require('analytics-node');
 
@@ -163,7 +163,7 @@ function loadSettings() {
  */
 function maybeMigrateProfile() {
 	if (pathExistsSync(LEGACY_PROFILES_PATH)) {
-		moveSync(LEGACY_PROFILES_PATH, PROFILES_PATH);
+		copySync(LEGACY_PROFILES_PATH, PROFILES_PATH);
 		return true;
 	} else {
 		return false;
