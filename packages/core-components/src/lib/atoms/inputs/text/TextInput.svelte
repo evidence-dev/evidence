77536366e475263b81640afc6a796352440d742c
Archie Wood <58074498+archiewood@users.noreply.github.com>
2023-12-29 01:50:40 +0000

    placeholder, default for TextInput

--- packages/core-components/src/lib/atoms/inputs/text/TextInput.svelte
+++ packages/core-components/src/lib/atoms/inputs/text/TextInput.svelte
@@ -17,6 +17,12 @@
 	/** @type {string} */
 	export let name;
 
+	/** @type {string} */
+	export let placeholder = "Type to search";
+
+	/** @type {string} */
+	export let defaultValue = '';
+
 	/** @type {boolean} */
 	export let unsafe = false;
 	$: unsafe = unsafe === true || unsafe === 'true';
@@ -33,7 +39,7 @@
 		};
 	}
 
-	let value = '';
+	let value = defaultValue;
 </script>
 
 <div class="mt-2 mb-4 mx-1 inline-block">
@@ -43,5 +49,6 @@
 	<input
 		bind:value
 		class="border border-gray-300 bg-white rounded-lg p-1 mt-2 px-2 pr-5 flex flex-row items-center max-w-fit bg-transparent cursor-text bg-right bg-no-repeat"
+		placeholder={placeholder}
 	/>
 </div>
