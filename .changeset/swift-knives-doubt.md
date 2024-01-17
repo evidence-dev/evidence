1ef5544d2239df7985ec42871c6b4c00db4c8c5d
Charles <charles@evidence.dev>
2024-01-17 13:17:12 -0500

    Improve the printing results for viz/map (#1488)
    * Attempt to improve the printing result using the map viz
    * Add changeset
    
    ---------
    
    Co-authored-by: Brian D <contact@noimbrian.com>

---
+++ .changeset/swift-knives-doubt.md
@@ -0,0 +1,6 @@
+---
+'@evidence-dev/core-components': minor
+'@evidence-dev/components': minor
+---
+
+Improve printing for Map viz
--- packages/core-components/src/lib/unsorted/viz/core/EChartsCopyTarget.svelte
+++ packages/core-components/src/lib/unsorted/viz/core/EChartsCopyTarget.svelte
@@ -28,9 +28,19 @@
 		use:eChartsCopy={{ config, ratio: 2, echartsOptions }}
 	/>
 {:else if printing}
-	<div
-		class="chart"
-		style="
+	<!-- special printing config for the visual map -->
+	{#if $$props.config.visualMap !== undefined}
+		<div
+			class="chart"
+			style="
+			height: {height};
+			width: {width};"
+			use:eChartsCopy={{ config, ratio: 4, echartsOptions }}
+		/>
+	{:else}
+		<div
+			class="chart"
+			style="
 		height: {height};
 		width: 590px;
 		margin-left: 0;
@@ -38,7 +48,8 @@
 		margin-bottom: 10px;
 		overflow: visible;
 		break-inside: avoid;
-  "
-		use:eChartsCopy={{ config, ratio: 4, echartsOptions }}
-	/>
+	"
+			use:eChartsCopy={{ config, ratio: 4, echartsOptions }}
+		/>
+	{/if}
 {/if}
