d5421e98cd8356506b84530af22aec608bf417ad
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-01-25 10:22:04 -0500

    fix tests

--- packages/component-utilities/src/tests/columnUnitSummary.spec.js
+++ packages/component-utilities/src/tests/columnUnitSummary.spec.js
@@ -13,6 +13,10 @@ describe('getColumnUnitSummary', () => {
 
 		const summary = getColumnUnitSummary(data, 'columnTwo');
 		expect(summary).toStrictEqual({
+			count: 5,
+			countDistinct: 5,
+			mean: 3.048318,
+			sum: 15.24159,
 			min: 1,
 			max: 5,
 			median: 3.1,
@@ -30,10 +34,48 @@ describe('getColumnUnitSummary', () => {
 		];
 		const summary = getColumnUnitSummary(data, 'column2');
 		expect(summary).toStrictEqual({
+			count: 5,
+			countDistinct: 4,
+			mean: 3,
+			sum: 9,
 			min: 1,
 			max: 5,
 			median: 3,
 			maxDecimals: 0,
+			unitType: 'number'
+		});
+	});
+	it('should only return count and countDistinct when the data is not numeric, and other values should be undefined', () => {
+		const data = [
+			{ column1: 'foo', column2: 'bar', bool: true},
+			{ column1: 'dar', column2: 'blah', bool: false},
+			{ column1: 'blah', column2: 'blah', bool: true},
+			{ column1: 'blah', column2: 'blah', bool: false}
+		];
+		const summary = getColumnUnitSummary(data, 'column2', false);
+		expect(summary).toStrictEqual({
+			count: 4,
+			countDistinct: 2,
+			maxDecimals: 0,
+			max: undefined,
+			mean: undefined,
+			median: undefined,
+			min: undefined,
+			sum: undefined,
+			// Probably undesired behavior, fixed downstream
+			unitType: 'number'
+		});
+		const summaryBool = getColumnUnitSummary(data, 'bool', false);
+		expect(summaryBool).toStrictEqual({
+			count: 4,
+			countDistinct: 2,
+			maxDecimals: 0,
+			max: undefined,
+			mean: undefined,
+			median: undefined,
+			min: undefined,
+			sum: undefined,
+			// Probably undesired behavior, fixed downstream
 			unitType: 'number'
 		});
 	});
