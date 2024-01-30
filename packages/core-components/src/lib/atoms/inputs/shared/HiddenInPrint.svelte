18e125280f0932f1e1705000464aecc246b53485
csjh <61282104+csjh@users.noreply.github.com>
2024-01-30 17:29:44 -0500

    make `HiddenInPrint` less dumb

--- packages/core-components/src/lib/atoms/inputs/shared/HiddenInPrint.svelte
+++ packages/core-components/src/lib/atoms/inputs/shared/HiddenInPrint.svelte
@@ -4,15 +4,8 @@
 
 <script>
 	export let enabled = true;
-
-	let printing = false;
 </script>
 
-<svelte:window
-	on:beforeprint={() => (printing = true)}
-	on:afterprint={() => (printing = false)}
-	on:export-beforeprint={() => (printing = true)}
-	on:export-afterprint={() => (printing = false)}
-/>
-
-{#if !(enabled && printing)}<slot />{/if}
+<div class:print:hidden={enabled} class="contents">
+	<slot />
+</div>
