7d3ab7a2a01b9d8b6e5c59a1f261c7e108224f75
Adam <mcrascal@users.noreply.github.com>
2024-01-29 21:55:05 -0500

    rough in interaction

--- packages/core-components/src/lib/molecules/dimension-grid/DimensionCut.svelte
+++ packages/core-components/src/lib/molecules/dimension-grid/DimensionCut.svelte
@@ -1,79 +1,111 @@
 <script>
+	import { getContext } from 'svelte';
 	import { buildQuery } from '@evidence-dev/component-utilities/buildQuery';
 	import { fmt as format } from '@evidence-dev/component-utilities/formatting';
 	import formatTitle from '@evidence-dev/component-utilities/formatTitle';
+	import { flip } from 'svelte/animate';
 	export let dimension;
 	export let metric;
 	export let limit;
 	export let data;
-	export let fmt = 'usd';
+	export let fmt;
+	export let whereClause;
 
 	export let others;
 	export let grandTotal;
 
-	let cut = buildQuery(
-		`select ${dimension.name} as dimension, ${metric} as metric, metric/sum(metric) over() as percentOfMaxMetric from (${$data.originalText}) group by 1 order by 2 desc limit ${limit}`
+	let selectedDimensions = getContext('selected-dimensions');
+
+	let selectedValue;
+
+	$: if (selectedValue) {
+		selectedDimensions.update((v) => {
+			let newV = v.filter((d) => d.dimension !== dimension.name);
+			newV.push({ dimension: dimension.name, value: selectedValue });
+			return newV;
+		});
+	} else {
+		selectedDimensions.update((v) => {
+			return v.filter((d) => d.dimension !== dimension.name);
+		});
+	}
+
+	let cut;
+	let allOthers;
+	let totalRow;
+
+	$: cut = buildQuery(
+		`select ${dimension.name} as dimension, ${metric} as metric, ${metric} filter(${whereClause}) as filteredMetric, metric/sum(metric) over() as percentOfMaxMetric from (${$data.originalText}) group by 1 order by 3 desc, 2 desc limit ${limit}`,
+		'cut'
 	);
 
-	let allOthers = buildQuery(
+	$: allOthers = buildQuery(
 		`select count(distinct ${dimension.name}) as n_records, ${metric} as metric from (${$data.originalText}) where ${dimension.name} not in (select dimension from (${$cut.originalText}))`
 	);
 
-	let totalRow = buildQuery(
+	$: totalRow = buildQuery(
 		`select count(distinct ${dimension.name}) as n_records, ${metric} as metric from (${$data.originalText})`
 	);
 
-	cut.fetch();
-	allOthers.fetch();
-	totalRow.fetch();
+	$: cut?.fetch();
+	$: allOthers?.fetch();
+	$: totalRow?.fetch();
 </script>
 
-{#if $cut.loaded && $allOthers.loaded && totalRow.loaded}
-	<div class="w-full sm:w-64 text-sm">
-		<div class="capitalize mb-1 text-base">
-			{formatTitle(dimension.name)}
+<!-- {$data.originalText} -->
+
+<div class="w-full sm:w-64 text-sm sm:flex-1">
+	<div class="capitalize pb-1 px-1 text-medium border-b">
+		{formatTitle(dimension.name)}
+	</div>
+	{#each $cut as row, i (row.dimension)}
+		<div
+			class="flex justify-between relative px-1 hover:bg-gray-50 group cursor-pointer"
+			on:click={() => {
+				selectedValue === row.dimension
+					? (selectedValue = undefined)
+					: (selectedValue = row.dimension);
+			}}
+			animate:flip={{ duration: 200 }}
+		>
+			<span class="text-gray-900 z-10 truncate">
+				{row.dimension}
+			</span>
+			<span class="text-gray-700 tabular-nums z-10">
+				{format(row.filteredMetric, fmt)}
+			</span>
+			<div
+				class="absolute inset-0 bg-blue-100/50 z-0 group-hover:bg-blue-200/50 transition duration-200"
+				style={'width:' + Math.max(row.percentOfMaxMetric * 100, 0) + '%'}
+			/>
 		</div>
-		{#each $cut as row, i}
-			<div class="flex justify-between relative px-1 hover:bg-gray-50 group">
-				<span class="text-gray-900 z-10 truncate">
-					{row.dimension}
-				</span>
-				<span class="text-gray-700 tabular-nums z-10">
-					{format(row.metric, fmt)}
-				</span>
-				<div
-					class="absolute inset-0 bg-blue-100/50 z-0 group-hover:bg-blue-200/50 transition duration-200"
-					style={'width:' + Math.max(row.percentOfMaxMetric * 100, 0) + '%'}
-				/>
-			</div>
-		{/each}
-		{#if others}
-			{#each $allOthers as row, i}
-				{#if row.n_records > 0}
-					<div class="flex justify-between relative px-1 hover:bg-gray-50 group">
-						<span class="text-gray-900 z-10 lowercase">
-							{row.n_records} other {formatTitle(dimension.name)}s
-						</span>
-						<span class="text-gray-700 tabular-nums z-10">
-							{format(row.metric, fmt)}
-						</span>
-						<div
-							class="absolute inset-0 z-0 bg-blue-100/50 group-hover:bg-blue-200/50 transition duration-200"
-							style={'width:' + Math.max(row.percentOfMaxMetric * 100, 0) + '%'}
-						/>
-					</div>
-				{/if}
-			{/each}
-		{/if}
-		{#if grandTotal}
-			{#each $totalRow as row, i}
-				<div class="flex justify-between relative px-1 font-medium hover:bg-gray-50 group">
-					<span class="text-gray-900 z-10 capitalize"> Total </span>
+	{/each}
+	{#if others}
+		{#each $allOthers as row, i}
+			{#if row.n_records > 0}
+				<div class="flex justify-between relative px-1 hover:bg-gray-50 group">
+					<span class="text-gray-900 z-10 lowercase">
+						{row.n_records} others
+					</span>
 					<span class="text-gray-700 tabular-nums z-10">
 						{format(row.metric, fmt)}
 					</span>
+					<div
+						class="absolute inset-0 z-0 bg-blue-100/50 group-hover:bg-blue-200/50 transition duration-200"
+						style={'width:' + Math.max(row.percentOfMaxMetric * 100, 0) + '%'}
+					/>
 				</div>
-			{/each}
-		{/if}
-	</div>
-{/if}
+			{/if}
+		{/each}
+	{/if}
+	{#if grandTotal}
+		{#each $totalRow as row, i}
+			<div class="flex justify-between relative px-1 font-medium hover:bg-gray-50 group">
+				<span class="text-gray-900 z-10 capitalize"> Total </span>
+				<span class="text-gray-700 tabular-nums z-10">
+					{format(row.metric, fmt)}
+				</span>
+			</div>
+		{/each}
+	{/if}
+</div>
--- packages/core-components/src/lib/molecules/dimension-grid/DimensionGrid.svelte
+++ packages/core-components/src/lib/molecules/dimension-grid/DimensionGrid.svelte
@@ -3,6 +3,10 @@
 </script>
 
 <script>
+	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
+
+	import { getContext, setContext } from 'svelte';
+	import { writable } from 'svelte/store';
 	import DimensionCut from './DimensionCut.svelte';
 
 	/** @type {string} */
@@ -10,16 +14,48 @@
 	/** @type {import('@evidence-dev/query-store').QueryStore} */
 	export let data;
 	export let metric = 'count(*)';
-	export let fmt = 'number';
+	export let fmt = '#,###.00';
 	export let limit = 10;
 	export let others = true;
 	export let grandTotal = false;
 
 	let dimensions = data?.columns?.filter((col) => col.evidenceType === 'string');
+
+	const inputs = getContext(INPUTS_CONTEXT_KEY);
+
+	let selectedDimensions = writable([]);
+
+	setContext('selected-dimensions', selectedDimensions);
+
+	let whereClause = 'true';
+
+	$: if ($selectedDimensions.length > 0) {
+		whereClause = $selectedDimensions
+			.map((d) => {
+				return `${d.dimension} = '${d.value}'`;
+			})
+			.join(' and ');
+	} else {
+		whereClause = 'true';
+	}
+
+	$: internallyFilteredData = data.where(whereClause);
 </script>
 
-<div class="flex flex-wrap gap-6">
+<!-- {$internallyFilteredData.originalText} -->
+
+<div class="flex flex-wrap gap-6 select-none">
 	{#each dimensions as dimension}
-		<DimensionCut {dimension} {metric} {data} {fmt} {limit} {others} {grandTotal} />
+		<DimensionCut
+			{data}
+			{whereClause}
+			{dimension}
+			{metric}
+			{fmt}
+			{limit}
+			{others}
+			{grandTotal}
+			{name}
+		/>
 	{/each}
 </div>
