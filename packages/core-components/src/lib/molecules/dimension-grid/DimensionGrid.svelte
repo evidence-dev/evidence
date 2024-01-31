d1f71c5ad9cda344b1159ace91a03a4a27500ac8
Adam <mcrascal@users.noreply.github.com>
2024-01-31 09:13:07 -0500

    lint

--- packages/core-components/src/lib/molecules/dimension-grid/DimensionGrid.svelte
+++ packages/core-components/src/lib/molecules/dimension-grid/DimensionGrid.svelte
@@ -3,33 +3,23 @@
 </script>
 
 <script>
-	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
-	import { getContext, setContext } from 'svelte';
+	// import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
+	// import { getContext, setContext } from 'svelte';
 	import { writable } from 'svelte/store';
 	import DimensionCut from './DimensionCut.svelte';
-	import { buildQuery } from '@evidence-dev/component-utilities/buildQuery';
 
 	/** @type {string} */
-	export let name;
 	/** @type {import('@evidence-dev/query-store').QueryStore} */
 	export let data;
 	export let metric = 'count(*)';
 	export let metricLabel = undefined;
-	export let fmt;
 	export let limit = 10;
-	export let others = true;
-	export let grandTotal = false;
 
-	const inputs = getContext(INPUTS_CONTEXT_KEY);
+	// const inputs = getContext(INPUTS_CONTEXT_KEY);
 	let dimensions = data?.columns?.filter((col) => col.evidenceType === 'string');
 	let selectedDimensions = writable([]);
 	setContext('selected-dimensions', selectedDimensions);
 </script>
-
-<!-- <pre>
-	{mainQuery}
-</pre>
-{[...$results]} -->
 
 <div class="flex flex-wrap select-none">
 	{#each dimensions as dimension}
