85fb8d26097eadb8d7055fd6cbde216818edfeae
Sean Hughes <sean@evidence.dev>
2024-02-01 08:53:57 -0500

    format

--- packages/core-components/src/lib/unsorted/viz/core/_Chart.svelte
+++ packages/core-components/src/lib/unsorted/viz/core/_Chart.svelte
@@ -1015,7 +1015,7 @@
 			});
 		} catch (e) {
 			error = e.message;
-                         const setTextRed = '\x1b[31m%s\x1b[0m'
+			const setTextRed = '\x1b[31m%s\x1b[0m';
 			console.error(setTextRed, `Error in ${chartType}: ${e.message}`);
 			// if the build is in production fail instead of sending the error to the chart
 			if (strictBuild) {
