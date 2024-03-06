72f9833a3044f573afd5a75dbb367128c62e97cb
Adam <mcrascal@users.noreply.github.com>
2024-03-05 21:11:39 -0500

    Input components  (#1668)
    * migrate from 1569
    
    * jsdoc conversion
    
    * convert icons
    
    * prevent squishing check icon in multi-select
    
    * changeset, _daterange ts conversion
    
    * chore: fix formatting issue
    
    * docs updates
    
    ---------
    
    Co-authored-by: Brian D <contact@noimbrian.com>

---
+++ packages/core-components/src/lib/atoms/shadcn/command/command-separator.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Command as CommandPrimitive } from 'cmdk-sv';
+	import { cn } from '$lib/utils';
+
+	/** @type {string | undefined | null} */
+	let className = undefined;
+
+	export { className as class };
+</script>
+
+<CommandPrimitive.Separator class={cn('-mx-1 h-px bg-gray-200', className)} {...$$restProps} />
---
+++ packages/core-components/src/lib/atoms/shadcn/select/select-separator.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { Select as SelectPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	/** @type {string | undefined | null} */
+	let className = undefined;
+
+	export { className as class };
+</script>
+
+<SelectPrimitive.Separator class={cn('-mx-1 my-1 h-px bg-muted', className)} {...$$restProps} />
