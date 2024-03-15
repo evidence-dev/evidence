8b132248e9ad0d93dbfee6bbf566f7d8bfa564ed
Brian D <contact@noimbrian.com>
2024-03-15 16:35:36 -0500

    chore: fix preprocessor

--- packages/lib/preprocess/src/add-script-tags.cjs
+++ packages/lib/preprocess/src/add-script-tags.cjs
@@ -5,13 +5,17 @@ const addScriptTags = {
 	markup({ content, filename }) {
 		if (filename.endsWith('.md')) {
 			if (!content.match(/\<script(.*)\>/)) {
-				return { code: '<script context="module"> </script>' + '<script> </script>' + content };
+				const result =
+					content + '\n\n<script context="module"> </script>\n\n<script> </script>\n\n';
+				return { code: result };
 			}
 			if (!content.match(/\<script(.*)context\=\"module\"(.*)\>/)) {
-				return { code: '<script context="module"> </script>' + content };
+				const result = content + '\n\n<script context="module"> </script>\n\n';
+				return { code: result };
 			}
 			if (!content.match(/\<script\>/)) {
-				return { code: '<script> </script>' + content };
+				const result = content + '\n\n<script> </script>\n\n';
+				return { code: result };
 			}
 		}
 	}
