cb669665d7327c8e72e7af95850462a0500c04ef
Sean Hughes <sean@evidence.dev>
2024-03-20 18:40:13 -0400

    align inputs to left side of page

---
+++ .changeset/cold-bats-smash.md
@@ -0,0 +1,5 @@
+---
+'@evidence-dev/core-components': patch
+---
+
+Adjust default margin on input components
--- packages/ui/core-components/src/lib/atoms/inputs/button-group/ButtonGroup.svelte
+++ packages/ui/core-components/src/lib/atoms/inputs/button-group/ButtonGroup.svelte
@@ -47,7 +47,7 @@
 </script>
 
 <HiddenInPrint enabled={hideDuringPrint}>
-	<div class="inline-flex flex-col mt-2 mb-4 mx-1">
+	<div class="inline-flex flex-col mt-2 mb-4 ml-0 mr-2">
 		{#if title}
 			<span class="text-gray-900 text-sm block mb-1">{title}</span>
 		{/if}
