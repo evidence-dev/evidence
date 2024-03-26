d63c983e978a5f9b417ad7731804a7cab487b035
Brian D <contact@noimbrian.com>
2024-03-26 15:21:11 -0500

    chore: QueryLoad only uses sdk Query now

--- packages/ui/core-components/src/lib/atoms/query-load/QueryLoad.svelte
+++ packages/ui/core-components/src/lib/atoms/query-load/QueryLoad.svelte
@@ -1,13 +1,14 @@
 <script>
 	import { onDestroy } from 'svelte';
 	import { Skeleton } from '../skeletons';
+	import { Query } from '@evidence-dev/sdk/usql';
 	import isEmptyDataset from '@evidence-dev/component-utilities/isEmptyDataset';
 
-	/** @type {import("@evidence-dev/query-store).QueryStore | unknown}*/
+	/** @type {import("@evidence-dev/sdk/usql).Query | unknown}*/
 	export let data;
 
-	$: if (data?.__isQueryStore) {
-		data.fetch(); // Somebody wants this to load. Without this the query builder features don't work
+	$: if (Query.isQuery(data)) {
+		const c = data.fetch(); // Somebody wants this to load. Without this the query builder features don't work
 		unsub();
 		unsub = data.subscribe((v) => {
 			_data = v;
@@ -23,7 +24,7 @@
 {#if !data}
 	<!-- data prop was not provided. Component to handle this prop-related error -->
 	<slot loaded={data} />
-{:else if !data?.__isQueryStore}
+{:else if !Query.isQuery(data)}
 	<!-- data prop was provided, but it is not a query store -->
 	{#if (Array.isArray(data) || !data) && isEmptyDataset(data) && $$slots.empty}
 		<!-- handle case where data is not a query store but is also empty -->
@@ -32,7 +33,7 @@
 		<!-- Not a query store, nothing to be done -->
 		<slot loaded={data} />
 	{/if}
-{:else if !_data || (!_data?.loaded && !_data.error)}
+{:else if !_data || (!_data?.ready && !_data.error)}
 	<!-- Data is loading -->
 	<slot name="skeleton">
 		<div class="w-full h-64">
