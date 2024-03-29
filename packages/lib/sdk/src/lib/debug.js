33c5df5103b173a24e10b94082bb974f125e2c5b
Brian D <contact@noimbrian.com>
2024-03-29 13:03:58 -0500

    chore: undo hardcoded debug logs

--- packages/lib/sdk/src/lib/debug.js
+++ packages/lib/sdk/src/lib/debug.js
@@ -1,8 +1,7 @@
 export const enableDebug = () => (process.env.EVIDENCE_DEBUG = 'true');
 export const isDebug = () => {
-	return true;
 	if (typeof process !== 'undefined')
-		return Boolean(process.env.EVIDENCE_DEBUG || process.env.NODE_ENV === 'test');
+		return Boolean(process.env.EVIDENCE_DEBUG || (process.env.NODE_ENV === 'test' && !process.env.EVIDENCE_DISABLE_TEST_DEBUG));
 	// @ts-expect-error
 	if (typeof import.meta.env !== 'undefined') return Boolean(import.meta.env.EVIDENCE_DEBUG);
 };
