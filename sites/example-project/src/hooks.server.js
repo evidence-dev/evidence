18b07b5a29c29bb94434d5445403c3f8e9c03c12
csjh <61282104+csjh@users.noreply.github.com>
2024-04-05 14:38:23 -0400

    add safety so `__evidence_project_splash` doesn't show in build

--- sites/example-project/src/hooks.server.js
+++ sites/example-project/src/hooks.server.js
@@ -1,3 +1,5 @@
+import { dev } from "$app/environment";
+
 /** @param {Error | unknown} e  */
 const transformError = (e) => {
 	if (!(e instanceof Error)) {
@@ -15,3 +17,17 @@ const transformError = (e) => {
 
 /** @type {import("@sveltejs/kit").HandleClientError } */
 export const handleError = (e) => transformError(e.error);
+
+/** @type {import('@sveltejs/kit').Handle} */
+export function handle({ event, resolve }) {
+	const loading = dev ? `
+	setTimeout(() => {
+		const splash = document.getElementById('__evidence_project_splash');
+		if (splash) {
+			splash.style.visibility = 'visible';
+		}
+	}, 250);
+	` : '';
+
+	return resolve(event, { transformPageChunk: ({ html }) => html.replace('/*loading*/', loading) });
+}
