0271031073c09e8b6d2253d4d92db78e04d6961e
Brian D <contact@noimbrian.com>
2024-04-15 23:22:36 -0500

    chore: add VITE_PUBLIC_EVIDENCE_DEBUG as an accepted way to enable debug

--- packages/lib/sdk/src/lib/debug.js
+++ packages/lib/sdk/src/lib/debug.js
@@ -1,13 +1,22 @@
-export const enableDebug = () => (process.env.EVIDENCE_DEBUG = 'true');
+
+export const enableDebug = () => {
+	process.env.EVIDENCE_DEBUG = 'true'
+	process.env.VITE_PUBLIC_EVIDENCE_DEBUG = 'true'
+};
+
 export const isDebug = () => {
 	if (typeof process !== 'undefined')
 		return Boolean(
-			process.env.EVIDENCE_DEBUG ||
+			process.env.EVIDENCE_DEBUG || process.env.VITE_PUBLIC_EVIDENCE_DEBUG ||
 				(process.env.NODE_ENV === 'test' && !process.env.EVIDENCE_DISABLE_TEST_DEBUG)
 		);
 	// @ts-expect-error
-	if (typeof import.meta.env !== 'undefined') return Boolean(import.meta.env.EVIDENCE_DEBUG);
+	if (typeof import.meta.env !== 'undefined')
+		return (
+			Boolean(import.meta.env.EVIDENCE_DEBUG) || Boolean(import.meta.env.VITE_PUBLIC_EVIDENCE_DEBUG)
+		);
 };
 export const disableDebug = () => {
 	delete process.env.EVIDENCE_DEBUG;
+	delete process.env.VITE_PUBLIC_EVIDENCE_DEBUG;
 };
