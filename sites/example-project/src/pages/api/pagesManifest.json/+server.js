2b4674867d8fb208e9080c16b6809c1c7f7bbbf7
csjh <61282104+csjh@users.noreply.github.com>
2024-05-03 13:54:45 -0400

    split `buildPageManifest` into its own function, test

--- sites/example-project/src/pages/api/pagesManifest.json/+server.js
+++ sites/example-project/src/pages/api/pagesManifest.json/+server.js
@@ -1,6 +1,58 @@
 import preprocess from '@evidence-dev/preprocess';
 import { error } from '@sveltejs/kit';
 
+/**
+ * @typedef {Object} PageManifestNode
+ * @property {string | undefined} label
+ * @property {string | undefined} href
+ * @property {Record<string, PageManifestNode>} children
+ * @property {Record<string, any>} frontMatter
+ * @property {boolean} isTemplated
+ * @property {boolean} isPage
+ */
+
+/**
+ * @param {Record<string, string>} pages
+ * @returns {PageManifestNode}
+ */
+export function _buildPageManifest(pages) {
+	const fileTree = {
+		label: 'Home',
+		href: undefined,
+		children: {},
+		frontMatter: {},
+		isTemplated: false,
+		isPage: false
+	};
+	for (const [pagePath, pageContent] of Object.entries(pages)) {
+		const path = pagePath.replace('/src/pages/', '');
+		let node = fileTree;
+		for (const part of path.split('/')) {
+			if (part === '+page.md') {
+				// cover home page
+				if (path === '+page.md') {
+					node.href = '/';
+				} else if (!path.includes('[')) {
+					node.href = encodeURI('/' + path.replace('/+page.md', ''));
+				}
+				node.frontMatter = preprocess.parseFrontmatter(pageContent);
+				node.isPage = true;
+			} else {
+				const label = part.includes('[') ? undefined : part.replace(/_/g, ' ').replace(/-/g, ' ');
+				node = node.children[part] = node.children[part] ?? {
+					label,
+					href: undefined,
+					children: {},
+					frontMatter: {},
+					isTemplated: part.includes('['),
+					isPage: false
+				};
+			}
+		}
+	}
+	return fileTree;
+}
+
 // Import pages and create an object structure corresponding to the file structure
 const pages = import.meta.glob('/src/pages/*/**/+page.md', {
 	import: 'default',
@@ -15,35 +67,7 @@ export const prerender = true;
  */
 export async function GET() {
 	try {
-		const fileTree = {
-			label: 'Home',
-			href: '/',
-			children: {},
-			isTemplated: false,
-			isPage: true
-		};
-		for (const [pagePath, pageContent] of Object.entries(pages)) {
-			const path = pagePath.replace('/src/pages/', '');
-			let node = fileTree;
-			for (const part of path.split('/')) {
-				if (part === '+page.md') {
-					if (!path.includes('[')) {
-						node.href = encodeURI('/' + path.replace('/+page.md', ''));
-					}
-					node.frontMatter = preprocess.parseFrontmatter(pageContent);
-					node.isPage = true;
-				} else {
-					const label = part.includes('[') ? undefined : part.replace(/_/g, ' ').replace(/-/g, ' ');
-					node = node.children[part] = node.children[part] ?? {
-						label,
-						href: undefined,
-						children: {},
-						isTemplated: part.includes('['),
-						isPage: false
-					};
-				}
-			}
-		}
+		const fileTree = _buildPageManifest(pages);
 
 		return new Response(JSON.stringify(fileTree));
 	} catch {
