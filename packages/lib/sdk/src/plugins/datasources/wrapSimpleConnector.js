928b835bd281c35093b8e501e507c23d40b404b2
Brian D <contact@noimbrian.com>
2024-06-17 10:20:22 -0500

    fix: dirent is a terrible interface

--- packages/lib/sdk/src/plugins/datasources/wrapSimpleConnector.js
+++ packages/lib/sdk/src/plugins/datasources/wrapSimpleConnector.js
@@ -30,7 +30,9 @@ export const wrapSimpleConnector = (mod, source) => {
 		const processDir = async function* (dir) {
 			const sourceFiles = await fs.readdir(dir, { withFileTypes: true });
 			for (const sourceFile of sourceFiles) {
-				const dirPath = 'parentPath' in sourceFile ? sourceFile.parentPath + '' : sourceFile.path;
+				// Why is the dirent interface so unstable?
+				// This behaves differently depending on which minor version of 18 / 20 you are using
+				const dirPath = 'parentPath' in sourceFile ? sourceFile.parentPath + '' : sourceFile.path ?? dir;
 				if (sourceFile.name === 'connection.yaml' || sourceFile.name === 'connection.options.yaml')
 					continue;
 				if (sourceFile.isDirectory()) {
