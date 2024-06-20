9e09ab4f49aa93d776cb1e366a980c4d3bb61c8d
Adam <mcrascal@users.noreply.github.com>
2024-06-19 20:55:55 -0400

    prevent spurious diffs

--- packages/ui/core-components/src/lib/unsorted/viz/map/Map.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/map/Map.stories.svelte
@@ -15,9 +15,15 @@
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
 
-	const data = Query.create(`SELECT * from locations order by point_name asc limit 20`, query);
-	const la_zip_sales = Query.create(`select * from la_zip_sales where zip_code <> 90704`, query);
-	const la_locations = Query.create(`select * from la_locations`, query);
+	const data = Query.create(
+		`SELECT * from locations order by point_name asc limit 20 order by 1`,
+		query
+	);
+	const la_zip_sales = Query.create(
+		`select * from la_zip_sales where zip_code <> 90704 order by 1`,
+		query
+	);
+	const la_locations = Query.create(`select * from la_locations order by 1`, query);
 </script>
 
 <Story name="Basic Usage">
@@ -84,6 +90,6 @@
 	<BubbleMap data={la_locations} lat="lat" long="long" size="sales" />
 </Story>
 
-<Story name="Point Map">
+<Story name="Points Map">
 	<PointMap data={la_locations} lat="lat" long="long" />
 </Story>
