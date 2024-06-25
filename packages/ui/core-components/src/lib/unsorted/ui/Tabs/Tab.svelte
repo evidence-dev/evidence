b57e7831eda444984d3490481a8b7f1595a5b2d9
Zachary Stence <zachstence@gmail.com>
2024-06-25 17:03:03 -0500

    fix: use Tab internalId to enable query-based tabs that may have their label/id changed

--- packages/ui/core-components/src/lib/unsorted/ui/Tabs/Tab.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/Tabs/Tab.svelte
@@ -4,6 +4,7 @@
 
 <script>
 	import { getContext, onDestroy, onMount } from 'svelte';
+	import { nanoid } from 'nanoid';
 
 	/**
 	 * @type {string}
@@ -12,36 +13,46 @@
 	/**
 	 * @type {string}
 	 */
-	export let id = label;
+	export let id;
+	$: id = label;
 
 	/**
 	 * @type {boolean}
 	 */
 	export let selected;
 
-	/**
-	 * @type {import("svelte/store").Writable<{ tabs: {label: string, id: string}[], active: string, tabsId: string}>}
-	 */
-	const tabs = getContext('TAB_REGISTRATION');
+	const internalId = nanoid();
 
+	/** @type {import('./index.d.ts').TabsContext} */
+	const context = getContext('TABS_STORE');
+
+	const addTabToContext = () => {
+		$context.tabs = [
+			...$context.tabs.filter((t) => t.internalId !== internalId),
+			{ internalId, id, label }
+		];
+
+		if (selected) {
+			$context.activeId = id;
+		}
+	};
+
+	// Add tab to context
+	$: if (id && label) {
+		addTabToContext();
+	}
+
+	// Update selected when the active tab changes
 	onMount(() => {
-		$tabs.tabs = [...$tabs.tabs, { label, id }];
-		if (selected) $tabs.active = id;
-		// We are creating our subscription (instead of using $tabs) after handling the selected prop to make sure that
-		// it is effective. Otherwise it would get set to false by our subscription and not work.
-		// Returning a function from onMount is basically onDestroy; so we are also cleaning this up easily
-		return tabs.subscribe(({ active }) => {
-			selected = active === id;
+		return context.subscribe(({ activeId }) => {
+			selected = activeId === id;
 		});
 	});
 
 	// Ensure that tabs remove themselves correctly when they are unmounted.
 	// Otherwise they will still exist but will never render content.
 	onDestroy(() => {
-		$tabs.tabs = $tabs.tabs.filter((t) => t.id !== id);
-		if ($tabs.active === id) {
-			$tabs.active = $tabs.tabs[0]?.id;
-		}
+		$context.tabs = $context.tabs.filter((t) => t.internalId !== internalId);
 	});
 </script>
 
---
+++ packages/ui/core-components/src/lib/unsorted/ui/Tabs/Tabs.stories.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	import Tabs from './Tabs.svelte';
+
+	export const meta = {
+		title: 'UI/Tabs',
+		component: Tabs
+	};
+</script>
+
+<script>
+	import { setContext } from 'svelte';
+	import { writable } from 'svelte/store';
+	import { Story } from '@storybook/addon-svelte-csf';
+	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
+	import { query } from '@evidence-dev/universal-sql/client-duckdb';
+	import { Query } from '@evidence-dev/sdk/usql';
+	import Tab from './Tab.svelte';
+	import TextInput from '../../../atoms/inputs/text/TextInput.svelte';
+
+	const inputStore = writable({});
+	setContext(INPUTS_CONTEXT_KEY, inputStore);
+
+	$: data = Query.create(
+		`
+      SELECT tag
+      FROM hashtags
+      ORDER BY tag
+      ${$inputStore.offset?.toString() ? `OFFSET ${$inputStore.offset.toString()}` : ``}
+      LIMIT 3
+    `,
+		query
+	);
+
+	$: if ($data) console.log($data[0]?.tag, $data[1]?.tag, $data[2]?.tag);
+</script>
+
+<Story name="Generated from a query">
+	<TextInput name="offset" title="Offset" />
+
+	<Tabs>
+		{#each $data as row}
+			{@const tag = row.tag}
+			<Tab label={tag}>
+				{tag}
+			</Tab>
+		{/each}
+	</Tabs>
+</Story>
