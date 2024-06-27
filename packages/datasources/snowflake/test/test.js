eee6ffbe920da74f332b9ac379f3ead5870903c0
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-06-27 17:48:41 -0400

    test:snowflake

--- packages/datasources/snowflake/test/test.js
+++ packages/datasources/snowflake/test/test.js
@@ -76,4 +76,63 @@ test('query batches results properly', async () => {
 	}
 });
 
+const timezoneInsertedQuery = `select
+  to_timestamp_tz('2024-06-27 00:00:00 +0000') as timestamp_explicit_utc,
+  to_timestamp_tz('2024-06-27 02:00:00 +0200') as timestamp_explicit_berlin,
+  to_timestamp_tz('2024-06-26 17:00:00 -0700') as timestamp_explicit_los_angeles,
+  to_timestamp_ntz('2024-06-27 00:00:00') as timestamp_implicit_utc,
+  to_timestamp_ntz('2024-06-27 02:00:00') as timestamp_implicit_berlin,
+  to_timestamp_ntz('2024-06-26 17:00:00') as timestamp_implicit_los_angeles`;
+
+
+	  test('timestamps are converted to UTC', async () => {
+		const timeZoneQuery = `select
+		  timestamp_explicit_utc,
+		  timestamp_explicit_berlin,
+		  timestamp_explicit_los_angeles,
+		  convert_timezone('UTC', 'UTC', timestamp_implicit_utc) as timestamp_implicit_utc_converted,
+		  convert_timezone('Europe/Berlin', 'UTC', timestamp_implicit_berlin ) as timestamp_implicit_berlin_converted,
+		  convert_timezone('America/Los_Angeles', 'UTC', timestamp_implicit_los_angeles) as timestamp_implicit_los_angeles_converted
+		  from (${timezoneInsertedQuery})`;
+	  
+		const { rows: row_generator, columnTypes } = await runQuery(timeZoneQuery,
+		  {
+			username: process.env.SNOWFLAKE_USERNAME,
+			password: process.env.SNOWFLAKE_PASSWORD,
+			account: process.env.SNOWFLAKE_ACCOUNT
+		  });
+		const rows = await batchedAsyncGeneratorToArray(row_generator);
+		const result = rows[0];
+		// All these should give the same result
+		assert.equal(
+			new Date(result.timestamp_explicit_utc).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'explicit utc timestamps should remain the same'
+		  );
+		  assert.equal(
+			new Date(result.timestamp_explicit_berlin).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'explicit berlin timestamps should be converted to utc'
+		  );
+		  assert.equal(
+			new Date(result.timestamp_explicit_los_angeles).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'explicit los angeles timestamps should be converted to utc'
+		  );
+		  assert.equal(
+			new Date(result.timestamp_implicit_utc_converted).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'implicit utc timestamps should be converted to utc by the user query'
+		  );
+		  assert.equal(
+			new Date(result.timestamp_implicit_berlin_converted).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'implicit berlin timestamps should be converted to utc by the user query'
+		  );
+		  assert.equal(
+			new Date(result.timestamp_implicit_los_angeles_converted).getTime(),
+			new Date('2024-06-27t00:00:00z').getTime(),
+			'implicit los angeles timestamps should be converted to utc by the user query'
+		  );		  
+	  });
 test.run();
