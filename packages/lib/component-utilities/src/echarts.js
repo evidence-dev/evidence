f07fb384854016c7c60afdffe4189b8e008bb162
Brian D <contact@noimbrian.com>
2024-07-01 10:32:18 -0500

    chore: remove dead code

--- packages/lib/component-utilities/src/echarts.js
+++ packages/lib/component-utilities/src/echarts.js
@@ -114,14 +114,6 @@ export default (node, option) => {
 	});
 
 	chart.on('dblclick', function (params) {
-		//This may need tweaking to handle future charts, consideration, change bubble from scatter to bubble name in params
-		//Bubble date, value and size property at index 0,1,2 so link gets pushed into index 3
-		if (params.dimensionNames.length === 4) {
-			window.location = params.data[3];
-		} else {
-			//otherwise all other charts thusfar links are at index 2
-			window.location = params.data[2];
-		}
 		dispatch('dblclick', params);
 	});
 
