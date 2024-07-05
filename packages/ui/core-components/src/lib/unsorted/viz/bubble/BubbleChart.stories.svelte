56cf7242f5c3fb9dca49df5151be6e1582305f6f
Kyle Wong <kyle.wong917@gmail.com>
2024-07-05 10:51:42 -0400

    bubble chart stories added

---
+++ packages/ui/core-components/src/lib/unsorted/viz/bubble/BubbleChart.stories.svelte
@@ -0,0 +1,54 @@
+<script context="module">
+	/** @type {import("@storybook/svelte").Meta}*/
+	export const meta = {
+		title: 'Charts/BarCharts',
+		argTypes: {
+			xHasGaps: {
+				type: 'boolean',
+				description: 'Determines if every series has every x value',
+				defaultValue: false
+			},
+			yHasNulls: {
+				type: 'boolean',
+				description: 'Determines if y can have nulls',
+				defaultValue: false
+			},
+			seriesAlwaysExists: {
+				type: 'boolean',
+				description: 'Determines if the series prop can be null',
+				defaultValue: true
+			},
+			type: {
+				type: 'string',
+				options: ['stacked', 'grouped', 'stacked100'],
+				control: { type: 'select' }
+			}
+		},
+		args: {
+			xHasGaps: false,
+			yHasNulls: false,
+			seriesAlwaysExists: true
+		}
+	};
+</script>
+
+<script>
+	import { Template, Story } from '@storybook/addon-svelte-csf';
+
+	import BarChart from './BarChart.svelte';
+
+	import { fakerSeries } from '$lib/faker-data-queries';
+</script>
+
+<Template let:args>
+	<BarChart
+		{...args}
+		x="x"
+		y="y"
+		series="series"
+		data={fakerSeries['numeric_series'][args.xHasGaps][args.yHasNulls][args.seriesAlwaysExists]
+			.store}
+	/>
+</Template>
+
+<Story name="Base" />
