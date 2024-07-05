17afd58917cc9bb501244732f0f1e54be8d18baf
Kyle Wong <kyle.wong917@gmail.com>
2024-07-05 10:23:47 -0400

    updated boxplot stories

--- packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
@@ -36,48 +36,54 @@
 	import { Template, Story } from '@storybook/addon-svelte-csf';
 
 	import BoxPlot from './BoxPlot.svelte';
-
-	import { fakerSeries } from '$lib/faker-data-queries';
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
 	import DataTable from '../table/DataTable.svelte';
-	const allFlights = Query.create(`SELECT * FROM numeric_series limit 500`, query);
+	const allFlights = Query.create(
+		`SELECT * FROM flights WHERE airline IN ('Qatar Airways', 'AirAsia') limit 50`,
+		query
+	);
 	const flightData = Query.create(
 		`
-SELECT distinct_airlines.airline, MAX(fare) AS max_fare, MIN(fare) AS min_fare, MEDIAN(fare) AS median_fare
+SELECT 
+    airline, 
+    MAX(distance) AS max_distance, 
+    MIN(COALESCE(distance, 0)) AS min_distance, 
+    MEDIAN(distance) AS median_distance,
+    CASE 
+        WHEN airline = 'Qatar Airways' THEN 'red' 
+        WHEN airline = 'AirAsia' THEN 'blue' 
+        ELSE 'gray' 
+    END AS color
 FROM flights
-JOIN (
-  SELECT DISTINCT airline
-  FROM flights
-  LIMIT 3
-) AS distinct_airlines ON flights.airline = distinct_airlines.airline
-GROUP BY distinct_airlines.airline
-limit 500`,
+WHERE airline IN ('Qatar Airways', 'AirAsia')
+GROUP BY airline, color
+limit 50`,
 		query
 	);
 </script>
 
 <Template let:args>
 	<BoxPlot
-		{...args}
-		intervalBottom="interval_bottom"
-		midpoint="median"
-		intervalTop="interval_top"
 		data={flightData}
-		yFmt="usd0"
+		name="airline"
+		intervalBottom="min_distance"
+		midpoint="median_distance"
+		intervalTop="max_distance"
 	/>
 </Template>
 
 <Story name="Base" />
 
-<Story name="test">
-	<DataTable data={allFlights} />
+<Story name="swapXY=true">
 	<DataTable data={flightData} />
 	<BoxPlot
 		data={flightData}
 		name="airline"
-		intervalBottom="min_fare"
-		midpoint="median_fare"
-		intervalTop="max_fare"
+		intervalBottom="min_distance"
+		midpoint="median_distance"
+		intervalTop="max_distance"
+		swapXY="true"
+		color="color"
 	/>
 </Story>
