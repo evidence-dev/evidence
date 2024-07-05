8782d9e950d3fc8678286dcbabb231234d8e6f61
Kyle Wong <kyle.wong917@gmail.com>
2024-07-05 10:57:32 -0400

    linted and formatted

--- packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
@@ -1,34 +1,7 @@
 <script context="module">
 	/** @type {import("@storybook/svelte").Meta}*/
 	export const meta = {
-		title: 'Charts/BoxPlots',
-		argTypes: {
-			xHasGaps: {
-				type: 'boolean',
-				description: 'Determines if every series has every x value',
-				defaultValue: false
-			},
-			yHasNulls: {
-				type: 'boolean',
-				description: 'Determines if y can have nulls',
-				defaultValue: false
-			},
-			seriesAlwaysExists: {
-				type: 'boolean',
-				description: 'Determines if the series prop can be null',
-				defaultValue: true
-			},
-			type: {
-				type: 'string',
-				options: ['stacked', 'grouped', 'stacked100'],
-				control: { type: 'select' }
-			}
-		},
-		args: {
-			xHasGaps: false,
-			yHasNulls: false,
-			seriesAlwaysExists: true
-		}
+		title: 'Charts/BoxPlots'
 	};
 </script>
 
@@ -39,10 +12,6 @@
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
 	import DataTable from '../table/DataTable.svelte';
-	const allFlights = Query.create(
-		`SELECT * FROM flights WHERE airline IN ('Qatar Airways', 'AirAsia') limit 50`,
-		query
-	);
 	const flightData = Query.create(
 		`
 SELECT 
@@ -63,7 +32,7 @@ limit 50`,
 	);
 </script>
 
-<Template let:args>
+<Template>
 	<BoxPlot
 		data={flightData}
 		name="airline"
