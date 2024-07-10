700059dc74704d7a9bf3bfd5283d3c49fced7275
Kyle Wong <kyle.wong917@gmail.com>
2024-07-10 13:38:02 -0400

    updated argTypes

--- packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
@@ -1,18 +1,130 @@
 <script context="module">
-	/** @type {import("@storybook/svelte").Meta}*/
+	/** @type {import('@storybook/addon-svelte-csf').MetaProps}*/
 	export const meta = {
-		title: 'Charts/BoxPlots'
+		title: 'Charts/BoxPlot',
+		component: BoxPlot,
+		argTypes: {
+			name: {
+				control: 'text'
+			},
+			min: {
+				control: 'text'
+			},
+			intervalBottom: {
+				control: 'text'
+			},
+			midpoint: {
+				control: 'text'
+			},
+			intervalTop: {
+				control: 'text'
+			},
+			max: {
+				control: 'text'
+			},
+			confidenceInterval: {
+				control: 'text'
+			},
+			emptySet: {
+				control: 'select',
+				options: ['pass', 'warn', 'error']
+			},
+			emptyMessage: {
+				control: 'text'
+			},
+			color: {
+				control: 'text'
+			},
+			yFmt: {
+				control: 'text'
+			},
+			xFmt: {
+				control: 'text'
+			},
+			seriesColors: {
+				control: 'object'
+			},
+			swapXY: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			xAxisTitle: {
+				control: 'text'
+			},
+			yAxisTitle: {
+				control: 'text'
+			},
+			xGridlines: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			yGridlines: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			xBaseline: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			yBaseline: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			xTickMarks: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			yTickMarks: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			yMin: {
+				control: 'number'
+			},
+			yMax: {
+				control: 'number'
+			},
+			showAllAxisLabels: {
+				control: 'boolean',
+				options: [true, false]
+			},
+			title: {
+				control: 'text'
+			},
+			subtitle: {
+				control: 'text'
+			},
+			chartAreaHeight: {
+				control: 'number'
+			},
+			renderer: {
+				control: 'select',
+				options: ['canvas', 'svg']
+			},
+			eChartsOptions: {
+				control: 'object'
+			},
+			seriesOptions: {
+				control: 'object'
+			},
+			printEchartsConfig: {
+				control: 'boolean',
+				options: ['true', 'false']
+			}
+		}
 	};
 </script>
 
 <script>
-	import { Template, Story } from '@storybook/addon-svelte-csf';
-
+	import { Story } from '@storybook/addon-svelte-csf';
 	import BoxPlot from './BoxPlot.svelte';
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
-	import DataTable from '../table/DataTable.svelte';
-	const data = Query.create(`SELECT * from flights`, query);
+	import { writable } from 'svelte/store';
+	import { setContext } from 'svelte';
+	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
+	const inputStore = writable({});
+	setContext(INPUTS_CONTEXT_KEY, inputStore);
 	const flightData = Query.create(
 		`
 SELECT 
@@ -20,9 +132,8 @@ SELECT
     MAX(fare) AS max_fare, 
     MIN(COALESCE(fare, 0)) AS min_fare, 
     MEDIAN(fare) AS median_fare,
-		MAX(fare)-500 AS intervalTop_fare,
-		MIN(fare)+500 AS intervalBottom_fare,
-
+	MAX(fare)-1000 AS intervalTop_fare,
+	MIN(fare)+1000 AS intervalBottom_fare,
     CASE 
         WHEN airline = 'Qatar Airways' THEN 'red' 
         WHEN airline = 'AirAsia' THEN 'blue' 
@@ -36,43 +147,47 @@ limit 50`,
 	);
 </script>
 
-<Template>
-	<BoxPlot
-		data={flightData}
-		name="airline"
-		intervalBottom="min_fare"
-		midpoint="median_fare"
-		intervalTop="max_fare"
-		title="Flights"
-	/>
-</Template>
-
-<Story name="Base" />
-
-<Story name="swapXY=true colors">
-	<DataTable data={data} />
-	<BoxPlot
-		data={flightData}
-		name="airline"
-		intervalBottom="intervalBottom_fare"
-		midpoint="median_fare"
-		intervalTop="intervalTop_fare"
-		swapXY="true"
-		color="color"
-		title="Flights: swapXY=true, colors"
-	/>
+<Story
+	name="Base"
+	args={{
+		name: 'airline',
+		intervalBottom: 'min_fare',
+		midpoint: 'median_fare',
+		intervalTop: 'max_fare',
+		yFmt:"usd0"
+	}}
+	let:args
+>
+	<BoxPlot {...args} data={flightData} />
 </Story>
 
-<Story name="whiskers">
-	<DataTable data={flightData} />
-	<BoxPlot
-		data={flightData}
-		name="airline"
-		intervalBottom="intervalBottom_fare"
-		midpoint="median_fare"
-		intervalTop="intervalTop_fare"
-		max="max_fare"
-		min="min_fare"
-		title="Flights: whiskers"
-	/>
+<Story
+	name="swapXY=true colors"
+	args={{
+		color: 'color',
+		swapXY: 'true',
+		name: 'airline',
+		intervalBottom: 'min_fare',
+		midpoint: 'median_fare',
+		intervalTop: 'max_fare',
+		yFmt:"usd0"
+	}}
+	let:args
+>
+	<BoxPlot {...args} data={flightData} />
+</Story>
+<Story
+	name="whiskers"
+	args={{
+		name: 'airline',
+		intervalBottom: 'intervalBottom_fare',
+		midpoint: 'median_fare',
+		intervalTop: 'intervalTop_fare',
+		max:"max_fare",
+		min:"min_fare",
+		yFmt:"usd0"
+	}}
+	let:args
+>
+	<BoxPlot {...args} data={flightData} />
 </Story>
