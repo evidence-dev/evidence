6119ca42adeab88ad90607f209066e86b09374bf
Adam <mcrascal@users.noreply.github.com>
2024-07-12 14:06:59 -0400

    lint

--- packages/lib/sdk/src/legacy-compat.js
+++ packages/lib/sdk/src/legacy-compat.js
@@ -5,4 +5,4 @@ import { rootCli } from './cli.root.js';
  * This function is provided to create a compatibility layer between the legacy Evidence CLI and the new SDK CLI
  * @returns {Promise<void>}
  */
-export const cli = () => runMain(rootCli)
+export const cli = () => runMain(rootCli);
