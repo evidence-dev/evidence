f5decad6b832adee2b85af54866f7599473023e1
Kyle Wong <kyle.wong917@gmail.com>
2024-07-12 10:42:41 -0400

    Created CalendarHeatmap Stories

--- packages/ui/core-components/src/lib/unsorted/viz/heatmap/CalendarHeatmap.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/heatmap/CalendarHeatmap.stories.svelte
@@ -3,7 +3,58 @@
 	export const meta = {
 		title: 'charts/CalendarHeatmap',
 		component: CalendarHeatmap,
+		argTypes: {
+			min: {
+				control: 'number'
+			},
+			max: {
+				control: 'number'
+			},
+			emptySet: {
+				control: 'select',
+				options: ['pass', 'warn', 'error']
+			},
+			emptyMessage: {
+				control: 'text'
+			},
+			colorPalette: {
+				control: 'text'
+			},
+			valueFmt: {
+				control: 'text'
+			},
+			yearLabel: {
+				control: 'boolean'
+			},
+			monthLabel: {
+				control: 'boolean'
+			},
+			dayLabel: {
+				control: 'boolean'
+			},
+			title: {
+				control: 'text'
+			},
+			subtitle: {
+				control: 'text'
+			},
+			chartAreaHeight: {
+				control: 'number'
+			},
+			legend: {
+				control: 'boolean'
+			},
+			filter: {
+				control: 'boolean'
+			},
+			renderer: {
+				control: 'select',
+				options: ['canvas', 'svg']
+			}
+		}
 	};
+
+	const data = Query.create(`SELECT departure_date, fare FROM flights limit 500`, query);
 </script>
 
 <script>
@@ -13,22 +64,21 @@
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
 	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
-    import CalendarHeatmap from './_CalendarHeatmap.svelte';
-
+	import CalendarHeatmap from './_CalendarHeatmap.svelte';
 
 	const inputStore = writable({});
 	setContext(INPUTS_CONTEXT_KEY, inputStore);
 </script>
 
-<Story
-	name="Basic hardcoded x,y"
-	args={{ x: 24, y: 514, label: 'Whoa look at this data!' }}
-	argTypes={{ x: { control: 'number' }, y: { control: 'number' } }}
-	let:args
->
-	{@const data = Query.create(`SELECT * FROM numeric_series WHERE series='pink'`, query)}
-	<LineChart x="x" y="y" {data}>
-		<ReferencePoint {...args} />
-	</LineChart>
+<Story name="Basic date and value" let:args>
+	<CalendarHeatmap {data} {...args} date="departure_date" value="fare" />
 </Story>
-
+<Story name="custom color palette" let:args>
+	<CalendarHeatmap
+		{data}
+		{...args}
+		date="departure_date"
+		value="fare"
+		colorPalette={['navy', 'lightyellow', 'purple']}
+	/>
+</Story>
