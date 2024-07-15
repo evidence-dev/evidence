23aa9aeb723bf09097e3e15671c7e998c043e763
Kyle Wong <kyle.wong917@gmail.com>
2024-07-15 12:33:10 -0400

    updated datatable story argTypes with columns args nested

--- packages/ui/core-components/src/lib/unsorted/viz/table/DataTable.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/table/DataTable.stories.svelte
@@ -5,21 +5,223 @@
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
 
+	const flightData = Query.create(`SELECT * from flights limit 50`, query);
+
 	/** @type {import("@storybook/svelte").Meta}*/
 	export const meta = {
 		title: 'Viz/Datatable',
-		component: DataTable
+		component: DataTable,
+		argTypes: {
+			rows: {
+				control: 'number'
+			},
+			headerColor: {
+				control: 'color'
+			},
+			headerFontColor: {
+				control: 'color'
+			},
+			totalRow: {
+				control: 'boolean'
+			},
+			totalRowColor: {
+				control: 'color'
+			},
+			totalFontColor: {
+				control: 'color'
+			},
+			rowNumber: {
+				control: 'boolean'
+			},
+			rowLines: {
+				control: 'boolean'
+			},
+			rowShading: {
+				control: 'boolean'
+			},
+			backgroundColor: {
+				control: 'color'
+			},
+			sortable: {
+				control: 'boolean'
+			},
+			search: {
+				control: 'boolean'
+			},
+			downloadable: {
+				control: 'boolean'
+			},
+			formatColumnTitles: {
+				control: 'boolean'
+			},
+			wrapTitles: {
+				control: 'boolean'
+			},
+			compact: {
+				control: 'boolean'
+			},
+			link: {
+				control: 'string'
+			},
+			showLinkCol: {
+				control: 'boolean'
+			},
+			generateMarkdown: {
+				control: 'boolean'
+			},
+			emptySet: {
+				control: 'boolean'
+			},
+			emptyMessage: {
+				control: 'string'
+			},
+			groupBy: {
+				control: 'string'
+			},
+			groupType: {
+				control: 'select',
+				options: ['accordion', 'section']
+			},
+			subtotals: {
+				control: 'boolean'
+			},
+			subtotalFmt: {
+				control: 'string'
+			},
+			groupsOpen: {
+				control: 'boolean'
+			},
+			accordionRowColor: {
+				control: 'color'
+			},
+			subtotalRowColor: {
+				control: 'color'
+			},
+			subtotalFontColor: {
+				control: 'color'
+			},
+			groupNamePosition: {
+				control: 'select',
+				options: ['top', 'middle', 'bottom']
+			},
+			columnArgs: {
+				title: {
+					control: 'string'
+				},
+				align: {
+					control: 'select',
+					options: ['left', 'center', 'right']
+				},
+				fmt: {
+					control: 'string'
+				},
+				fmtColumn: {
+					control: 'string'
+				},
+				totalAgg: {
+					control: 'select'
+				},
+				totalFmt: {
+					control: 'string'
+				},
+				weightCol: {
+					control: 'string'
+				},
+				wrap: {
+					control: 'boolean'
+				},
+				wrapTitle: {
+					control: 'boolean'
+				},
+				contentType: {
+					control: 'select',
+					options: ['link', 'image', 'delta', 'colorscale', 'html']
+				},
+				colGroup: {
+					control: 'string'
+				},
+				//contentType=images
+				height: {
+					control: 'number'
+				},
+				width: {
+					control: 'number'
+				},
+				alt: {
+					control: 'string'
+				},
+				//contentType=link
+				linkLabel: {
+					control: 'string'
+				},
+				openInNewTab: {
+					control: 'boolean'
+				},
+				//contentType=delta
+				deltaSymbol: {
+					control: 'boolean'
+				},
+				downIsGood: {
+					control: 'boolean'
+				},
+				showValue: {
+					control: 'boolean'
+				},
+				neutralMin: {
+					control: 'number'
+				},
+				neutralMax: {
+					control: 'number'
+				},
+				chip: {
+					control: 'boolean'
+				},
+				//contentType=colorscale
+				scaleColor: {
+					control: 'color'
+				},
+				colorMin: {
+					control: 'number'
+				},
+				colorMid: {
+					control: 'number'
+				},
+				colorMax: {
+					control: 'number'
+				},
+				colorBreakpoints: {
+					control: 'array'
+				},
+				scaleColumn: {
+					control: 'string'
+				}
+			}
+		}
 	};
 </script>
 
-<Story name="Simple Case">
-	{@const data = Query.create(`SELECT * from flights LIMIT 1000`, query)}
-	<DataTable {data} />
+<Story name="Simple Case" let:args>
+	<DataTable data={flightData} {...args} />
 </Story>
 
-<Story name="With Search">
-	{@const data = Query.create(`SELECT * from flights`, query)}
-	<DataTable {data} title="Flights" search>
+<Story name="Selecting Specific Columns" let:args>
+	<DataTable data={flightData} {...args}>
+		<Column id="airline" title="Airline" />
+		<Column id="fare" title="fare" />
+		<Column id="departure_airport" title="Departure Airport" />
+	</DataTable>
+</Story>
+
+<Story name="Custom Column Formatting" let:args>
+	<DataTable data={flightData} {...args}>
+		<Column id="airline" title="Airline" />
+		<Column id="fare" title="fare" {...args} />
+		<Column id="departure_airport" title="Departure Airport" />
+	</DataTable>
+</Story>
+
+<Story name="With Search" args={{ search: true }} let:args>
+	<DataTable data={flightData} title="Flights" {...args}>
 		<Column id="id" title="ID" />
 		<Column id="airline" title="Airline" />
 		<Column id="departure_airport" title="Departure Airport" />
