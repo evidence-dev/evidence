7ae51df0aa54b84cd9fc026370baef48f0b905c6
Zachary Stence <zachstence@gmail.com>
2024-07-24 16:52:04 -0500

    test: evidence page should have title

--- e2e/basic/playwright.config.js
+++ e2e/basic/playwright.config.js
@@ -67,12 +67,12 @@ export default defineConfig({
 		//   name: 'Google Chrome',
 		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
 		// },
-	]
+	],
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   url: 'http://127.0.0.1:3000',
-	//   reuseExistingServer: !process.env.CI,
-	// },
+	webServer: {
+		command: process.env.DEV ? 'pnpm dev' : 'pnpm build && pnpm preview',
+		port: 3000,
+		reuseExistingServer: !process.env.CI
+	}
 });
