cb4d6d70e6e1470214be7cbde487a01f3f1bf7f8
Zachary Stence <zachstence@gmail.com>
2024-07-25 14:22:30 -0500

    test: creating page hmr

--- e2e/fs-utils.js
+++ e2e/fs-utils.js
@@ -1,13 +1,22 @@
 import fs from 'node:fs';
+import path from 'node:path';
 
 /** @type {Map<string, string>} */
 const originalFiles = new Map();
+
+/** @type {Set<string>} */
+const createdFiles = new Set();
 
 export const restoreChangedFiles = () => {
 	originalFiles.forEach((content, file) => {
 		fs.writeFileSync(file, content, 'utf-8');
 	});
 	originalFiles.clear();
+
+	createdFiles.forEach((file) => {
+		fs.unlinkSync(file);
+	});
+	createdFiles.clear();
 };
 
 /**
@@ -21,4 +30,10 @@ export const editFile = (filePath, replacer) => {
 	}
 	const newContent = replacer(content);
 	fs.writeFileSync(filePath, newContent);
+};
+
+export const createFile = (filePath, content) => {
+	fs.mkdirSync(path.dirname(filePath), { recursive: true });
+	fs.writeFileSync(filePath, content);
+	createdFiles.add(filePath);
 };
--- e2e/hmr/tests/tests.spec.js
+++ e2e/hmr/tests/tests.spec.js
@@ -1,6 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-import { editFile, restoreChangedFiles } from '../../fs-utils';
+import { createFile, editFile, restoreChangedFiles } from '../../fs-utils';
 
 test.afterAll(() => {
 	restoreChangedFiles();
@@ -13,4 +13,17 @@ test('editing a page should HMR', async ({ page }) => {
 	editFile('pages/page.md', (content) => content.replace('some text', 'some different text'));
 
 	await expect(page.getByText('This page has some different text on it')).toBeVisible();
+});
+
+test('creating a new page should add it to the sidebar and allow navigation to it', async ({
+	page
+}) => {
+	await page.goto('/');
+	await expect(page.getByText('Index')).toBeVisible();
+
+	createFile('pages/new-page.md', 'This is a new page');
+
+	await expect(page.getByRole('link', { name: 'New Page' })).toBeVisible();
+	await page.goto('/new-page');
+	await expect(page.getByText('This is a new page')).toBeVisible();
 });
