57a5253a8ec3a537d60af049e888dc108e4810e4
Zachary Stence <zachstence@gmail.com>
2024-07-25 16:37:59 -0500

    test: record video on first retry

--- e2e/basic/playwright.config.js
+++ e2e/basic/playwright.config.js
@@ -28,7 +28,9 @@ export default defineConfig({
 		// baseURL: 'http://127.0.0.1:3000',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: 'on-first-retry'
+		trace: 'on-first-retry',
+
+		video: 'on-first-retry'
 	},
 
 	/* Configure projects for major browsers */
