160299bb64551b5ae9a0352c3d57ca3f521c3d57
Zachary Stence <zachstence@gmail.com>
2024-07-30 11:23:48 -0500

    test: rename and nest in describe

--- e2e/hmr/tests/tests.spec.js
+++ e2e/hmr/tests/tests.spec.js
@@ -14,43 +14,41 @@ test.afterEach(() => {
 	restoreChangedFiles();
 });
 
-test('editing a page should HMR', async ({ page }) => {
-	await page.goto('/page');
-	await waitForDevModeToLoad(page);
+test.describe('pages', () => {
+	test('editing should HMR', async ({ page }) => {
+		await page.goto('/page');
+		await waitForDevModeToLoad(page);
 
-	await expect(page.getByText('This page has some text on it')).toBeVisible();
+		await expect(page.getByText('This page has some text on it')).toBeVisible();
 
-	editFile('pages/page.md', (content) => content.replace('some text', 'some different text'));
+		editFile('pages/page.md', (content) => content.replace('some text', 'some different text'));
 
-	await expect(page.getByText('This page has some different text on it')).toBeVisible();
-});
+		await expect(page.getByText('This page has some different text on it')).toBeVisible();
+	});
 
-test('creating a new page should add it to the sidebar and allow navigation to it', async ({
-	page
-}) => {
-	await page.goto('/');
-	await waitForDevModeToLoad(page);
+	test('creating should add to the sidebar and allow navigation', async ({ page }) => {
+		await page.goto('/');
+		await waitForDevModeToLoad(page);
 
-	await expect(page.getByText('Index')).toBeVisible();
+		await expect(page.getByText('Index')).toBeVisible();
 
-	createFile('pages/new-page.md', 'This is a new page');
+		createFile('pages/new-page.md', 'This is a new page');
 
-	await expect(page.getByRole('link', { name: 'New Page' })).toBeVisible();
-	await page.goto('/new-page');
-	await expect(page.getByText('This is a new page')).toBeVisible();
-});
+		await expect(page.getByRole('link', { name: 'New Page' })).toBeVisible();
+		await page.goto('/new-page');
+		await expect(page.getByText('This is a new page')).toBeVisible();
+	});
 
-test('deleting a page should remove it from the sidebar and prevent navigation to it', async ({
-	page
-}) => {
-	await page.goto('/');
-	await waitForDevModeToLoad(page);
+	test('deleting should remove from the sidebar and prevent navigation', async ({ page }) => {
+		await page.goto('/');
+		await waitForDevModeToLoad(page);
 
-	await expect(page.getByText('Index')).toBeVisible();
+		await expect(page.getByText('Index')).toBeVisible();
 
-	deleteFile('pages/page.md');
+		deleteFile('pages/page.md');
 
-	await expect(page.getByRole('link', { name: 'Page' })).not.toBeVisible();
-	await page.goto('/page');
-	await expect(page.getByText('Page Not Found')).toBeVisible();
+		await expect(page.getByRole('link', { name: 'Page' })).not.toBeVisible();
+		await page.goto('/page');
+		await expect(page.getByText('Page Not Found')).toBeVisible();
+	});
 });
