6386d58027a91c3c46dbdf783ee24c10b198ecfa
Zachary Stence <zachstence@gmail.com>
2024-08-14 11:32:52 -0500

    test: story for area map with changing geoJsonUrl

--- packages/ui/core-components/src/lib/unsorted/viz/map/AreaMap.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/map/AreaMap.stories.svelte
@@ -7,11 +7,16 @@
 </script>
 
 <script>
+	import { getContext } from 'svelte';
 	import { Story } from '@storybook/addon-svelte-csf';
 	import { Query } from '@evidence-dev/sdk/usql';
 	import { query } from '@evidence-dev/universal-sql/client-duckdb';
+	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
+	import { Dropdown, DropdownOption } from '../../../atoms/inputs/dropdown/index.js';
 
 	import AreaMap from './AreaMap.svelte';
+
+	const inputStore = getContext(INPUTS_CONTEXT_KEY);
 
 	/** @type {typeof query} */
 	const slowQuery = async (...args) => {
@@ -28,6 +33,8 @@
 		`select * from la_zip_sales where zip_code <> 90704 order by 1 limit 100`,
 		slowQuery
 	);
+
+	const state_sales = Query.create(`select * from state_sales`, query);
 </script>
 
 <Story name="Basic Usage" parameters={{ chromatic: { disableSnapshot: true } }}>
@@ -36,4 +43,21 @@
 
 <Story name="Loading" parameters={{ chromatic: { disableSnapshot: true } }}>
 	<AreaMap data={slow_la_zip_sales} geoId="ZCTA5CE10" value="sales" areaCol="zip_code" />
+</Story>
+
+<Story name="Reactive geoJsonUrl">
+	<Dropdown name="dropdown">
+		<DropdownOption value="la" valueLabel="Sales by LA ZIP Code" default />
+		<DropdownOption value="states" valueLabel="Orders by State" />
+	</Dropdown>
+
+	<AreaMap
+		data={$inputStore.dropdown.value === 'la' ? la_zip_sales : state_sales}
+		geoJsonUrl={$inputStore.dropdown.value === 'la'
+			? 'https://evd-geojson.b-cdn.net/ca_california_zip_codes_geo_1.min.json'
+			: 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_110m_admin_1_states_provinces_shp.geojson'}
+		geoId={$inputStore.dropdown.value === 'la' ? 'ZCTA5CE10' : 'name'}
+		areaCol={$inputStore.dropdown.value === 'la' ? 'zip_code' : 'state'}
+		value={$inputStore.dropdown.value === 'la' ? 'sales' : 'orders'}
+	/>
 </Story>
