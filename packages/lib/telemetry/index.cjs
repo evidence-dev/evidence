c24f8ad0331b84da0a4c16ecf098aaa90dfd0d1e
Brian D <contact@noimbrian.com>
2024-08-15 16:39:33 -0500

    chore: formatting

--- packages/lib/telemetry/index.cjs
+++ packages/lib/telemetry/index.cjs
@@ -96,10 +96,10 @@ const logEvent = async (
 ) => {
 	try {
 		const usageStats = settings
-			? settings.send_anonymous_usage_stats ?? 'yes'
-			: process.env['SEND_ANONYMOUS_USAGE_STATS'] ??
+			? (settings.send_anonymous_usage_stats ?? 'yes')
+			: (process.env['SEND_ANONYMOUS_USAGE_STATS'] ??
 				process.env['send_anonymous_usage_stats'] ??
-				'yes';
+				'yes');
 		let repo;
 		let database;
 		let demoDb;
