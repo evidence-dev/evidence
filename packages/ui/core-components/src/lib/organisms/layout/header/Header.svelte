a13c6b5972a9a146136b0209d18e62543f561002
Adam <mcrascal@users.noreply.github.com>
2024-08-22 10:46:57 -0400

    color on the x logo

--- packages/ui/core-components/src/lib/organisms/layout/header/Header.svelte
+++ packages/ui/core-components/src/lib/organisms/layout/header/Header.svelte
@@ -79,7 +79,7 @@
 				{#if xProfile}
 					<a
 						href={xProfile}
-						class="hover:bg-muted rounded-lg p-2 transition-all duration-200"
+						class="hover:bg-muted rounded-lg p-2 transition-all duration-200 "
 						target="_blank"
 						rel="noreferrer"
 					>
@@ -87,7 +87,7 @@
 							role="img"
 							viewBox="0 0 24 24"
 							xmlns="http://www.w3.org/2000/svg"
-							class="w-4 h-4 text-foreground"
+							class="w-4 h-4 fill-foreground"
 							><path
 								d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
 							/>
