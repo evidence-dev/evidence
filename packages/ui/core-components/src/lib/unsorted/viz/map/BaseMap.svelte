20053f697983a1d5e0ff2e33d71f66a1c6485b76
Adam <mcrascal@users.noreply.github.com>
2024-08-22 12:48:30 -0400

    base map

--- packages/ui/core-components/src/lib/unsorted/viz/map/BaseMap.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/map/BaseMap.svelte
@@ -26,12 +26,16 @@
 
 	// Determine if the initial view is user-defined
 	const userDefinedView = startingLat || startingLong;
-
+	
 	/** @type {number} */
 	export let height = 300; // height in pixels
-
+	
+	if (browser) {
+		const isDarkMode = document.documentElement.style.colorScheme === 'dark';
+	}
+	
 	/** @type {string} */
-	export let basemap = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png';
+	export let basemap = `https://{s}.basemaps.cartocdn.com/${isDarkMode ? 'light' : 'dark'}_all/{z}/{x}/{y}{r}.png`;
 
 	/** @type {string|undefined} */
 	export let title = undefined;
