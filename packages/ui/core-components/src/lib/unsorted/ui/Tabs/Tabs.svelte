b4a944a5b654fd3be3480e5f6dde0b51af836fb0
Adam <mcrascal@users.noreply.github.com>
2024-08-22 11:23:38 -0400

    feat: Add dark mode support to Tabs component

--- packages/ui/core-components/src/lib/unsorted/ui/Tabs/Tabs.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/Tabs/Tabs.svelte
@@ -8,8 +8,8 @@
 
 	const classes = {
 		notActive:
-			'border-gray-100 text-gray-600 border-b-2 bg-gray-50 border-b-gray-200 hover:text-gray-800 hover:bg-gray-200 hover:border-b-gray-400',
-		active: 'text-black border-b-2 border-[--borderColor] bg-[--bgColor]'
+			'border-border text-muted-foreground border-b-2 bg-muted hover:text-foreground hover:bg-secondary hover:border-b-primary',
+		active: 'text-foreground border-b-2 border-primary bg-background'
 	};
 
 	/**
@@ -17,43 +17,6 @@
 	 * @type {string}
 	 */
 	export let id;
-
-	/**
-	 * color can be provided to set custom background color for active tab.
-	 * @type {string}
-	 */
-	export let color = 'hsla(207, 65%, 39%, 1)';
-	color = color.replace(/\s+/g, ''); // clean string
-
-	const bgColor = isValidColorString(color) ? addOpacityToColor(color) : 'hsla(207, 65%, 39%, 0.1)';
-	const borderColor = isValidColorString(color) ? color : 'hsla(207, 65%, 39%, 1)';
-
-	function isHex(inputColor) {
-		const hexRegex = /^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/i;
-		return hexRegex.test(inputColor);
-	}
-
-	function isRGB(inputColor) {
-		const rgbRegex = /^rgb\(\s*(\d{1,3})\s*,\s*(\d{1,3})\s*,\s*(\d{1,3})\s*\)$/i;
-		return rgbRegex.test(inputColor);
-	}
-
-	function isHSL(inputColor) {
-		const hslRegex = /^hsl\(\s*(\d{1,3})\s*,\s*(\d{1,3}%)\s*,\s*(\d{1,3}%)\s*\)$/i;
-		return hslRegex.test(inputColor);
-	}
-
-	function isValidColorString(inputColor) {
-		return isHex(inputColor) || isRGB(inputColor) || isHSL(inputColor);
-	}
-
-	function addOpacityToColor(colorString) {
-		if (isHex(colorString)) {
-			return colorString + '1a';
-		} else if (isRGB(colorString) || isHSL(colorString)) {
-			return colorString.replace(/(\)|\s|$)/, ', 0.1$1');
-		}
-	}
 
 	/** @type {import('./index.d.ts').TabsContext} */
 	const context = writable({ tabs: [], active: null });
@@ -90,10 +53,8 @@
 	<nav class="my-6 flex flex-wrap gap-x-1 gap-y-1">
 		{#each $context.tabs as tab}
 			<button
-				style:--bgColor={bgColor}
-				style:--borderColor={borderColor}
 				on:click={() => ($context.activeId = tab.id)}
-				class="mt-2 p-2 rounded-t flex-1 text-sm font-sans whitespace-nowrap transition ease-in duration-200 active:bg-gray-100 {$context.activeId ===
+				class="mt-2 p-2 rounded-t flex-1 text-sm font-sans whitespace-nowrap transition ease-in duration-200 {$context.activeId ===
 				tab.id
 					? classes.active
 					: classes.notActive}"
@@ -102,7 +63,7 @@
 			</button>
 		{/each}
 	</nav>
-	<div class="text-base">
+	<div class="text-foreground">
 		<slot />
 	</div>
 </section>
