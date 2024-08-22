91c8f83f4589eaddc69d11a0096aaca33fa28b58
Adam <mcrascal@users.noreply.github.com>
2024-08-22 13:06:09 -0400

    tw biglink

--- packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
@@ -6,40 +6,10 @@
 	export let href;
 </script>
 
-<a {href}>
-	<div>
+<a {href} >
+	<div class="border rounded-md px-2 py-2 text-sm mt-4 mb-5 transition-all duration-200 hover:text-blue-700 hover:shadow-sm">
 		<span>
 			<slot />
 		</span>
 	</div>
 </a>
-
-<style>
-	div {
-		border: 1px solid var(--grey-200);
-		border-radius: 6px;
-		padding: 0.3em 0.6em;
-		font-family: var(--ui-font-family);
-		font-size: 0.8em;
-		margin-top: 1em;
-		margin-bottom: 1.25em;
-		cursor: pointer;
-		transition: all 400ms;
-	}
-
-	a {
-		text-decoration: none;
-		color: var(--blue-600);
-	}
-
-	a:hover {
-		text-decoration: none;
-		color: var(--blue-700);
-	}
-
-	div:hover {
-		border: 1px solid var(--grey-300);
-		transition: all 400ms;
-		box-shadow: 0 5px 5px 2px hsl(0deg 0% 97%);
-	}
-</style>
