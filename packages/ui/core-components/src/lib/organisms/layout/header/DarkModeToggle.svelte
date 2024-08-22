4dc1207bd341ef6eb452a36561bf2ed4ea30c4b3
Adam <mcrascal@users.noreply.github.com>
2024-08-22 10:39:56 -0400

    feat: update DarkModeToggle component to use color tokens

--- packages/ui/core-components/src/lib/organisms/layout/header/DarkModeToggle.svelte
+++ packages/ui/core-components/src/lib/organisms/layout/header/DarkModeToggle.svelte
@@ -15,18 +15,18 @@
 		<Button builders={[builder]} variant="ghost" size="icon">
 			<Icon
 				src={Sun}
-				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-200 dark:-rotate-90 dark:scale-0"
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all duration-200 dark:-rotate-90 dark:scale-0 text-foreground"
 			/>
 			<Icon
 				src={Moon}
-				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100 "
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all duration-200 dark:rotate-0 dark:scale-100 text-foreground"
 			/>
 			<span class="sr-only">Toggle dark or light mode</span>
 		</Button>
 	</DropdownMenu.Trigger>
 	<DropdownMenu.Content align="end">
-		<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
-		<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
-		<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => setMode('light')} class="text-foreground">Light</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => setMode('dark')} class="text-foreground">Dark</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => resetMode()} class="text-foreground">System</DropdownMenu.Item>
 	</DropdownMenu.Content>
 </DropdownMenu.Root>
