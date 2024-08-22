3e0920f7854f70f8beac15dd50e44b38ffa8b686
Adam <mcrascal@users.noreply.github.com>
2024-08-22 13:06:31 -0400

    feat: Add dark mode support to BigLink component

--- packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
@@ -6,8 +6,8 @@
 	export let href;
 </script>
 
-<a {href} >
-	<div class="border rounded-md px-2 py-2 text-sm mt-4 mb-5 transition-all duration-200 hover:text-blue-700 hover:shadow-sm">
+<a {href}>
+	<div class="border rounded-md px-2 py-2 text-sm mt-4 mb-5 transition-all duration-200 hover:text-primary hover:shadow-sm dark:border-border dark:hover:text-primary-foreground">
 		<span>
 			<slot />
 		</span>
