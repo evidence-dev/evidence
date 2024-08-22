8c488e56d71434a5b26290875cab9244729b0235
Adam <mcrascal@users.noreply.github.com>
2024-08-22 14:52:00 -0400

    feat: update badge component to support dark mode

--- packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
+++ packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
@@ -2,12 +2,12 @@ import { tv } from 'tailwind-variants';
 
 export { default as Badge } from './badge.svelte';
 export const badgeVariants = tv({
-	base: 'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 select-none',
+	base: 'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 select-none',
 	variants: {
 		variant: {
-			default: 'border-transparent bg-gray-900 text-white shadow hover:bg-gray-900/80',
-			secondary: 'border-transparent bg-gray-100 text-gray-900 hover:bg-gray-100/80',
-			destructive: 'border-transparent bg-red-500 text-red-50 shadow hover:bg-red-500/80',
+			default: 'border-transparent bg-primary text-primary-foreground hover:bg-primary/80',
+			secondary: 'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+			destructive: 'border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80',
 			outline: 'text-foreground'
 		}
 	},
