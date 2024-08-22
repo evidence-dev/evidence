84a05dfea26b02b8e5cc499e57710b8c306e9712
Adam <mcrascal@users.noreply.github.com>
2024-08-22 10:50:46 -0400

    feat: update button component to use color tokens from base layer

--- packages/ui/core-components/src/lib/atoms/shadcn/button/index.js
+++ packages/ui/core-components/src/lib/atoms/shadcn/button/index.js
@@ -2,15 +2,15 @@ import { tv } from 'tailwind-variants';
 import Root from './button.svelte';
 
 const buttonVariants = tv({
-	base: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-400 disabled:pointer-events-none disabled:opacity-50',
+	base: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
-			default: 'bg-gray-900 text-white shadow hover:bg-gray-900/90',
-			destructive: 'bg-red-500 text-red-50 shadow-sm hover:bg-red-500/90',
-			outline: 'border border-input bg-transparent shadow-sm hover:bg-gray-100 hover:text-gray-900',
-			secondary: 'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80',
-			ghost: 'hover:bg-gray-100 hover:text-gray-900',
-			link: 'text-gray-900 underline-offset-4 hover:underline'
+			default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+			destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
+			outline: 'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+			secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+			ghost: 'hover:bg-accent hover:text-accent-foreground',
+			link: 'text-primary underline-offset-4 hover:underline'
 		},
 		size: {
 			default: 'h-9 px-4 py-2',
