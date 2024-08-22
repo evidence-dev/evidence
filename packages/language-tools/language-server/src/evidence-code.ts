e27702430e8051fa93942c3b1945020a8a7a1ade
Zachary Stence <zachstence@gmail.com>
2024-08-22 16:25:45 -0400

    feat: use svelte parser to split md file into svelte components

--- packages/language-tools/language-server/src/evidence-code.ts
+++ packages/language-tools/language-server/src/evidence-code.ts
@@ -1,5 +1,6 @@
 import { CodeMapping, VirtualCode } from '@volar/language-core';
 import { IScriptSnapshot } from 'typescript';
+import { parse as parseSvelte } from 'svelte/compiler';
 
 export class EvidenceCode implements VirtualCode {
 	id = 'root';
@@ -42,27 +43,38 @@ export class EvidenceCode implements VirtualCode {
 	}
 
 	static getEmbeddedCodes(snapshot: IScriptSnapshot): VirtualCode[] {
-		return [
-			{
-				id: 'something',
-				languageId: 'svelte',
-				mappings: [
-					{
-						sourceOffsets: [0],
-						generatedOffsets: [0],
-						lengths: [snapshot.getLength()],
-						data: {
-							completion: true,
-							format: true,
-							navigation: true,
-							semantic: true,
-							structure: true,
-							verification: true
-						}
+		const snapshotContent = snapshot.getText(0, snapshot.getLength());
+		const svelteAst = parseSvelte(snapshotContent);
+		const svelteComponents = svelteAst.html.children?.filter(
+			(node) => node.type === 'InlineComponent'
+		);
+
+		const embeddedSvelteCodes = svelteComponents?.map((component, index) => ({
+			id: `svelte-${index}`,
+			languageId: 'svelte',
+			snapshot: {
+				getText: (start: number, end: number) =>
+					snapshotContent.substring(component.start + start, component.start + end),
+				getLength: () => component.end - component.start,
+				getChangeRange: () => undefined
+			},
+			mappings: [
+				{
+					sourceOffsets: [component.start],
+					generatedOffsets: [0],
+					lengths: [component.end - component.start],
+					data: {
+						completion: true,
+						format: true,
+						navigation: true,
+						semantic: true,
+						structure: true,
+						verification: true
 					}
-				],
-				snapshot
-			}
-		];
+				}
+			]
+		}));
+
+		return [...(embeddedSvelteCodes ?? [])];
 	}
 }
