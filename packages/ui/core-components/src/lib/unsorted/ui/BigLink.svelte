2aa93a80e6319452473d4fe8ea91f0efd06cda43
Adam <mcrascal@users.noreply.github.com>
2024-08-22 13:07:16 -0400

    fix: ensure text remains visible during hover in dark mode

--- packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
@@ -7,7 +7,7 @@
 </script>
 
 <a {href}>
-	<div class="border rounded-md px-2 py-2 text-sm mt-4 mb-5 transition-all duration-200 hover:text-primary hover:shadow-sm dark:border-border dark:hover:text-primary-foreground">
+	<div class="border rounded-md px-2 py-2 text-sm mt-4 mb-5 transition-all duration-200 hover:text-primary hover:shadow-sm dark:border-border dark:hover:text-primary dark:text-foreground">
 		<span>
 			<slot />
 		</span>
