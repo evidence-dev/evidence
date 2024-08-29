5922bf15eafca6daeb02553f9ea2fde87543bfa8
Kyle Wong <kyle.wong917@gmail.com>
2024-08-29 15:52:19 -0400

    updated nameGenerator test

--- sites/docs/components/PropListing.spec.js
+++ sites/docs/components/PropListing.spec.js
@@ -7,8 +7,7 @@ describe('updateIdName', () => {
 		let namesArray = ['name', 'name', 'name', 'name'];
 
 		for (const name of namesArray) {
-			let idName = name;
-			nameGenerator(name, idName, names);
+			nameGenerator(name, names);
 		}
 		namesArray = Array.from(names);
 		expect(namesArray).toEqual(['name', 'name-2', 'name-3', 'name-4']);
