9d982af8164c33dd90e1a9814570abf2195e7a09
Kyle Wong <kyle.wong917@gmail.com>
2024-09-03 14:00:54 -0400

    chore: lint+ format

--- packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/box/BoxPlot.stories.svelte
@@ -122,7 +122,6 @@
 	import { writable } from 'svelte/store';
 	import { setContext } from 'svelte';
 	import { INPUTS_CONTEXT_KEY } from '@evidence-dev/component-utilities/globalContexts';
-	import QueryLoad from '$lib/atoms/query-load/QueryLoad.svelte';
 	const inputStore = writable({});
 	setContext(INPUTS_CONTEXT_KEY, inputStore);
 	const flightData = Query.create(
