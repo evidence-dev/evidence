4101bdaf5e153665fe968fb3752138eb6c1574f6
Zachary Stence <zachstence@gmail.com>
2024-09-05 11:13:30 -0500

    test: templated page in e2e/spa

--- e2e/spa/tests/tests.spec.js
+++ e2e/spa/tests/tests.spec.js
@@ -2,9 +2,9 @@
 import { test, expect } from '@playwright/test';
 import { waitForDevModeToLoad } from '../../test-utils';
 
-test('has title', async ({ page }) => {
-	await page.goto('/');
+test('templated page uses query param', async ({ page }) => {
+	const param = 'bing-bong';
+	await page.goto(`/${param}`);
 	await waitForDevModeToLoad(page);
-
-	await expect(page).toHaveTitle(/Welcome to Evidence/);
+	await expect(page.getByText(`This page uses a query param: ${param}`)).toBeVisible();
 });
