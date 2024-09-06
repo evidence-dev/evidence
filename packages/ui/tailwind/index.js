f50f8c4c2759340303ef5733d021a46a7b05fdd1
Zachary Stence <zachstence@gmail.com>
2024-09-06 12:26:32 -0500

    chore: remove unused color variables

--- packages/ui/tailwind/index.js
+++ packages/ui/tailwind/index.js
@@ -1,37 +1,11 @@
 import { createThemes } from 'tw-colors';
 
-export const colors = {
-	color: {
-		1: '#923d59',
-		2: '#488f96',
-		3: '#518eca',
-		4: '#B3A9A0',
-		5: '#3F6C51',
-		6: '#ffc857',
-		7: '#495867',
-		8: '#bfdbf7',
-		9: '#bc4749',
-		10: '#EEEBD0',
-		11: '#F68E5F',
-		12: '#923d59',
-		13: '#488f96',
-		14: '#518eca',
-		15: '#B3A9A0',
-		16: '#3F6C51',
-		17: '#ffc857',
-		18: '#495867',
-		19: '#bfdbf7',
-		20: '#bc4749'
-	}
-};
-
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 export const config = {
 	theme: {
 		extend: {
-			colors,
 			fontFamily: {
 				sans: [
 					`Inter, ${defaultTheme.fontFamily.sans}`,
