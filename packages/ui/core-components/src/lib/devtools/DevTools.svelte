f4347514df6f33a134f14891c1f571b3a2dcd09f
Brian D <contact@noimbrian.com>
2024-09-09 11:28:45 -0500

    fix: silly mistake

--- packages/ui/core-components/src/lib/devtools/DevTools.svelte
+++ packages/ui/core-components/src/lib/devtools/DevTools.svelte
@@ -1,14 +1,14 @@
 <script>
-	import { building, dev } from '$app/environment';
+	import { dev } from '$app/environment';
 	import DevTools from './_DevTools.svelte';
 
 	// This wrapper is used to prevent devtools from escaping dev mode
 </script>
 
-{#if building}
-	<slot />
-{:else if dev}
+{#if dev}
 	<DevTools>
 		<slot />
 	</DevTools>
+{:else}
+	<slot />
 {/if}
