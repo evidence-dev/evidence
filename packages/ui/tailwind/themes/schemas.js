b10c57853ca4bea196056efee16ed1d113ed1ce0
Zachary Stence <zachstence@gmail.com>
2024-09-10 16:58:37 -0500

    fix: schema supports empty theme(s)

--- packages/ui/tailwind/themes/schemas.js
+++ packages/ui/tailwind/themes/schemas.js
@@ -1,12 +1,18 @@
 import z from 'zod';
 
 export const ThemesConfigSchema = z.object({
-	themes: z.record(
-		z.string(),
-		z.object({
-			mySemanticColor: z.string().optional()
-		})
-	)
+	themes: z
+		.record(
+			z.string(),
+			z
+				.object({
+					mySemanticColor: z.string().optional()
+				})
+				.nullish()
+				.transform((value) => value ?? {})
+		)
+		.nullish()
+		.transform((value) => value ?? {})
 });
 
 /** @typedef {z.infer<typeof ThemesConfigSchema>['themes']} Themes */
