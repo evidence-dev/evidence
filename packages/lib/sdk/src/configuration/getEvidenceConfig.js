435825ca81f25c1373579e9d3277dfd864e2911e
Zachary Stence <zachstence@gmail.com>
2024-09-10 15:25:58 -0500

    refactor: getEvidenceConfig is synchronous

--- packages/lib/sdk/src/configuration/getEvidenceConfig.js
+++ packages/lib/sdk/src/configuration/getEvidenceConfig.js
@@ -1,25 +1,24 @@
-import { findFile } from 'pkg-types';
+import fs from 'node:fs';
+import path from 'node:path';
 import yaml from 'yaml';
-import fs from 'fs/promises';
 import { EvidenceConfigSchema } from './schemas/config.schema.js';
 import { EvidenceError } from '../lib/EvidenceError.js';
 import { getEvidenceConfigLegacy } from './getEvidenceConfig.legacy.js';
+import { projectRoot } from '../lib/projectPaths.js';
 
 /**
- * @returns {Promise<import("zod").infer<typeof EvidenceConfigSchema>>}
+ * @returns {import("zod").infer<typeof EvidenceConfigSchema>}
  */
-export const getEvidenceConfig = async () => {
+export const getEvidenceConfig = () => {
 	try {
-		const configFileContent = await fs.readFile(
-			await findFile('evidence.config.yaml', { reverse: true }),
-			'utf-8'
-		);
+		const configFilePath = path.join(projectRoot, 'evidence.config.yaml');
+		const configFileContent = fs.readFileSync(configFilePath, 'utf-8');
 
 		const result = yaml.parse(configFileContent.replaceAll(/($|\s)(@.+):/g, '$1"$2":'));
 		return EvidenceConfigSchema.parse(result);
 	} catch (e) {
 		if (e instanceof Error && e.message.startsWith('Cannot find matching evidence.config.yaml')) {
-			return await getEvidenceConfigLegacy();
+			return getEvidenceConfigLegacy();
 		}
 		throw new EvidenceError('Unknown Error while loading Evidence Configuration', [], { cause: e });
 	}
