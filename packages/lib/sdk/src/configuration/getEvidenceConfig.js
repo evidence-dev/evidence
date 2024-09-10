07e478b88b8562869eae5a9d86963e39c42c79e3
Zachary Stence <zachstence@gmail.com>
2024-09-10 16:32:17 -0500

    refactor: getEvidenceConfig optionally accepts a schema

--- packages/lib/sdk/src/configuration/getEvidenceConfig.js
+++ packages/lib/sdk/src/configuration/getEvidenceConfig.js
@@ -6,16 +6,26 @@ import { EvidenceError } from '../lib/EvidenceError.js';
 import { getEvidenceConfigLegacy } from './getEvidenceConfig.legacy.js';
 import { projectRoot } from '../lib/projectPaths.js';
 
+/** @typedef {import("zod").AnyZodObject} AnyZodObject */
+
 /**
- * @returns {import("zod").infer<typeof EvidenceConfigSchema>}
+ * @template {AnyZodObject} [Schema=EvidenceConfigSchema]
+ * @param {Schema} [schema]
+ * @returns {import("zod").infer<Schema>}
  */
-export const getEvidenceConfig = () => {
+export const getEvidenceConfig = (
+	// The typecasting here is necessary because Typescript
+	// https://stackoverflow.com/a/59363875/9080819
+	// Technically, this function could be misused by passing a template type
+	// that doesn't match the default parameter, but that shouldn't happen
+	// with normal usage (especially since we're using JS, not TS directly)
+	schema = /** @type {Schema} */ (/** @type {unknown} */ (EvidenceConfigSchema))
+) => {
 	try {
 		const configFilePath = path.join(projectRoot, 'evidence.config.yaml');
 		const configFileContent = fs.readFileSync(configFilePath, 'utf-8');
-
 		const result = yaml.parse(configFileContent.replaceAll(/($|\s)(@.+):/g, '$1"$2":'));
-		return EvidenceConfigSchema.parse(result);
+		return schema.parse(result);
 	} catch (e) {
 		if (e instanceof Error && e.message.startsWith('Cannot find matching evidence.config.yaml')) {
 			return getEvidenceConfigLegacy();
