6ee0ab8efb5f43803f2940314c93903d0441c31c
Zachary Stence <zachstence@gmail.com>
2024-09-17 14:04:51 -0500

    test: e2e/themes test

--- e2e/themes/pages/index.md
+++ e2e/themes/pages/index.md
@@ -1,2 +1,2 @@
-<div class="w-20 h-20 bg-mySemanticColor">
+<div data-testid="div-with-background" class="w-20 h-20 bg-mySemanticColor">
 </div>
--- e2e/themes/tests/tests.spec.js
+++ e2e/themes/tests/tests.spec.js
@@ -2,9 +2,22 @@
 import { test, expect } from '@playwright/test';
 import { waitForDevModeToLoad } from '../../test-utils';
 
-test('has title', async ({ page }) => {
+test('should change color based on theme', async ({ page }) => {
 	await page.goto('/');
 	await waitForDevModeToLoad(page);
 
-	await expect(page).toHaveTitle(/Welcome to Evidence/);
+	const divWithBackground = await page.getByTestId('div-with-background');
+
+	// Starts with system theme (dark)
+	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+
+	await page.getByLabel('Menu').click();
+
+	// Light theme
+	await page.getByRole('menuitem', { name: 'Appearance' }).click();
+	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(255, 0, 0)');
+
+	// Dark theme
+	await page.getByRole('menuitem', { name: 'Appearance' }).click();
+	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
 });
