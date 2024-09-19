4437fcd16ed530f9041e901885fbb57b7453da39
brian <contact@noimbrian.com>
2024-09-19 14:07:40 -0500

    chore: ensure that query tracking stores do not activate unless in dev mode

--- packages/ui/core-components/src/lib/devtools/ActiveQueries.store.js
+++ packages/ui/core-components/src/lib/devtools/ActiveQueries.store.js
@@ -1,23 +1,26 @@
+import { dev } from '$app/environment';
 import { writable } from 'svelte/store';
 import { batchUp } from '@evidence-dev/sdk/utils';
 import { Query } from '@evidence-dev/sdk/usql';
 const activeQueries = writable(new Map());
 
-Query.addEventListener(
-	'queryCreated',
-	batchUp((insertedQueries) => {
-		activeQueries.update(($activeQueries) => {
-			for (const query of insertedQueries) {
-				if (Query.isQuery(query)) $activeQueries.set(query.id, query);
-			}
-			return $activeQueries;
-		});
-	})
-);
+if (dev) {
+	Query.addEventListener(
+		'queryCreated',
+		batchUp((insertedQueries) => {
+			activeQueries.update(($activeQueries) => {
+				for (const query of insertedQueries) {
+					if (Query.isQuery(query)) $activeQueries.set(query.id, query);
+				}
+				return $activeQueries;
+			});
+		})
+	);
 
-// Cache was cleared so we can reasonably assume that all queries are no longer active
-// 🚩 Is this actually a true assumption?
-Query.addEventListener('cacheCleared', () => activeQueries.set(new Map()));
+	// Cache was cleared so we can reasonably assume that all queries are no longer active
+	// 🚩 Is this actually a true assumption?
+	Query.addEventListener('cacheCleared', () => activeQueries.set(new Map()));
+}
 
 export const ActiveQueries = {
 	subscribe: activeQueries.subscribe.bind(activeQueries),
