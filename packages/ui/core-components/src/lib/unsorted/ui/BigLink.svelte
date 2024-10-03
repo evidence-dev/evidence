0d7562034afc2d89ad6500b9452f2fced1c8a858
Zachary Stence <zachstence@gmail.com>
2024-10-03 14:28:46 -0500

    feat: BigLink theming

--- packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/BigLink.svelte
@@ -14,9 +14,9 @@
 	</div>
 </a>
 
-<style>
+<style lang="postcss">
 	div {
-		border: 1px solid var(--grey-200);
+		@apply border border-base-300 hover:shadow-md shadow-base-100;
 		border-radius: 6px;
 		padding: 0.3em 0.6em;
 		font-family: var(--ui-font-family);
@@ -35,11 +35,5 @@
 	a:hover {
 		text-decoration: none;
 		color: var(--blue-700);
-	}
-
-	div:hover {
-		border: 1px solid var(--grey-300);
-		transition: all 400ms;
-		box-shadow: 0 5px 5px 2px hsl(0deg 0% 97%);
 	}
 </style>
