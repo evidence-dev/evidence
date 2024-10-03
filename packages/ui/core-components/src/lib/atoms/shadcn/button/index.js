f1389e62577eb4396c965612822e9836abdf38a4
Zachary Stence <zachstence@gmail.com>
2024-10-03 16:13:57 -0500

    fix: shadcn button variants use text-base-content

--- packages/ui/core-components/src/lib/atoms/shadcn/button/index.js
+++ packages/ui/core-components/src/lib/atoms/shadcn/button/index.js
@@ -5,12 +5,13 @@ const buttonVariants = tv({
 	base: 'inline-flex items-center justify-center rounded-md text-sm font-medium whitespace-nowrap transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-400 disabled:pointer-events-none disabled:opacity-50',
 	variants: {
 		variant: {
-			default: 'bg-gray-900 text-white shadow hover:bg-gray-900/90',
+			default: 'bg-base-content text-white shadow hover:bg-base-content/90',
 			destructive: 'bg-red-500 text-red-50 shadow-sm hover:bg-red-500/90',
-			outline: 'border border-input bg-transparent shadow-sm hover:bg-gray-100 hover:text-gray-900',
-			secondary: 'bg-gray-100 text-gray-900 shadow-sm hover:bg-gray-100/80',
-			ghost: 'hover:bg-gray-100 hover:text-gray-900',
-			link: 'text-gray-900 underline-offset-4 hover:underline'
+			outline:
+				'border border-input bg-transparent shadow-sm hover:bg-gray-100 hover:text-base-content',
+			secondary: 'bg-gray-100 text-base-content shadow-sm hover:bg-gray-100/80',
+			ghost: 'hover:bg-gray-100 hover:text-base-content',
+			link: 'text-base-content underline-offset-4 hover:underline'
 		},
 		size: {
 			default: 'h-9 px-4 py-2',
