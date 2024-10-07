89811ae0fe208a19e917e19cc2e45a7801dbe6db
csjh <61282104+csjh@users.noreply.github.com>
2024-10-07 11:49:28 -0400

    fix playwright config to test built project

--- e2e/prerender/playwright.config.js
+++ e2e/prerender/playwright.config.js
@@ -1,4 +1,13 @@
 import { defineConfig } from '@playwright/test';
 import { config } from '../playwright-config';
 
-export default defineConfig(config);
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+export default {
+	...defineConfig(config),
+	webServer: {
+		command: 'pnpm sources && pnpm build && pnpm preview',
+		port: 3000,
+		reuseExistingServer: true,
+		timeout: 240_000
+	}
+};
