d1c08b3b4159a2ec149d8a93182dfb0d90171be8
csjh <61282104+csjh@users.noreply.github.com>
2024-10-09 15:00:22 -0400

    `defineConfig` on whole config instead of just imported `config`

--- e2e/prerender/playwright.config.js
+++ e2e/prerender/playwright.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from '@playwright/test';
 import { config } from '../playwright-config';
 
 /** @type {import('@playwright/test').PlaywrightTestConfig} */
-export default {
-	...defineConfig(config),
+export default defineConfig({
+	...config,
 	use: {
 		...config.use,
 		serviceWorkers: 'block'
 	}
-};
+});
