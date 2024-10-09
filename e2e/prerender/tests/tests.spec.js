2dc1c432ea3b2bd6ddae9729e99050a55a865528
csjh <61282104+csjh@users.noreply.github.com>
2024-10-09 14:08:19 -0400

    move waitForWasm to utils

--- e2e/prerender/tests/tests.spec.js
+++ e2e/prerender/tests/tests.spec.js
@@ -1,15 +1,6 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-
-/** @param {import("@playwright/test").BrowserContext} context */
-async function waitForWasm(context) {
-	return new Promise((res) => {
-		context.route(/duckdb-eh\..*?\.wasm$/, async (route) => {
-			await route.abort();
-			res(undefined);
-		});
-	});
-}
+import { waitForWasm } from '../../test-utils';
 
 test('has title', async ({ page }) => {
 	await page.goto('/');
@@ -29,7 +20,7 @@ test('queries on page prerender', async ({ page }) => {
 
 test('queries on page prerender (right before duckdb loads)', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	// length query prerenders
 	await expect(page.getByTestId('category-count')).toHaveText('4');
@@ -49,7 +40,7 @@ test('dropdown options prerender', async ({ page }) => {
 
 test('dropdown options prerender (right before duckdb loads)', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	await expect(page.getByTestId('Dropdown-category')).toHaveText('All Categories');
 	await expect(page.getByTestId('Dropdown-category2')).toHaveText('Sinister Toys');
@@ -59,7 +50,7 @@ test('dropdown options prerender (right before duckdb loads)', async ({ page }) 
 
 test('buildQuery without supplied initialData should be in loading state', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	await expect(page.getByTestId('loading-1')).toHaveText('Loading...');
 	await expect(page.getByTestId('loaded-1')).toHaveCount(0);
@@ -67,7 +58,7 @@ test('buildQuery without supplied initialData should be in loading state', async
 
 test('buildQuery with supplied initialData should be in loaded state', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	await expect(page.getByTestId('loading-2')).toHaveCount(0);
 	await expect(page.getByTestId('loaded-2')).toHaveText(
@@ -81,7 +72,7 @@ test('DataTable with buildQuery query should be loaded (right before duckdb load
 	page
 }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	await expect(page.getByTestId('DataTable-ssr')).toHaveText(
 		'    Category  Count     Sinister Toys5.54k   Odd Equipment4.97k   Mysterious Apparel5.19k   Cursed Sporting Goods4.31k    No Results  '
@@ -90,7 +81,7 @@ test('DataTable with buildQuery query should be loaded (right before duckdb load
 
 test('DataTable with page query should be loaded (right before duckdb loads)', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	await expect(page.getByTestId('DataTable-orders_by_category'))
 		.toHaveText(`    Month  Sales Usd  Category     2021-06-01$5,903Odd Equipment   2020-11-01$5,726Odd Equipment   2021-03-01$5,551Odd Equipment   2021-11-01$5,526Odd Equipment   2021-11-01$5,515Cursed Sporting Goods   2021-10-01$5,435Odd Equipment   2019-10-01$5,345Odd Equipment   2021-09-01$5,332Odd Equipment   2020-03-01$5,252Odd Equipment   2021-07-01$5,165Odd Equipment    No Results   Page
@@ -101,7 +92,7 @@ test('DataTable with page query should be loaded (right before duckdb loads)', a
 
 test('charts should render once', async ({ page }) => {
 	await page.goto('/');
-	await waitForWasm(page.context());
+	await waitForWasm(page);
 
 	// first BarChart imitates a chart that double loads, second is normal
 	const expected = [3, 1];
