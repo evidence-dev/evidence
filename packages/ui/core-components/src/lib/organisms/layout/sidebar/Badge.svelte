e5c07fe2dfe8d2edb7ed16814d46287dcf215024
Zachary Stence <zachstence@gmail.com>
2024-10-17 16:01:33 -0500

    feat: sidebar badge theming

--- packages/ui/core-components/src/lib/organisms/layout/sidebar/Badge.svelte
+++ packages/ui/core-components/src/lib/organisms/layout/sidebar/Badge.svelte
@@ -1,5 +1,5 @@
 <span
-	class="rounded px-0.5 py-[1px] bg-green-50 border border-green-200 text-green-800 group-hover:text-green-950 text-base sm:text-xs"
+	class="rounded px-0.5 py-[1px] bg-positive/10 border border-positive/20 text-positive text-base sm:text-xs"
 >
 	<slot />
 </span>
