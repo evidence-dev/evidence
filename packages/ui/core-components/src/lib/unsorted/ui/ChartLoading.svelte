1f78244303ad75809022345854d2dad3ea34abe6
Zachary Stence <zachstence@gmail.com>
2024-10-17 16:14:01 -0500

    feat: ChartLoading theming

--- packages/ui/core-components/src/lib/unsorted/ui/ChartLoading.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/ChartLoading.svelte
@@ -7,7 +7,7 @@
 <div role="status" class="animate-pulse" in:fade|local>
 	<span class="sr-only">Loading...</span>
 	<div
-		class="bg-gray-200 rounded-md dark:bg-gray-400 max-w-[100%]"
+		class="bg-base-100 rounded-md max-w-[100%]"
 		style="height:{height}; margin-top: 15px; margin-bottom: 31px;"
 	/>
 </div>
