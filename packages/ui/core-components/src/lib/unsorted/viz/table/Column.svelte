224ec9b3cd68e92f356d78747b2b90abdbc40632
csjh <61282104+csjh@users.noreply.github.com>
2024-10-24 15:18:01 -0400

    add types to column.svelte, add datatable aggregate store

--- packages/ui/core-components/src/lib/unsorted/viz/table/Column.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/table/Column.svelte
@@ -3,15 +3,22 @@
 </script>
 
 <script>
+	// @ts-check
+
 	import { getContext } from 'svelte';
 	import { propKey, strictBuild } from '@evidence-dev/component-utilities/chartContext';
 
+	/** @typedef {import("./datatable.store.js").ColumnConfig} ColumnConfig */
+
+	/** @type {import("svelte/store").Writable<import("./datatable.store.js").DataTableProps>} */
 	let props = getContext(propKey);
 
 	let error;
 
+	/** @type {ColumnConfig["identifier"]} */
 	const identifier = Symbol();
 
+	/** @type {ColumnConfig["id"]} */
 	export let id;
 
 	/**
@@ -31,88 +38,120 @@
 		}
 	}
 
+	/** @type {ColumnConfig["title"]} */
 	export let title = undefined;
+	/** @type {ColumnConfig["align"]} */
 	export let align = undefined;
 	if (align === 'centre') {
 		align = 'center';
 	}
+	/** @type {ColumnConfig["wrap"] | string} */
 	export let wrap = false;
 	$: wrap = wrap === 'true' || wrap === true;
 
+	/** @type {ColumnConfig["wrapTitle"] | string} */
 	export let wrapTitle = false;
 	$: wrapTitle = wrapTitle === 'true' || wrapTitle === true;
 
 	// COLUMN CONTENT TYPES:
+	/** @type {ColumnConfig["contentType"]} */
 	export let contentType = undefined;
 
 	// Images:
+	/** @type {ColumnConfig["height"]} */
 	export let height = undefined;
+	/** @type {ColumnConfig["width"]} */
 	export let width = undefined;
+	/** @type {ColumnConfig["alt"]} */
 	export let alt = undefined;
 
 	// Links:
+	/** @type {ColumnConfig["openInNewTab"] | string} */
 	export let openInNewTab = false;
 	$: openInNewTab = openInNewTab === 'true' || openInNewTab === true;
 
+	/** @type {ColumnConfig["linkLabel"]} */
 	export let linkLabel = undefined;
 
 	// Formatting:
+	/** @type {ColumnConfig["fmt"]} */
 	export let fmt = undefined;
 
 	// Totals:
+	/** @type {ColumnConfig["totalAgg"]} */
 	export let totalAgg = undefined;
+	/** @type {ColumnConfig["totalFmt"]} */
 	export let totalFmt = undefined;
+	/** @type {ColumnConfig["weightCol"]} */
 	export let weightCol = undefined; // column to use as the weights for weighted average
 
 	// Subtotals:
+	/** @type {ColumnConfig["subtotalFmt"]} */
 	export let subtotalFmt = undefined;
 
 	// Color Scale:
+	/** @type {ColumnConfig["colorMax"]} */
 	export let colorMax = undefined;
+	/** @type {ColumnConfig["colorMin"]} */
 	export let colorMin = undefined;
+	/** @type {ColumnConfig["colorMid"]} */
 	export let colorMid = undefined;
+	/** @type {ColumnConfig["colorBreakpoints"]} */
 	export let colorBreakpoints = undefined;
+	/** @type {ColumnConfig["scaleColor"]} */
 	export let scaleColor = 'green'; // name of predefined color palette, custom color, array of custom colors
+	/** @type {ColumnConfig["scaleColumn"]} */
 	export let scaleColumn = undefined;
 
-	let colorList = {
+	const colorList = {
 		green: ['white', 'hsla(129, 33%, 57%,1)'],
 		red: ['white', 'hsla(0, 56%, 56%,1)'],
 		blue: ['white', 'hsla(198, 56%, 56%,1)']
 	};
 
+	/** @type {ColumnConfig["colorPalette"]} */
 	let colorPalette;
-	if (scaleColor instanceof Array) {
+	if (Array.isArray(scaleColor)) {
 		colorPalette = scaleColor;
+	} else if (!Object.hasOwn(colorList, scaleColor)) {
+		colorPalette = ['white', scaleColor];
 	} else {
-		colorPalette = colorList[scaleColor];
-		if (colorPalette == undefined) {
-			colorPalette = ['white', scaleColor];
-		}
+		colorPalette = colorList[/** @type {keyof typeof colorList} */ (scaleColor)];
 	}
 
 	// Delta:
+	/** @type {ColumnConfig["downIsGood"] | string} */
 	export let downIsGood = false;
 	$: downIsGood = downIsGood === 'true' || downIsGood === true;
+	/** @type {ColumnConfig["showValue"] | string} */
 	export let showValue = true;
 	$: showValue = showValue === 'true' || showValue === true;
+	/** @type {ColumnConfig["deltaSymbol"] | string} */
 	export let deltaSymbol = true;
 	$: deltaSymbol = deltaSymbol === 'true' || deltaSymbol === true;
+	/** @type {ColumnConfig["neutralMin"]} */
 	export let neutralMin = 0;
+	/** @type {ColumnConfig["neutralMax"]} */
 	export let neutralMax = 0;
+	/** @type {ColumnConfig["chip"] | string} */
 	export let chip = false;
 	$: chip = chip === 'true' || chip === true;
 
 	// Column Groups:
+	/** @type {ColumnConfig["colGroup"]} */
 	export let colGroup = undefined;
 
 	// Formats defined in another column:
+	/** @type {ColumnConfig["fmtColumn"]} */
 	export let fmtColumn = undefined;
 
 	// Neagtive value font color:
+	/** @type {ColumnConfig["redNegatives"] | string} */
 	export let redNegatives = false;
 	$: redNegatives = redNegatives === 'true' || redNegatives === true;
 
+	/** @type {import("./datatable.store.js").ColumnConfig} */
+	let options;
 	$: options = {
 		identifier,
 		id,
---
+++ packages/ui/core-components/src/lib/unsorted/viz/table/datatable.store.js
@@ -0,0 +1,165 @@
+// @ts-check
+
+import { derived, writable } from "svelte/store";
+import { aggregateColumn } from "./datatable.js";
+
+/** @typedef {Object} ColumnConfig
+ * @property {symbol} identifier
+ * @property {string} id
+ * @property {string} [title]
+ * @property {string} [align]
+ * @property {boolean} [wrap]
+ * @property {boolean} [wrapTitle]
+ * @property {string} [contentType]
+ * @property {number} [height]
+ * @property {number} [width]
+ * @property {string} [alt]
+ * @property {boolean} [openInNewTab]
+ * @property {string} [linkLabel]
+ * @property {string} [fmt]
+ * @property {string} [fmtColumn]
+ * @property {string} [totalAgg]
+ * @property {string} [totalFmt]
+ * @property {string} [subtotalFmt]
+ * @property {string} [weightCol]
+ * @property {boolean} [downIsGood]
+ * @property {boolean} [deltaSymbol]
+ * @property {boolean} [chip]
+ * @property {number} [neutralMin]
+ * @property {number} [neutralMax]
+ * @property {boolean} [showValue]
+ * @property {string} [colorMax]
+ * @property {string} [colorMin]
+ * @property {string | string[]} scaleColor
+ * @property {string} [scaleColumn]
+ * @property {string} [colGroup]
+ * @property {string} [colorMid]
+ * @property {string} [colorBreakpoints]
+ * @property {Object} [colorPalette]
+ * @property {boolean} [redNegatives]
+ */
+
+/**
+ * @typedef {Object} DataTableProps
+ * @property {Record<string, unknown>[]} data
+ * @property {ColumnConfig[]} columns
+ * @property {string[]} priorityColumns
+ */
+
+/**
+ * @typedef {Object} NonStoreDependents
+ * @property {Record<string, unknown>[]} data
+ * @property {string} groupBy
+ * @property {boolean} groupsOpen
+ */
+
+/** @typedef {Object} StoresDependents
+ * @property {import("svelte/store").Readable<(import("@evidence-dev/component-utilities/getColumnSummary").ColumnSummary & { id: string })[]>} columnSummary
+ * @property {import("svelte/store").Readable<DataTableProps>} props
+ * @property {import("svelte/store").Readable<{ col: string | null; ascending: boolean | null; }>} sortBy
+ */
+
+/** @typedef {Record<string, Record<string, unknown>[]>} GroupedData */
+
+/**
+ * @param {StoresDependents & NonStoreDependents} param0 
+ */
+export function aggregateStores({ columnSummary, props, sortBy, data: _data, groupBy: _groupBy, groupsOpen: _groupsOpen }) {
+	const data = writable(_data);
+	const groupBy = writable(_groupBy);
+	const groupsOpen = writable(_groupsOpen);
+
+	const groupedData = derived([data, groupBy], ([$data, $groupBy]) =>
+		/** @type {GroupedData} */($data.reduce((acc, row) => {
+		const groupName = /** @type {string} */ (row[$groupBy]);
+		acc[groupName] ??= [];
+		acc[groupName].push(row);
+		return acc;
+	}, {}))
+	);
+
+	const groupRowData = derived([groupedData, columnSummary, props], ([$groupedData, $columnSummary, $props]) =>
+		Object.keys($groupedData).reduce((acc, groupName) => {
+			acc[groupName] = {}; // Initialize groupRow object for this group
+
+			for (const col of $props.columns) {
+				const id = col.id;
+				const colType = $columnSummary.find((d) => d.id === id)?.type ?? "string";
+				const totalAgg = col.totalAgg;
+				const weightCol = col.weightCol;
+				const rows = $groupedData[groupName];
+				acc[groupName][id] = aggregateColumn(rows, id, totalAgg, colType, weightCol);
+			}
+
+			return acc;
+		}, /** @type {Record<string, Record<string, string | number>>} */({}))
+	);
+
+	/** @type {Record<string, boolean>} */
+	let previousToggleStates = {};
+	const groupToggleStates = derived([groupedData, groupsOpen], ([$groupedData, $groupsOpen]) => {
+		const existingGroups = Object.keys(previousToggleStates);
+		for (const groupName of Object.keys($groupedData)) {
+			if (!existingGroups.includes(groupName)) {
+				previousToggleStates[groupName] = $groupsOpen; // Only add new groups with the default state
+			}
+			// Existing states are untouched
+		}
+		return previousToggleStates;
+	});
+
+	const sortedGroupNames = derived(
+		[groupRowData, sortBy, groupedData],
+		([$groupRowData, $sortBy, $groupedData]) => {
+			if (groupBy && $sortBy.col) {
+				const col = $sortBy.col;
+				// Sorting groups based on aggregated values or group names
+				return Object.entries($groupRowData)
+					.sort((a, b) => {
+						const valA = a[1][col],
+							valB = b[1][col];
+						// Use the existing sort logic but apply it to groupRowData's values
+						if (
+							(valA === undefined || valA === null || Number.isNaN(valA)) &&
+							valB !== undefined &&
+							valB !== null &&
+							!Number.isNaN(valB)
+						) {
+							return -1 * ($sortBy.ascending ? 1 : -1);
+						}
+						if (
+							(valB === undefined || valB === null || Number.isNaN(valB)) &&
+							valA !== undefined &&
+							valA !== null &&
+							!Number.isNaN(valA)
+						) {
+							return 1 * ($sortBy.ascending ? 1 : -1);
+						}
+						if (valA < valB) {
+							return -1 * ($sortBy.ascending ? 1 : -1);
+						} else if (valA > valB) {
+							return 1 * ($sortBy.ascending ? 1 : -1);
+						}
+						return 0;
+					})
+					.map((entry) => entry[0]); // Extract sorted group names
+			} else {
+				// Default to alphabetical order of group names or another criterion when not sorting by a specific column
+				return Object.keys($groupedData).sort();
+			}
+		}
+	);
+
+	return {
+		/** @param {NonStoreDependents} param0 */
+		update: ({ data: _data, groupBy: _groupBy, groupsOpen: _groupsOpen }) => {
+			data.set(_data);
+			groupBy.set(_groupBy);
+			groupsOpen.set(_groupsOpen);
+		},
+		groupedData,
+		groupRowData,
+		groupToggleStates,
+		sortedGroupNames,
+	};
+}
