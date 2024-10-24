d7fa7cea3c932440e3dc6b821b0847ec94782112
Zachary Stence <zachstence@gmail.com>
2024-10-24 18:06:08 -0500

    fix: throw error

--- packages/ui/core-components/src/lib/unsorted/viz/table/_DataTable.svelte
+++ packages/ui/core-components/src/lib/unsorted/viz/table/_DataTable.svelte
@@ -180,7 +180,9 @@
 		// Check if sort column is in table
 		if (sortBy) {
 			if (!columnSummary.map((d) => d.id).includes(sortBy)) {
-				`${sortBy} is not a column in the dataset. sort should contain one column name and optionally a direction (asc or desc). E.g., sort=my_column or sort="my_column desc"`;
+				throw Error(
+					`${sortBy} is not a column in the dataset. sort should contain one column name and optionally a direction (asc or desc). E.g., sort=my_column or sort="my_column desc"`
+				);
 			}
 			if (sortDirection && !['asc', 'desc'].includes(sortDirection)) {
 				throw Error(`${sortDirection} is not a valid sort direction. Please use asc or desc`);
