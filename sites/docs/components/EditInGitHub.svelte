b3f609ef33a7cb402083347c7a3e9335b468a7d9
Archie Wood <58074498+archiewood@users.noreply.github.com>
2024-10-30 18:17:46 -0400

    remove comments

--- sites/docs/components/EditInGitHub.svelte
+++ sites/docs/components/EditInGitHub.svelte
@@ -1,29 +1,20 @@
 <script>
-    import { page } from '$app/stores';
-    import { derived } from 'svelte/store';
-    import { Icon } from '@steeze-ui/svelte-icon';
-	import { Pencil } from '@steeze-ui/tabler-icons';
+	import { page } from '$app/stores';
+	import { derived } from 'svelte/store';
 
-    // Base URL for the GitHub repository
-    const repoUrl = 'https://github.com/evidence-dev/evidence/edit/next/sites/docs/pages/';
+	const repoUrl = 'https://github.com/evidence-dev/evidence/edit/next/sites/docs/pages/';
 
-        // Derive the file path from the current page route
-        const filePath = derived(page, $page => {
-            // Assuming the route matches the file structure
-            return $page.url.pathname.replace(/^\//, '').replace(/\/$/, '') + '/index.md'; // Adjust the extension as needed
-        });
+	const filePath = derived(page, ($page) => {
+		return $page.url.pathname.replace(/^\//, '').replace(/\/$/, '') + '/index.md';
+	});
 
-    // Construct the full GitHub edit URL
-    const editUrl = derived(filePath, $filePath => `${repoUrl}${$filePath}`);
+	const editUrl = derived(filePath, ($filePath) => `${repoUrl}${$filePath}`);
 </script>
 
 <div class="max-w-7xl mx-auto px-6 sm:px-8 md:px-12 flex flex-row pb-4">
-    <div class="text-gray-700 text-sm md:ml-48 md:pl-8">
-        <a href={$editUrl} target="_blank" rel="noopener noreferrer" class="hover:underline">
-            <span class="inline-flex items-center">
-            <Icon src={Pencil} class="w-4 h-4 mb-1 mr-1" />
-            Edit page on GitHub
-            </span>
-        </a>
-    </div>
+	<div class="text-gray-700 text-sm md:ml-48 md:pl-8">
+		<a href={$editUrl} target="_blank" rel="noopener noreferrer" class="hover:underline">
+			<span class="inline-flex items-center"> Edit page on GitHub </span>
+		</a>
+	</div>
 </div>
