434b349f67309fcc7a3277e284064f12572cc7c4
Adam <mcrascal@users.noreply.github.com>
2024-10-30 14:39:36 -0400

    support html in prop listing descriptions, adjust breakpoints

--- sites/docs/components/PropListing.svelte
+++ sites/docs/components/PropListing.svelte
@@ -68,7 +68,7 @@
 </script>
 
 <section
-	class="pt-4 pb-2 border-b text-sm flex flex-col lg:flex-row gap-4 scroll-mt-[3.5rem] transition-colors duration-300 {$hashLocation ===
+	class="pt-4 pb-2 border-b text-sm flex flex-col xl:flex-row gap-4 scroll-mt-[3.5rem] transition-colors duration-300 {$hashLocation ===
 	`#props-${idName}`
 		? 'bg-blue-50 border-blue-400 border-t'
 		: ''}"
@@ -88,8 +88,8 @@
 			<span class="text-red-500 uppercase tracking-wide">Required</span>
 		{/if}
 	</div>
-	<div class="ml-3.5 lg:ml-0">
-		<div id="markdown-slot"><slot>{description}</slot></div>
+	<div class="ml-3.5 xl:ml-0">
+		<div id="markdown-slot"><slot>{@html description}</slot></div>
 		{#if Array.isArray(options) && options.length > 0}
 			<div class="mt-1 select-none flex">
 				<span class="text-sm text-gray-400 mr-2">Options:</span>
