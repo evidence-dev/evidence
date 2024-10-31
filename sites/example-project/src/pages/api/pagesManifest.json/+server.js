706636a52f2641550c9c0e64864898743491236b
csjh <61282104+csjh@users.noreply.github.com>
2024-10-31 14:08:30 -0400

    replace import.meta.glob

--- sites/example-project/src/pages/api/pagesManifest.json/+server.js
+++ sites/example-project/src/pages/api/pagesManifest.json/+server.js
@@ -1,5 +1,5 @@
 import preprocess from '@evidence-dev/preprocess';
-import { error } from '@sveltejs/kit';
+import fs from 'fs';
 
 /**
  * @typedef {Object} PageManifestNode
@@ -12,53 +12,64 @@ import { error } from '@sveltejs/kit';
  */
 
 /**
- * @param {Record<string, string>} pages
- * @returns {PageManifestNode}
+ * @param {PageManifestNode} fileTree
+ * @param {string} pagePath
+ * @param {string} pageContent
  */
-export function _buildPageManifest(pages) {
-	const fileTree = {
-		label: 'Home',
-		href: undefined,
-		children: {},
-		frontMatter: {},
-		isTemplated: false,
-		isPage: false
-	};
-	for (const [pagePath, pageContent] of Object.entries(pages)) {
-		const path = pagePath.replace('/src/pages/', '');
-		let node = fileTree;
-		for (const part of path.split('/')) {
-			if (part === '+page.md') {
-				// cover home page
-				if (path === '+page.md') {
-					node.href = '/';
-				} else if (!path.includes('[')) {
-					node.href = encodeURI('/' + path.replace('/+page.md', ''));
+function addPageToTree(fileTree, pagePath, pageContent) {
+	const path = pagePath.replace('/src/pages/', '');
+	let node = fileTree;
+	for (const part of path.split('/')) {
+		if (part === '+page.md') {
+			// cover home page
+			if (path === '+page.md') {
+				node.href = '/';
+			} else if (!path.includes('[')) {
+				node.href = encodeURI('/' + path.replace('/+page.md', ''));
+			}
+			node.frontMatter = preprocess.parseFrontmatter(pageContent);
+			node.isPage = true;
+		} else {
+			const label = part.includes('[') ? undefined : part.replace(/_/g, ' ').replace(/-/g, ' ');
+			node = node.children[part] = node.children[part] ?? {
+				label,
+				href: undefined,
+				children: {},
+				frontMatter: {},
+				isTemplated: part.includes('['),
+				isPage: false
+			};
+		}
+	}
+}
+
+/** @type {PageManifestNode} */
+const fileTree = {
+	label: 'Home',
+	href: undefined,
+	children: {},
+	frontMatter: {},
+	isTemplated: false,
+	isPage: false
+};
+
+try {
+	const pagesDir = fs.readdirSync('src/pages', { withFileTypes: true });
+	for (const dirent of pagesDir) {
+		if (dirent.isDirectory()) {
+			const pageDir = fs.readdirSync(`src/pages/${dirent.name}`, { withFileTypes: true });
+			for (const pageDirent of pageDir) {
+				if (pageDirent.isFile() && pageDirent.name.endsWith('.md')) {
+					const path = `src/pages/${dirent.name}/${pageDirent.name}`;
+					const content = fs.readFileSync(path, 'utf-8');
+					addPageToTree(fileTree, '/' + path, content);
 				}
-				node.frontMatter = preprocess.parseFrontmatter(pageContent);
-				node.isPage = true;
-			} else {
-				const label = part.includes('[') ? undefined : part.replace(/_/g, ' ').replace(/-/g, ' ');
-				node = node.children[part] = node.children[part] ?? {
-					label,
-					href: undefined,
-					children: {},
-					frontMatter: {},
-					isTemplated: part.includes('['),
-					isPage: false
-				};
 			}
 		}
 	}
-	return fileTree;
+} catch (e) {
+	console.log('Failed to build file tree: ', e.message);
 }
-
-// Import pages and create an object structure corresponding to the file structure
-const pages = import.meta.glob('/src/pages/**/+page.md', {
-	import: 'default',
-	query: 'raw',
-	eager: true
-});
 
 export const prerender = true;
 
@@ -66,11 +77,5 @@ export const prerender = true;
  * @type {import("@sveltejs/kit").RequestHandler}
  */
 export async function GET() {
-	try {
-		const fileTree = _buildPageManifest(pages);
-
-		return new Response(JSON.stringify(fileTree));
-	} catch {
-		throw error(500, 'Failed to build pages manifest.');
-	}
+	return new Response(JSON.stringify(fileTree));
 }
