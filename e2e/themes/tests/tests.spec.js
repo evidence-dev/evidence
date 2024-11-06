26b107486fecb82444d177c451d6b7acbfd8b84c
Zachary Stence <zachstence@gmail.com>
2024-11-06 14:53:11 -0600

    feat: support custom color tokens, automatically create css vars

--- e2e/themes/tests/tests.spec.js
+++ e2e/themes/tests/tests.spec.js
@@ -6,14 +6,26 @@ test('should change colors based on theme', async ({ page }) => {
 	await page.goto('/');
 	await waitForDevModeToLoad(page);
 
-	const divWithBackground = await page.getByTestId('div-with-background');
+	const divPrimaryClass = await page.getByTestId('div-primary-class');
+	const divPrimaryVar = await page.getByTestId('div-primary-var');
+	const divMyCustomColorClass = await page.getByTestId('div-myCustomColor-class');
+	const divMyCustomColorVar = await page.getByTestId('div-myCustomColor-var');
 
 	await switchAppearance(page, 'system');
-	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divPrimaryClass).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divPrimaryVar).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divMyCustomColorClass).toHaveCSS('background-color', 'rgb(254, 220, 186)');
+	await expect(divMyCustomColorVar).toHaveCSS('background-color', 'rgb(254, 220, 186)');
 
 	await switchAppearance(page, 'light');
-	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(255, 0, 0)');
+	await expect(divPrimaryClass).toHaveCSS('background-color', 'rgb(255, 0, 0)');
+	await expect(divPrimaryVar).toHaveCSS('background-color', 'rgb(255, 0, 0)');
+	await expect(divMyCustomColorClass).toHaveCSS('background-color', 'rgb(171, 205, 239)');
+	await expect(divMyCustomColorVar).toHaveCSS('background-color', 'rgb(171, 205, 239)');
 
 	await switchAppearance(page, 'dark');
-	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divPrimaryClass).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divPrimaryVar).toHaveCSS('background-color', 'rgb(0, 255, 0)');
+	await expect(divMyCustomColorClass).toHaveCSS('background-color', 'rgb(254, 220, 186)');
+	await expect(divMyCustomColorVar).toHaveCSS('background-color', 'rgb(254, 220, 186)');
 });
