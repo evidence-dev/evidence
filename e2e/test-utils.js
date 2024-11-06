33f758a9a59223535e9a67c0f0fa06a03758339f
Zachary Stence <zachstence@gmail.com>
2024-11-06 11:51:53 -0600

    refactor: e2e test utils for interacting with kebab menu and appearance

--- e2e/test-utils.js
+++ e2e/test-utils.js
@@ -1,3 +1,5 @@
+// @ts-check
+
 import { expect } from '@playwright/test';
 
 export const waitForDevModeToLoad = async (page) => {
@@ -18,3 +20,109 @@ export function waitForWasm(page) {
 		});
 	});
 }
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @returns {Promise<boolean>}
+ */
+export const isKebabMenuOpen = async (page) => {
+	try {
+		const menu = await page.getByRole('menu', { name: 'Menu' });
+		const isVisible = await menu.isVisible();
+		return isVisible;
+	} catch (e) {
+		console.error('isKebabMenuOpen failed', e);
+		throw e;
+	}
+};
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @returns {Promise<boolean>} True if the kebab menu was opened, false if it was already open
+ */
+export const openKebabMenu = async (page) => {
+	if (await isKebabMenuOpen(page)) return false;
+
+	try {
+		// Click button to open menu
+		await page.getByRole('button', { name: 'Menu' }).click();
+		// Make sure menu is open
+		await expect(page.getByRole('menu', { name: 'Menu' })).toBeVisible();
+		return true;
+	} catch (e) {
+		console.error('openKebabMenu failed', e);
+		throw e;
+	}
+};
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @returns {Promise<boolean>} True if the kebab menu was closed, false if it was already closed
+ */
+export const closeKebabMenu = async (page) => {
+	if (!(await isKebabMenuOpen(page))) return false;
+
+	try {
+		// Click button to close menu
+		await page.getByRole('button', { name: 'Menu' }).click();
+		// Make sure menu is closed
+		await expect(page.getByRole('menu', { name: 'Menu' })).not.toBeVisible();
+		return true;
+	} catch (e) {
+		console.error('closeKebabMenu failed', e);
+		throw e;
+	}
+};
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @param {{ closeKebabMenu?: boolean }} options
+ * @returns {Promise<'light' | 'dark' | 'system'>}
+ */
+const getAppearance = async (page, options = { closeKebabMenu: true }) => {
+	try {
+		await openKebabMenu(page);
+
+		const textContent = await page.getByRole('menuitem', { name: 'Appearance' }).textContent();
+		const appearance = textContent?.split(/\s+/)[1].toLowerCase();
+		if (!appearance || !['light', 'dark', 'system'].includes(appearance)) {
+			throw new Error(`Invalid appearance ${appearance}`);
+		}
+		return /** @type {'light' | 'dark' | 'system'} */ (appearance);
+	} catch (e) {
+		console.error('getAppearance failed', e);
+		throw e;
+	} finally {
+		if (options.closeKebabMenu) {
+			await closeKebabMenu(page);
+		}
+	}
+};
+
+/**
+ * @param {import('@playwright/test').Page} page
+ * @param {'light' | 'dark' | 'system'} appearance
+ */
+export const switchAppearance = async (page, appearance) => {
+	try {
+		let current = await getAppearance(page);
+		if (current === appearance) return;
+
+		await openKebabMenu(page);
+		// Switch appearance a max of 2 times (since there's 3 options)
+		for (let i = 0; i < 2; i++) {
+			await page.getByRole('menuitem', { name: 'Appearance' }).click();
+			current = await getAppearance(page, { closeKebabMenu: false });
+			if (current === appearance) break;
+		}
+
+		if (current !== appearance) {
+			throw new Error(`Appearance ${current} doesnt match ${appearance} after switching`);
+		}
+	} catch (e) {
+		console.error('switchAppearance failed', e);
+		throw e;
+	} finally {
+		await closeKebabMenu(page);
+	}
+};
--- e2e/themes/tests/tests.spec.js
+++ e2e/themes/tests/tests.spec.js
@@ -1,23 +1,19 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
-import { waitForDevModeToLoad } from '../../test-utils';
+import { switchAppearance, waitForDevModeToLoad } from '../../test-utils';
 
-test('should change color based on theme', async ({ page }) => {
+test('should change colors based on theme', async ({ page }) => {
 	await page.goto('/');
 	await waitForDevModeToLoad(page);
 
 	const divWithBackground = await page.getByTestId('div-with-background');
 
-	// Starts with system theme (dark)
+	await switchAppearance(page, 'system');
 	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
 
-	await page.getByLabel('Menu').click();
-
-	// Light theme
-	await page.getByRole('menuitem', { name: 'Appearance' }).click();
+	await switchAppearance(page, 'light');
 	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(255, 0, 0)');
 
-	// Dark theme
-	await page.getByRole('menuitem', { name: 'Appearance' }).click();
+	await switchAppearance(page, 'dark');
 	await expect(divWithBackground).toHaveCSS('background-color', 'rgb(0, 255, 0)');
 });
