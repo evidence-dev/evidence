be09f101bb9a1f2d168c26f31b6bbdc3e2728997
Zachary Stence <zachstence@gmail.com>
2024-11-08 16:52:21 -0600

    fix: apply basePath even in dev mode

--- packages/lib/sdk/src/utils/svelte/index.js
+++ packages/lib/sdk/src/utils/svelte/index.js
@@ -2,7 +2,4 @@ export * from './inputs.js';
 import { addBasePath as _addBasePath } from './addBasePath.js';
 import { config } from '$evidence/config';
 /** @type {(path: string) => string} */
-export const addBasePath = (path) => {
-	if (import.meta.env.DEV && import.meta.env.MODE !== 'test') return path;
-	return _addBasePath(path, config);
-};
+export const addBasePath = (path) => _addBasePath(path, config);
