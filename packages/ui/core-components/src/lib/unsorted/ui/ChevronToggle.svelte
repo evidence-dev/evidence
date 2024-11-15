6fb141a68ea2908f04cc64a4816da14f1c170f88
Zachary Stence <zachstence@gmail.com>
2024-11-15 11:26:30 -0600

    feat: ChevronToggle uses resolveColor

--- packages/ui/core-components/src/lib/unsorted/ui/ChevronToggle.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/ChevronToggle.svelte
@@ -5,17 +5,21 @@
 <script>
 	import { getThemeStores } from '../../themes.js';
 
-	export let toggled = false;
-	export let color = undefined;
-	export let size = 10;
+	const { resolveColor } = getThemeStores();
 
-	const { theme } = getThemeStores();
+	export let toggled = false;
+
+	export let color = 'base-content';
+	$: colorStore = resolveColor(color);
+	$: console.log({ $colorStore });
+
+	export let size = 10;
 </script>
 
 <span aria-expanded={toggled}>
 	<svg viewBox="0 0 16 16" width={size} height={size}
 		><path
-			fill={color ?? $theme.colors['base-content']}
+			fill={$colorStore}
 			fill-rule="evenodd"
 			d="M6.22 3.22a.75.75 0 011.06 0l4.25 4.25a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06-1.06L9.94 8 6.22 4.28a.75.75 0 010-1.06z"
 		/></svg
