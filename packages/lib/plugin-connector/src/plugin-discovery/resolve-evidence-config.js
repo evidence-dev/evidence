dc1bb3f013387f8c53f29c92d09a33bc09e8c334
Michael Nahkies <michael@nahkies.co.nz>
2024-11-17 14:57:40 +0000

    fix: resolve may return a path in a sub-directory of the package

--- packages/lib/plugin-connector/src/plugin-discovery/resolve-evidence-config.js
+++ packages/lib/plugin-connector/src/plugin-discovery/resolve-evidence-config.js
@@ -4,12 +4,40 @@ import { loadConfig } from './load-config';
 import { createRequire } from 'module';
 
 /**
+ *
+ * @param {string} packageName
+ * @returns {string}
+ */
+const findPluginDirectory = (packageName) => {
+	// TODO: it would probably be better to use findPackageJSON but only available in node ^23
+	//       https://nodejs.org/api/module.html#modulefindpackagejsonspecifier-base
+	const mainPackageExportFile = createRequire(import.meta.url).resolve(packageName);
+
+	let packageRoot = mainPackageExportFile;
+
+	const packageNamePath = packageName.replace('/', path.sep);
+
+	while (packageRoot !== path.sep && packageRoot.endsWith(packageNamePath) === false) {
+		packageRoot = path.dirname(packageRoot);
+	}
+
+	if (packageRoot.endsWith(packageName) === false) {
+		throw new Error(
+			`failed to find plugin package root for ${packageName}, export file ${mainPackageExportFile}`
+		);
+	}
+
+	return packageRoot;
+};
+
+/**
  * Validates that the given package name exists and is a valid plugin package
  * @param {string} packageName
  * @returns {Promise<EvidencePluginPackage<ValidPackage> | false>}
  */
 const validatePlugin = async (packageName) => {
-	const packagePath = path.dirname(createRequire(import.meta.url).resolve(packageName));
+	const packagePath = findPluginDirectory(packageName);
+
 	const validPackage = await isValidPackage(packagePath);
 	if (!validPackage) return false;
 	return {
