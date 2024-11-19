af60fee19df305961db185400b07a22f46cedf9d
Zachary Stence <zachstence@gmail.com>
2024-11-19 15:45:34 -0600

    feat: default Badge uses primary instead of neutral

--- packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
+++ packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
@@ -5,7 +5,7 @@ export const badgeVariants = tv({
 	base: 'inline-flex items-center rounded-md border border-base-300 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-base-content-muted focus:ring-offset-2 select-none',
 	variants: {
 		variant: {
-			default: 'border-transparent bg-neutral text-neutral-content shadow hover:bg-neutral/80',
+			default: 'border-transparent bg-primary text-primary-content shadow hover:bg-primary/80',
 			secondary: 'border-transparent bg-base-200 hover:bg-base-200/80',
 			destructive:
 				'border-transparent bg-negative text-negative-content shadow hover:bg-negative/80',
