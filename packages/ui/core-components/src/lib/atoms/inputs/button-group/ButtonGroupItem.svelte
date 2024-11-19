ced875b99ad2072a0c158cf61bf0169acc9beb38
Adam <mcrascal@users.noreply.github.com>
2024-11-19 13:53:39 -0500

    prettier

--- packages/ui/core-components/src/lib/atoms/inputs/button-group/ButtonGroupItem.svelte
+++ packages/ui/core-components/src/lib/atoms/inputs/button-group/ButtonGroupItem.svelte
@@ -43,9 +43,9 @@
 	<button
 		type="button"
 		class=" flex-none py-1 font-medium px-3 text-xs truncate
-		border-r last:border-none  
+		border-r last:border-none
 		hover:bg-gray-100 focus:z-10 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-400
-		transition-colors duration-200 
+		transition-colors duration-200
 		{$currentValue?.value === value
 			? 'z-10 border-gray-200 bg-gray-50 text-blue-600'
 			: 'z-0 bg-white text-gray-900 border-gray-200'}
