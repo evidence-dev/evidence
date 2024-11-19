3e3d02cbe1d22ad1dcdf880bcdb616626c5280f5
Zachary Stence <zachstence@gmail.com>
2024-11-19 15:45:54 -0600

    feat: secondary Badge uses secondary instead of base

--- packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
+++ packages/ui/core-components/src/lib/atoms/shadcn/badge/index.js
@@ -6,7 +6,7 @@ export const badgeVariants = tv({
 	variants: {
 		variant: {
 			default: 'border-transparent bg-primary text-primary-content shadow hover:bg-primary/80',
-			secondary: 'border-transparent bg-base-200 hover:bg-base-200/80',
+			secondary: 'border-transparent bg-secondary text-secondary-content hover:bg-secondary/80',
 			destructive:
 				'border-transparent bg-negative text-negative-content shadow hover:bg-negative/80',
 			outline: 'text-foreground'
