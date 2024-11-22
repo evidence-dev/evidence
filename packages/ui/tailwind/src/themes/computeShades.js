2695ccfe85b16d05f0e542dff94aec3ccf9c349c
Zachary Stence <zachstence@gmail.com>
2024-11-22 15:12:02 -0600

    feat: add base-heading color to theme

--- packages/ui/tailwind/src/themes/computeShades.js
+++ packages/ui/tailwind/src/themes/computeShades.js
@@ -12,7 +12,7 @@ const colorsWithContentShade = BUILTIN_COLORS.filter((color) => color.endsWith('
 export const computeShades = (config) => {
 	const colors = config.themes?.colors ?? {};
 
-	// Generate -content shades
+	// Compute -content shades
 	colorsWithContentShade.forEach((colorName) => {
 		const contentColorName = `${colorName}-content`;
 		const color = colors[colorName];
@@ -26,43 +26,50 @@ export const computeShades = (config) => {
 		colors[contentColorName] = contentColor;
 	});
 
-	// Generate base shades
+	// Compute base-200 from base-100
 	colors['base-100'] = colors['base-100'] ?? {};
 	colors['base-200'] = colors['base-200'] ?? {};
-	colors['base-300'] = colors['base-300'] ?? {};
-
 	if (colors['base-100'].light && !colors['base-200'].light) {
 		colors['base-200'].light = darken(colors['base-100'].light, 0.25);
 	}
-	if (colors['base-200'].light && !colors['base-300'].light) {
-		colors['base-300'].light = darken(colors['base-200'].light, 0.5);
-	}
-
 	if (colors['base-100'].dark && !colors['base-200'].dark) {
 		colors['base-200'].dark = brighten(colors['base-100'].dark, 0.25);
+	}
+
+	// Compute base-300 from base-200
+	colors['base-300'] = colors['base-300'] ?? {};
+	if (colors['base-200'].light && !colors['base-300'].light) {
+		colors['base-300'].light = darken(colors['base-200'].light, 0.5);
 	}
 	if (colors['base-200'].dark && !colors['base-300'].dark) {
 		colors['base-300'].dark = brighten(colors['base-200'].dark, 0.5);
 	}
 
-	// Generate base-content shades
-	colors['base-content'] = colors['base-content'] ?? {};
-	if (colors['base-100'].light && !colors['base-content'].light) {
-		colors['base-content'].light = computeContentShade(colors['base-100'].light);
+	// Compute base-heading from base-100
+	colors['base-heading'] = colors['base-heading'] ?? {};
+	if (colors['base-100'].light && !colors['base-heading'].light) {
+		colors['base-heading'].light = computeContentShade(colors['base-100'].light);
 	}
-	if (colors['base-100'].dark && !colors['base-content'].dark) {
-		colors['base-content'].dark = computeContentShade(colors['base-100'].dark);
+	if (colors['base-100'].dark && !colors['base-heading'].dark) {
+		colors['base-heading'].dark = computeContentShade(colors['base-100'].dark);
 	}
 
-	colors['base-content-muted'] = colors['base-content-muted'] ?? {};
-	if (colors['base-100'].light && !colors['base-content-muted'].light) {
-		colors['base-content-muted'].light = brighten(
-			computeContentShade(colors['base-100'].light),
-			1.5
-		);
+	// Compute base-content from base-heading
+	colors['base-content'] = colors['base-content'] ?? {};
+	if (colors['base-heading'].light && !colors['base-content'].light) {
+		colors['base-content'].light = brighten(colors['base-heading'].light, 0.25);
 	}
-	if (colors['base-100'].dark && !colors['base-content-muted'].dark) {
-		colors['base-content-muted'].dark = darken(computeContentShade(colors['base-100'].dark), 1.5);
+	if (colors['base-heading'].dark && !colors['base-content'].dark) {
+		colors['base-content'].dark = darken(colors['base-heading'].dark, 0.25);
+	}
+
+	// Compute base-content-muted from base-content
+	colors['base-content-muted'] = colors['base-content-muted'] ?? {};
+	if (colors['base-content'].light && !colors['base-content-muted'].light) {
+		colors['base-content-muted'].light = brighten(colors['base-content'].light, 1);
+	}
+	if (colors['base-content'].dark && !colors['base-content-muted'].dark) {
+		colors['base-content-muted'].dark = darken(colors['base-content'].dark, 1);
 	}
 
 	return {
