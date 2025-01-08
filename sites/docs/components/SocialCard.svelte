2c9afab5ba3b70cc9b46068c514d12ed79fc65cd
Archie Wood <58074498+archiewood@users.noreply.github.com>
2025-01-08 13:32:29 -0500

    tweak layout

--- sites/docs/components/SocialCard.svelte
+++ sites/docs/components/SocialCard.svelte
@@ -4,7 +4,7 @@
     export let category = 'Evidence Documentation';
 </script>
 
-<div class="font-sans flex flex-col w-full h-full bg-[#0a0a0a] text-[#fafafa] p-16 px-16 normal grid-background border border-b-[rgba(255,255,255,0.15)] border-r-[rgba(255,255,255,0.15)]">
+<div class="font-sans flex flex-col w-full h-full bg-[#0a0a0a] text-[#fafafa] p-18 normal grid-background border border-b-[rgba(255,255,255,0.15)] border-r-[rgba(255,255,255,0.15)]">
 	<div class="flex flex-col w-full h-full text-2xl">
         <div class="flex items-center">
             <img
@@ -16,7 +16,7 @@
         </div>
 		<div class="flex flex-col flex-1 h-full my-auto justify-around my-10">
 			<div class="text-7xl tight -ml-1 leading-[1.2]">{title}</div>
-			<div class="text-4xl -ml-0.5 leading-[1.2]">{description}</div>
+			<div class="text-4xl -ml-0.5 leading-[1.3]">{description}</div>
 		</div>
         <div class="flex items-center justify-between -my-2 opacity-70">
 		    <div class="mono text-3xl">DOCS.EVIDENCE.DEV</div>
@@ -39,7 +39,7 @@
         background-image: 
             linear-gradient(to right, rgba(255, 255, 255, 0.15) 1px, transparent 1px),
             linear-gradient(to bottom, rgba(255, 255, 255, 0.15) 1px, transparent 1px);
-        background-size: 60px 60px;
+        background-size: 63.157px 63.157px;
         background-position: 0 0;
     }
 
