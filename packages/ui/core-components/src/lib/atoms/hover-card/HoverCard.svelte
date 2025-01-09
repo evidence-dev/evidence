a21d8158e7912043ad07fde6f9674101a1338305
Adam <mcrascal@users.noreply.github.com>
2025-01-09 15:39:08 -0500

    prettier

--- packages/ui/core-components/src/lib/atoms/hover-card/HoverCard.svelte
+++ packages/ui/core-components/src/lib/atoms/hover-card/HoverCard.svelte
@@ -7,16 +7,13 @@
 	export let sideOffset = 4;
 	export let openDelay = 0;
 	export let closeDelay = 0;
-
 </script>
 
-<!-- class="rounded-sm underline-offset-4 hover:underline focus-visible:outline-2 focus-visible:outline-offset-8 focus-visible:outline-black" -->
-
-<HoverCard.Root {openDelay} {closeDelay} >
+<HoverCard.Root {openDelay} {closeDelay}>
 	<HoverCard.Trigger>
 		<slot name="trigger" />
 	</HoverCard.Trigger>
-	<HoverCard.Content  {align} {side} {alignOffset} {sideOffset}>
+	<HoverCard.Content {align} {side} {alignOffset} {sideOffset}>
 		<slot name="content" />
 	</HoverCard.Content>
 </HoverCard.Root>
