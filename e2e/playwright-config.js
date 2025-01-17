82963124e7dab2e686d53d7b915e6783ff12d566
brian <contact@noimbrian.com>
2025-01-17 10:48:11 -0600

    chore: increase timeout for expects

--- e2e/playwright-config.js
+++ e2e/playwright-config.js
@@ -15,6 +15,9 @@ export const config = {
 		]
 	],
 	timeout: process.platform === 'win32' ? 60_000 : 30_000,
+	expect: {
+		timeout: 15_000,
+	},
 	use: {
 		colorScheme: 'dark',
 		trace: 'retain-on-failure',
