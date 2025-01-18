f98d5f5e385dda299eb03115ccb3f0e9aececbed
Sean Hughes <sean@evidence.dev>
2025-01-18 18:48:09 -0500

    fmt

--- packages/ui/core-components/src/lib/unsorted/viz/map/EvidenceMap.js
+++ packages/ui/core-components/src/lib/unsorted/viz/map/EvidenceMap.js
@@ -147,14 +147,14 @@ export class EvidenceMap {
 	 */
 	updateBounds = debounce(() => {
 		this.#bounds = Leaflet.latLngBounds(); // Reset bounds to recalculate
-	
+
 		this.#map.eachLayer((layer) => {
 			// Handle layers with sublayers (_layers) - necessary for setting ignoreZoom correctly
 			if (layer._layers && layer._layers instanceof Object) {
 				const firstLayer = Object.values(layer._layers)?.[0];
 				layer.ignoreZoom = firstLayer?.ignoreZoom ?? false;
 			}
-	
+
 			// Extend bounds for layers that shouldn't ignore zoom
 			if (
 				(layer instanceof Leaflet.Marker ||
@@ -166,7 +166,7 @@ export class EvidenceMap {
 				if (bounds) this.#bounds.extend(bounds);
 			}
 		});
-	
+
 		if (this.#bounds.isValid()) {
 			this.#map.fitBounds(this.#bounds, { maxZoom: 12 });
 			if (this.#initZoom) this.#map.setZoom(this.#initZoom);
@@ -175,7 +175,6 @@ export class EvidenceMap {
 			throw new Error('Bounds are invalid!');
 		}
 	}, 100);
-	
 
 	/**
 	 * Adds an interactive geoJSON layer to the map that responds to click events.
