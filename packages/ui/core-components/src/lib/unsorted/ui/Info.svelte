59d6755a821861c7032ef27fc9d6a4d08a5a4c61
Sean Hughes <sean@evidence.dev>
2025-02-02 16:19:14 -0500

    Fix mobile usage issue for Info component

--- packages/ui/core-components/src/lib/unsorted/ui/Info.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/Info.svelte
@@ -9,27 +9,33 @@
 	import chroma from 'chroma-js';
 	import HoverCard from '../../atoms/hover-card/HoverCard.svelte';
 
-	/** @type {import('@steeze-ui/svelte-icon').IconSource} */
 	let icon = InfoCircled;
-
 	export let description = '';
-
 	export let size = 4;
-
 	export let className = undefined;
-
 	const { resolveColor } = getThemeStores();
 	export let color = 'base-content-muted';
+
 	$: colorStore = resolveColor(color);
 	$: textColor = chroma($colorStore).css();
+
+	// State for manual toggle
+	let isOpen = false;
+
+	function toggleOpen() {
+		isOpen = !isOpen;
+	}
 </script>
 
-<HoverCard align="start" side="right" alignOffset={-8} sideOffset={4}>
+<HoverCard open={isOpen} align="start" side="right" alignOffset={-8} sideOffset={4}>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 	<span
 		slot="trigger"
 		style:--textColor={textColor}
-		class="inline-block align-middle pb-0.5 pr-1 leading-4 w-fit {className}"
+		class="inline-block align-middle pb-0.5 pr-1 leading-4 w-fit {className} cursor-pointer"
 		role="tooltip"
+		on:click={toggleOpen}
 	>
 		<slot name="handle">
 			<Icon src={icon} class="w-{size} h-{size} text-[--textColor]" />
