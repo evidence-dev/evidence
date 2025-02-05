5cb914d5b0f9aa7ee41f10b0a35f0ca71ac056d0
Sean Hughes <sean@evidence.dev>
2025-02-05 09:58:01 -0500

    fix a11y issues

--- packages/ui/core-components/src/lib/unsorted/ui/Info.svelte
+++ packages/ui/core-components/src/lib/unsorted/ui/Info.svelte
@@ -28,19 +28,22 @@
 </script>
 
 <HoverCard open={isOpen} align="start" side="right" alignOffset={-8} sideOffset={4}>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
 	<span
 		slot="trigger"
 		style:--textColor={textColor}
 		class="inline-block align-middle pb-0.5 pr-1 leading-4 w-fit {className} cursor-pointer"
-		role="tooltip"
+		role="button"
+		aria-expanded={isOpen}
+		aria-label="Toggle tooltip"
+		tabindex="0"
 		on:click={toggleOpen}
+		on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && toggleOpen()}
 	>
 		<slot name="handle">
 			<Icon src={icon} class="w-{size} h-{size} text-[--textColor]" />
 		</slot>
 	</span>
+
 	<div slot="content" class="bg-base-100 p-2 rounded-md text-base-content text-xs max-w-52">
 		<p class="leading-relaxed text-pretty">
 			{description}
