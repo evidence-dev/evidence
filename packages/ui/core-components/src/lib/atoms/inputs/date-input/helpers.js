7870a48bb0e1c4baf1554cd186002f563c05018a
arefabouhamdan <arefabouhamdan@gmail.com>
2025-09-25 23:52:37 +0300

    Fixed timezone issue

--- packages/ui/core-components/src/lib/atoms/inputs/date-input/helpers.js
+++ packages/ui/core-components/src/lib/atoms/inputs/date-input/helpers.js
@@ -1,7 +1,10 @@
 const YYYYMMDD = /^\d{4}-\d{2}-\d{2}$/;
 
 function dateToYYYYMMDD(date) {
-	return date.toISOString().split('T')[0];
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, '0');
+	const day = String(date.getDate()).padStart(2, '0');
+	return `${year}-${month}-${day}`;
 }
 
 function formatDateString(date) {
