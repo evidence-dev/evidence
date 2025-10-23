f28eea13cadefade895ae8c4c7ee07b2512b5333
Archie Wood <58074498+archiewood@users.noreply.github.com>
2025-10-23 12:37:18 -0400

    formatting

--- packages/datasources/postgres/test/test.js
+++ packages/datasources/postgres/test/test.js
@@ -6,47 +6,53 @@ import 'dotenv/config';
 
 test('query runs', async () => {
 	if (process.env.POSTGRES_DATABASE) {
-	try {
-		const { rows: row_generator, columnTypes } = await runQuery(
-			"select 100 as number_col, now()::date  as date_col, current_timestamp as timestamp_col, 'Evidence' as string_col, true as bool_col",
-			{
-				host: process.env.POSTGRES_HOST,
-				password: process.env.POSTGRES_PASSWORD,
-				database: process.env.POSTGRES_DATABASE,
-				user: process.env.POSTGRES_USER,
-				port: process.env.POSTGRES_PORT,
-				ssl: process.env.POSTGRES_SSL
-			}
-		);
-		const rows = await batchedAsyncGeneratorToArray(row_generator);
-		assert.instance(rows, Array);
-		assert.instance(columnTypes, Array);
-		assert.type(rows[0], 'object');
-		assert.equal(rows[0].number_col, 100);
+		try {
+			const { rows: row_generator, columnTypes } = await runQuery(
+				"select 100 as number_col, now()::date  as date_col, current_timestamp as timestamp_col, 'Evidence' as string_col, true as bool_col",
+				{
+					host: process.env.POSTGRES_HOST,
+					password: process.env.POSTGRES_PASSWORD,
+					database: process.env.POSTGRES_DATABASE,
+					user: process.env.POSTGRES_USER,
+					port: process.env.POSTGRES_PORT,
+					ssl: process.env.POSTGRES_SSL
+				}
+			);
+			const rows = await batchedAsyncGeneratorToArray(row_generator);
+			assert.instance(rows, Array);
+			assert.instance(columnTypes, Array);
+			assert.type(rows[0], 'object');
+			assert.equal(rows[0].number_col, 100);
 
-		let actualColumnTypes = columnTypes.map((columnType) => columnType.evidenceType);
-		let actualColumnNames = columnTypes.map((columnType) => columnType.name);
-		let actualTypePrecisions = columnTypes.map((columnType) => columnType.typeFidelity);
+			let actualColumnTypes = columnTypes.map((columnType) => columnType.evidenceType);
+			let actualColumnNames = columnTypes.map((columnType) => columnType.name);
+			let actualTypePrecisions = columnTypes.map((columnType) => columnType.typeFidelity);
 
-		let expectedColumnTypes = ['number', 'date', 'date', 'string', 'boolean'];
-		let expectedColumnNames = ['number_col', 'date_col', 'timestamp_col', 'string_col', 'bool_col'];
-		let expectedTypePrecision = Array(5).fill(TypeFidelity.PRECISE);
+			let expectedColumnTypes = ['number', 'date', 'date', 'string', 'boolean'];
+			let expectedColumnNames = [
+				'number_col',
+				'date_col',
+				'timestamp_col',
+				'string_col',
+				'bool_col'
+			];
+			let expectedTypePrecision = Array(5).fill(TypeFidelity.PRECISE);
 
-		assert.equal(
-			true,
-			expectedColumnTypes.length === actualColumnTypes.length &&
-				expectedColumnTypes.every((value, index) => value === actualColumnTypes[index])
-		);
-		assert.equal(
-			true,
-			expectedColumnNames.length === actualColumnNames.length &&
-				expectedColumnNames.every((value, index) => value === actualColumnNames[index])
-		);
-		assert.equal(
-			true,
-			expectedTypePrecision.length === actualTypePrecisions.length &&
-				expectedTypePrecision.every((value, index) => value === actualTypePrecisions[index])
-		);
+			assert.equal(
+				true,
+				expectedColumnTypes.length === actualColumnTypes.length &&
+					expectedColumnTypes.every((value, index) => value === actualColumnTypes[index])
+			);
+			assert.equal(
+				true,
+				expectedColumnNames.length === actualColumnNames.length &&
+					expectedColumnNames.every((value, index) => value === actualColumnNames[index])
+			);
+			assert.equal(
+				true,
+				expectedTypePrecision.length === actualTypePrecisions.length &&
+					expectedTypePrecision.every((value, index) => value === actualTypePrecisions[index])
+			);
 		} catch (e) {
 			throw Error(e);
 		}
@@ -58,30 +64,30 @@ test('query runs', async () => {
 
 test('query batches results properly', async () => {
 	if (process.env.POSTGRES_DATABASE) {
-	try {
-		const { rows, expectedRowCount } = await runQuery(
-			'select 1 union all select 2 union all select 3 union all select 4 union all select 5',
-			{
-				host: process.env.POSTGRES_HOST,
-				password: process.env.POSTGRES_PASSWORD,
-				database: process.env.POSTGRES_DATABASE,
-				user: process.env.POSTGRES_USER,
-				port: process.env.POSTGRES_PORT,
-				ssl: process.env.POSTGRES_SSL
-			},
-			2
-		);
+		try {
+			const { rows, expectedRowCount } = await runQuery(
+				'select 1 union all select 2 union all select 3 union all select 4 union all select 5',
+				{
+					host: process.env.POSTGRES_HOST,
+					password: process.env.POSTGRES_PASSWORD,
+					database: process.env.POSTGRES_DATABASE,
+					user: process.env.POSTGRES_USER,
+					port: process.env.POSTGRES_PORT,
+					ssl: process.env.POSTGRES_SSL
+				},
+				2
+			);
 
-		const arr = [];
-		for await (const batch of rows()) {
-			arr.push(batch);
-		}
-		for (const batch of arr.slice(0, -1)) {
-			assert.equal(batch.length, 2);
-		}
-		assert.equal(arr[arr.length - 1].length, 1);
-		assert.equal(expectedRowCount, 5);
-	} catch (e) {
+			const arr = [];
+			for await (const batch of rows()) {
+				arr.push(batch);
+			}
+			for (const batch of arr.slice(0, -1)) {
+				assert.equal(batch.length, 2);
+			}
+			assert.equal(arr[arr.length - 1].length, 1);
+			assert.equal(expectedRowCount, 5);
+		} catch (e) {
 			throw Error(e);
 		}
 	} else {
